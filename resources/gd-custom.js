d6fd631ac95ae188d0cf3770e797907ccb8ead4d
狂风代码 <csg800@126.com>
2022-01-26 14:53:12 +0800

    1. Fixed a syntax error when the a tag has no href attribute 2. If the target of the a tag link is audio, play it directly

--- resources/gd-custom.js
+++ resources/gd-custom.js
@@ -2,7 +2,23 @@
 (function($){
 $(function() {
         $("a").click(function(event) {
-            var link = $(this).attr("href");
+            var ext = '';
+            var link = '';
+            if ('string' == typeof($(this).attr("href"))) {
+                link = $(this).attr("href");
+            } else if ('string' == typeof($(this).attr("src"))) {
+                link = $(this).attr("src");
+            }
+            if ('' == link) {
+                return;
+            }
+            
+            ext = link.substring(link.indexOf('.')).toLowerCase();
+            if ('.mp3' == ext || '.wav' == ext || '.spx' == ext) {
+                playSound(link);
+                return;
+            }
+
             emitClickedEvent(link);
             if(link.indexOf(":")>=0){
                 return;
