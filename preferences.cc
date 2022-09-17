c5035d87fcc93a2fc86ce878c3565e951d8b6b0d
Xiao YiFang <yifang.xiao@gmail.com>
2022-09-17 23:00:13 +0800

    opt:remove scan popup options

--- preferences.cc
+++ preferences.cc
@@ -19,31 +19,8 @@ Preferences::Preferences( QWidget * parent, Config::Class & cfg_ ):
   connect( ui.enableScanPopup, SIGNAL( toggled( bool ) ),
            this, SLOT( enableScanPopupToggled( bool ) ) );
 
-  connect( ui.enableScanPopupModifiers, SIGNAL( toggled( bool ) ),
-           this, SLOT( enableScanPopupModifiersToggled( bool ) ) );
-
   connect( ui.showScanFlag, SIGNAL( toggled( bool ) ),
            this, SLOT( showScanFlagToggled( bool ) ) );
-
-  connect( ui.altKey, SIGNAL( clicked( bool ) ),
-           this, SLOT( wholeAltClicked( bool ) ) );
-  connect( ui.ctrlKey, SIGNAL( clicked( bool ) ),
-           this, SLOT( wholeCtrlClicked( bool ) ) );
-  connect( ui.shiftKey, SIGNAL( clicked( bool ) ),
-           this, SLOT( wholeShiftClicked( bool ) ) );
-
-  connect( ui.leftAlt, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideAltClicked( bool ) ) );
-  connect( ui.rightAlt, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideAltClicked( bool ) ) );
-  connect( ui.leftCtrl, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideCtrlClicked( bool ) ) );
-  connect( ui.rightCtrl, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideCtrlClicked( bool ) ) );
-  connect( ui.leftShift, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideShiftClicked( bool ) ) );
-  connect( ui.rightShift, SIGNAL( clicked( bool ) ),
-           this, SLOT( sideShiftClicked( bool ) ) );
 
   connect( ui.buttonBox, SIGNAL( helpRequested() ),
            this, SLOT( helpRequested() ) );
@@ -192,21 +169,7 @@ Preferences::Preferences( QWidget * parent, Config::Class & cfg_ ):
 
   ui.enableScanPopup->setChecked( p.enableScanPopup );
   ui.startWithScanPopupOn->setChecked( p.startWithScanPopupOn );
-  ui.enableScanPopupModifiers->setChecked( p.enableScanPopupModifiers );
 
-  ui.altKey->setChecked( p.scanPopupModifiers & KeyboardState::Alt );
-  ui.ctrlKey->setChecked( p.scanPopupModifiers & KeyboardState::Ctrl );
-  ui.shiftKey->setChecked( p.scanPopupModifiers & KeyboardState::Shift );
-  ui.winKey->setChecked( p.scanPopupModifiers & KeyboardState::Win );
-  ui.leftAlt->setChecked( p.scanPopupModifiers & KeyboardState::LeftAlt );
-  ui.rightAlt->setChecked( p.scanPopupModifiers & KeyboardState::RightAlt );
-  ui.leftCtrl->setChecked( p.scanPopupModifiers & KeyboardState::LeftCtrl );
-  ui.rightCtrl->setChecked( p.scanPopupModifiers & KeyboardState::RightCtrl );
-  ui.leftShift->setChecked( p.scanPopupModifiers & KeyboardState::LeftShift );
-  ui.rightShift->setChecked( p.scanPopupModifiers & KeyboardState::RightShift );
-
-  ui.scanPopupAltMode->setChecked( p.scanPopupAltMode );
-  ui.scanPopupAltModeSecs->setValue( p.scanPopupAltModeSecs );
   ui.ignoreOwnClipboardChanges->setChecked( p.ignoreOwnClipboardChanges );
   ui.scanToMainWindow->setChecked( p.scanToMainWindow );
   ui.scanPopupUnpinnedWindowFlags->setCurrentIndex( p.scanPopupUnpinnedWindowFlags );
@@ -237,22 +200,6 @@ Preferences::Preferences( QWidget * parent, Config::Class & cfg_ ):
   ui.maxDictsInContextMenu->setValue( p.maxDictionaryRefsInContextMenu );
 
   // Different platforms have different keys available
-
-#ifdef Q_OS_WIN32
-  ui.winKey->hide();
-#else
-  ui.leftAlt->hide();
-  ui.rightAlt->hide();
-  ui.leftCtrl->hide();
-  ui.rightCtrl->hide();
-  ui.leftShift->hide();
-  ui.rightShift->hide();
-#ifdef Q_OS_MAC
-  ui.altKey->setText( "Opt" );
-  ui.winKey->setText( "Ctrl" );
-  ui.ctrlKey->setText( "Cmd" );
-#endif
-#endif
 
 //Platform-specific options
 
@@ -415,21 +362,7 @@ Config::Preferences Preferences::getPreferences()
 
   p.enableScanPopup = ui.enableScanPopup->isChecked();
   p.startWithScanPopupOn = ui.startWithScanPopupOn->isChecked();
-  p.enableScanPopupModifiers = ui.enableScanPopupModifiers->isChecked();
 
-  p.scanPopupModifiers += ui.altKey->isChecked() ? KeyboardState::Alt : 0;
-  p.scanPopupModifiers += ui.ctrlKey->isChecked() ? KeyboardState::Ctrl: 0;
-  p.scanPopupModifiers += ui.shiftKey->isChecked() ? KeyboardState::Shift: 0;
-  p.scanPopupModifiers += ui.winKey->isChecked() ? KeyboardState::Win: 0;
-  p.scanPopupModifiers += ui.leftAlt->isChecked() ? KeyboardState::LeftAlt: 0;
-  p.scanPopupModifiers += ui.rightAlt->isChecked() ? KeyboardState::RightAlt: 0;
-  p.scanPopupModifiers += ui.leftCtrl->isChecked() ? KeyboardState::LeftCtrl: 0;
-  p.scanPopupModifiers += ui.rightCtrl->isChecked() ? KeyboardState::RightCtrl: 0;
-  p.scanPopupModifiers += ui.leftShift->isChecked() ? KeyboardState::LeftShift: 0;
-  p.scanPopupModifiers += ui.rightShift->isChecked() ? KeyboardState::RightShift: 0;
-
-  p.scanPopupAltMode = ui.scanPopupAltMode->isChecked();
-  p.scanPopupAltModeSecs = ui.scanPopupAltModeSecs->value();
   p.ignoreOwnClipboardChanges = ui.ignoreOwnClipboardChanges->isChecked();
   p.scanToMainWindow = ui.scanToMainWindow->isChecked();
 #ifdef HAVE_X11
@@ -582,7 +515,7 @@ Config::Preferences Preferences::getPreferences()
 
 void Preferences::enableScanPopupToggled( bool b )
 {
-  ui.scanPopupModifiers->setEnabled( b && ui.enableScanPopupModifiers->isChecked() );
+  ui.scanPopupModifiers->setEnabled( b );
 }
 
 void Preferences::enableScanPopupModifiersToggled( bool b )
@@ -592,60 +525,9 @@ void Preferences::enableScanPopupModifiersToggled( bool b )
     ui.showScanFlag->setChecked( false );
 }
 
-void Preferences::showScanFlagToggled( bool b )
-{
-  if( b )
-    ui.enableScanPopupModifiers->setChecked( false );
-}
-
 void Preferences::on_scanPopupUnpinnedWindowFlags_currentIndexChanged( int index )
 {
   ui.scanPopupUnpinnedBypassWMHint->setEnabled( Config::spwfFromInt( index ) != Config::SPWF_default );
-}
-
-void Preferences::wholeAltClicked( bool b )
-{
-  if ( b )
-  {
-    ui.leftAlt->setChecked( false );
-    ui.rightAlt->setChecked( false );
-  }
-}
-
-void Preferences::wholeCtrlClicked( bool b )
-{
-  if ( b )
-  {
-    ui.leftCtrl->setChecked( false );
-    ui.rightCtrl->setChecked( false );
-  }
-}
-
-void Preferences::wholeShiftClicked( bool b )
-{
-  if ( b )
-  {
-    ui.leftShift->setChecked( false );
-    ui.rightShift->setChecked( false );
-  }
-}
-
-void Preferences::sideAltClicked( bool )
-{
-  if ( ui.leftAlt->isChecked() || ui.rightAlt->isChecked() )
-    ui.altKey->setChecked( false );
-}
-
-void Preferences::sideCtrlClicked( bool )
-{
-  if ( ui.leftCtrl->isChecked() || ui.rightCtrl->isChecked() )
-    ui.ctrlKey->setChecked( false );
-}
-
-void Preferences::sideShiftClicked( bool )
-{
-  if ( ui.leftShift->isChecked() || ui.rightShift->isChecked() )
-    ui.shiftKey->setChecked( false );
 }
 
 void Preferences::on_enableMainWindowHotkey_toggled( bool checked )
