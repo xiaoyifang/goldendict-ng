7c30b89336769ae48695a08b6aca6c7c06612264
Xiao YiFang <yifang.xiao@foxmail.com>
2022-10-02 19:53:54 +0800

    opt: headwords dialog filtering optimization
    when in filtering process, fetch more return false.

--- dictionary.hh
+++ dictionary.hh
@@ -443,7 +443,7 @@ public:
   /// Retrieve all dictionary headwords
   virtual bool getHeadwords( QStringList & )
   { return false; }
-  virtual  void findHeadWordsWithLenth( int &, QSet< QString > * headwords, uint32_t length ){}
+  virtual  void findHeadWordsWithLenth( int &, QSet< QString > * /*headwords*/, uint32_t ){}
 
   /// Enable/disable search via synonyms
   void setSynonymSearchEnabled( bool enabled )
