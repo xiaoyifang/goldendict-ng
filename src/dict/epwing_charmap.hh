50cf0a948e1f5a9625aeab92a1c8a9c1208f87cb
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-04-29 19:54:31 +0800

    fix:epwing narrow font display incorrectly
    fix goldendict/goldendict#1244

--- src/dict/epwing_charmap.hh
+++ src/dict/epwing_charmap.hh
@@ -1,7 +1,6 @@
 #ifndef __EPWING_CHARMAP_HH_INCLUDED_
 #define __EPWING_CHARMAP_HH_INCLUDED_
 
-#include <QString>
 #include <QByteArray>
 #include <QMap>
 
@@ -10,16 +9,12 @@ namespace Epwing {
 class EpwingCharmap
 {
 public:
-
   /// The class is a singleton.
   static EpwingCharmap & instance();
 
   /// Map Epwing extra char to Utf-8
   QByteArray mapToUtf8( QString const & code );
-
 private:
-  EpwingCharmap();
-
   void addEntry( QString const & code, int ch );
 
   QMap< QString, QChar > charMap;
