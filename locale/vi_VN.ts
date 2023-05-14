01c91d72dac37b75f75e865cc2ea80cfac3f5de3
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-05-14 06:37:32 -0800

    New translations Vietnamese from Crowdin

--- locale/vi_VN.ts
+++ locale/vi_VN.ts
@@ -5278,6 +5278,21 @@ Nhật, dựa trên hệ thống âm vị Anh ngữ</translation>
       <translation>Bảng chữ cái tiếng Nhật Hiragana</translation>
     </message>
     <message>
+      <location filename="../src/ui/sources.ui" line="835"/>
+      <source>Custom transliteration</source>
+      <translation type="unfinished">Custom transliteration</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/sources.ui" line="844"/>
+      <source>This only applied in search phrase, with each line represent a transliteration,semicolon seperated. For example, ae;æ,users can input ae to represent æ in the target word.</source>
+      <translation type="unfinished">This only applied in search phrase, with each line represent a transliteration,semicolon seperated. For example, ae;æ,users can input ae to represent æ in the target word.</translation>
+    </message>
+    <message>
+      <location filename="../src/ui/sources.ui" line="854"/>
+      <source>ae;æ #this is an example</source>
+      <translation type="unfinished">ae;æ #this is an example</translation>
+    </message>
+    <message>
       <location filename="../src/ui/sources.ui" line="681"/>
       <source>Transliteration</source>
       <translation>Chuyển tự</translation>
