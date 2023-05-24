02de8eb6dccabd87e656c9b6d7aac7cad820ba58
YiFang Xiao <yifang.xiao@foxmail.com>
2023-05-25 07:55:41 +0800

    fix: tts exception when initiliaze

--- src/speechclient.cc
+++ src/speechclient.cc
@@ -14,33 +14,39 @@ SpeechClient::Engines SpeechClient::availableEngines()
   const auto innerEngines = QTextToSpeech::availableEngines();
 
   for ( const auto & engine_name : innerEngines ) {
-    const auto sp = new QTextToSpeech( engine_name == "default" ? nullptr : engine_name );
+    try {
+      auto * const sp = new QTextToSpeech( engine_name == "default" ? nullptr : engine_name );
 
-    #if ( QT_VERSION >= QT_VERSION_CHECK( 6, 0, 0 ) )
-    if ( !sp || sp->state() == QTextToSpeech::Error )
-      continue;
-    #else
+#if ( QT_VERSION >= QT_VERSION_CHECK( 6, 0, 0 ) )
+      if ( !sp || sp->state() == QTextToSpeech::Error )
+        continue;
+#else
       if ( !sp || sp->state() == QTextToSpeech::BackendError )
         continue;
-    #endif
+#endif
 
-    qDebug() << engine_name << sp->state();
+      qDebug() << engine_name << sp->state();
 
-    const QVector< QLocale > locales = sp->availableLocales();
-    for ( const QLocale & locale : locales ) {
-      //on some platforms ,change the locale will change voices too.
-      sp->setLocale( locale );
-      for ( const QVoice & voice : sp->availableVoices() ) {
-        const QString name( QString( "%4 - %3 %1 (%2)" )
-          .arg( QLocale::languageToString( locale.language() ),
-                ( QLocale::countryToString( locale.country() ) ),
-                voice.name(),
-                engine_name ) );
-        Engine engine( Config::VoiceEngine( engine_name, name, voice.name(), QLocale( locale ), 50, 0 ) );
-        engines.push_back( engine );
+      const QVector< QLocale > locales = sp->availableLocales();
+      for ( const QLocale & locale : locales ) {
+        //on some platforms ,change the locale will change voices too.
+        sp->setLocale( locale );
+        for ( const QVoice & voice : sp->availableVoices() ) {
+          const QString name( QString( "%4 - %3 %1 (%2)" )
+                                .arg( QLocale::languageToString( locale.language() ),
+                                      ( QLocale::countryToString( locale.country() ) ),
+                                      voice.name(),
+                                      engine_name ) );
+          Engine engine( Config::VoiceEngine( engine_name, name, voice.name(), QLocale( locale ), 50, 0 ) );
+          engines.push_back( engine );
+        }
+
+        sp->deleteLater();
       }
-
-      sp->deleteLater();
+    }
+    catch ( std::exception & e ) {
+      qDebug() << e.what();
+      continue;
     }
   }
 
