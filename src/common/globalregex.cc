defc2447cf1d67573fc6a5ca541b02b2b0bf4cd2
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-05-27 00:27:50 +0800

    opt: convert escape string to rawstring (#749)
    * opt: convert escape string to rawstring
    
    * 🎨 apply clang-format changes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/common/globalregex.cc
+++ src/common/globalregex.cc
@@ -36,24 +36,24 @@ QRegularExpression Mdx::anchorIdRe2( R"(([\s"'](?:name|id)\s*=)\s*(?=[^"'])([^\s
                                      QRegularExpression::CaseInsensitiveOption );
 QRegularExpression Mdx::anchorLinkRe( R"(([\s"']href\s*=\s*["'])entry://#)",
                                       QRegularExpression::CaseInsensitiveOption );
-QRegularExpression Mdx::audioRe( R"(([\s"']href\s*=)\s*(["'])sound://([^">]+)\2)",
-                                 QRegularExpression::CaseInsensitiveOption
-                                   | QRegularExpression::InvertedGreedinessOption );
-QRegularExpression Mdx::stylesRe( "([\\s\"']href\\s*=)\\s*([\"'])(?!\\s*\\b(?:(?:bres|https?|ftp)://"
-                                  "|(?:data|javascript):))(?:file://)?[\\x00-\\x1f\\x7f]*\\.*/?([^\">]+)\\2",
-                                  QRegularExpression::CaseInsensitiveOption );
-QRegularExpression Mdx::stylesRe2( "([\\s\"']href\\s*=)\\s*(?![\\s\"']|\\b(?:(?:bres|https?|ftp)://"
-                                   "|(?:data|javascript):))(?:file://)?[\\x00-\\x1f\\x7f]*\\.*/?([^\\s\">]+)",
-                                   QRegularExpression::CaseInsensitiveOption );
+const QRegularExpression Mdx::audioRe( R"(([\s"']href\s*=)\s*(["'])sound://([^">]+)\2)",
+                                       QRegularExpression::CaseInsensitiveOption
+                                         | QRegularExpression::InvertedGreedinessOption );
+const QRegularExpression Mdx::stylesRe(
+  R"(([\s"']href\s*=)\s*(["'])(?!\s*\b(?:(?:bres|https?|ftp)://|(?:data|javascript):))(?:file://)?[\x00-\x1f\x7f]*\.*/?([^">]+)\2)",
+  QRegularExpression::CaseInsensitiveOption );
+QRegularExpression Mdx::stylesRe2(
+  R"(([\s"']href\s*=)\s*(?![\s"']|\b(?:(?:bres|https?|ftp)://|(?:data|javascript):))(?:file://)?[\x00-\x1f\x7f]*\.*/?([^\s">]+))",
+  QRegularExpression::CaseInsensitiveOption );
 QRegularExpression Mdx::inlineScriptRe( R"(<\s*script(?:(?=\s)(?:(?![\s"']src\s*=)[^>])+|\s*)>)",
                                         QRegularExpression::CaseInsensitiveOption );
 QRegularExpression Mdx::closeScriptTagRe( "<\\s*/script\\s*>", QRegularExpression::CaseInsensitiveOption );
-QRegularExpression Mdx::srcRe( "([\\s\"'](?:src|srcset)\\s*=)\\s*([\"'])(?!\\s*\\b(?:(?:bres|https?|ftp)://"
-                               "|(?:data|javascript):))(?:file://)?[\\x00-\\x1f\\x7f]*\\.*/?([^\">]+)\\2",
-                               QRegularExpression::CaseInsensitiveOption );
-QRegularExpression Mdx::srcRe2( "([\\s\"'](?:src|srcset)\\s*=)\\s*(?![\\s\"']|\\b(?:(?:bres|https?|ftp)://"
-                                "|(?:data|javascript):))(?:file://)?[\\x00-\\x1f\\x7f]*\\.*/?([^\\s\">]+)",
-                                QRegularExpression::CaseInsensitiveOption );
+QRegularExpression Mdx::srcRe(
+  R"(([\s"'](?:src|srcset)\s*=)\s*(["'])(?!\s*\b(?:(?:bres|https?|ftp)://|(?:data|javascript):))(?:file://)?[\x00-\x1f\x7f]*\.*/?([^">]+)\2)",
+  QRegularExpression::CaseInsensitiveOption );
+QRegularExpression Mdx::srcRe2(
+  R"(([\s"'](?:src|srcset)\s*=)\s*(?![\s"']|\b(?:(?:bres|https?|ftp)://|(?:data|javascript):))(?:file://)?[\x00-\x1f\x7f]*\.*/?([^\s">]+))",
+  QRegularExpression::CaseInsensitiveOption );
 
 QRegularExpression Mdx::links( R"(url\(\s*(['"]?)([^'"]*)(['"]?)\s*\))",
                                QRegularExpression::CaseInsensitiveOption );
@@ -61,8 +61,8 @@ QRegularExpression Mdx::links( R"(url\(\s*(['"]?)([^'"]*)(['"]?)\s*\))",
 QRegularExpression Mdx::fontFace( R"((?:url\s*\(\s*\"(.*?)\"\s*)\))",
                                     QRegularExpression::CaseInsensitiveOption|QRegularExpression::DotMatchesEverythingOption );
 
-QRegularExpression Mdx::styleElment( R"((<style[^>]*>)([\w\W]*?)(<\/style>))",
-                                  QRegularExpression::CaseInsensitiveOption);
+QRegularExpression Mdx::styleElement( R"((<style[^>]*>)([\w\W]*?)(<\/style>))",
+                                      QRegularExpression::CaseInsensitiveOption );
 
 
 QRegularExpression Zim::linkSpecialChar("[\\.\\/]");
