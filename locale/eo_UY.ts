fc62422200cbe71a40df4b6e2b6ef789998e2ea3
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-01 23:11:32 +0800

    New translations Esperanto from Crowdin

--- locale/eo_UY.ts
+++ locale/eo_UY.ts
@@ -41,7 +41,7 @@
     <message>
       <location filename="../src/ui/about.cc" line="19"/>
       <source>Based on Qt %1 (%2, %3)</source>
-      <translation type="unfinished">Based on Qt %1 (%2, %3)</translation>
+      <translation>Surbaze de Qt %1 (%2, %3)</translation>
     </message>
   </context>
   <context>
@@ -127,7 +127,7 @@
     <message>
       <location filename="../src/article_maker.cc" line="236"/>
       <source>No translation for &lt;b dir=&quot;%3&quot;&gt;%1&lt;/b&gt; was found in group &lt;b&gt;%2&lt;/b&gt;.</source>
-      <translation type="unfinished">No translation for &lt;b dir=&quot;%3&quot;&gt;%1&lt;/b&gt; was found in group &lt;b&gt;%2&lt;/b&gt;.</translation>
+      <translation>Neniu traduko por &lt;b dir=&quot;%3&quot;&gt;%1&lt;/b&gt; estis trovita en grupo &lt;b&gt;%2&lt;/b&gt;.</translation>
     </message>
     <message>
       <location filename="../src/article_maker.cc" line="293"/>
@@ -1028,7 +1028,7 @@ inter klasika kaj lerneja ortografio en cirila)</translation>
     <message>
       <location filename="../src/fulltextsearch.cc" line="339"/>
       <source>The querying word can not be empty.</source>
-      <translation type="unfinished">The querying word can not be empty.</translation>
+      <translation>La demanda vorto ne povas esti malplena.</translation>
     </message>
     <message>
       <location filename="../src/fulltextsearch.cc" line="234"/>
@@ -1447,7 +1447,7 @@ inter klasika kaj lerneja ortografio en cirila)</translation>
     <message>
       <location filename="../src/ui/initializing.ui" line="99"/>
       <source>Indexing: </source>
-      <translation type="unfinished">Indexing: </translation>
+      <translation>Indeksado: </translation>
     </message>
     <message>
       <location filename="../src/ui/initializing.ui" line="121"/>
@@ -1467,7 +1467,7 @@ inter klasika kaj lerneja ortografio en cirila)</translation>
     <message>
       <location filename="../src/initializing.cc" line="28"/>
       <source>Indexing...</source>
-      <translation type="unfinished">Indexing...</translation>
+      <translation>Indeksante...</translation>
     </message>
   </context>
   <context>
@@ -3044,7 +3044,7 @@ inter klasika kaj lerneja ortografio en cirila)</translation>
     <message>
       <location filename="../src/ui/mainwindow.cc" line="945"/>
       <source>WARNING: %1</source>
-      <translation type="unfinished">WARNING: %1</translation>
+      <translation>AVERTO: %1</translation>
     </message>
     <message>
       <location filename="../src/ui/mainwindow.cc" line="1075"/>
@@ -4109,12 +4109,12 @@ tamen, la artikolo el la plej supra vortaro estas montrata.</translation>
     <message>
       <location filename="../src/ui/preferences.ui" line="629"/>
       <source>Delay time</source>
-      <translation type="unfinished">Delay time</translation>
+      <translation>Malfrua tempo</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui" line="636"/>
       <source> ms</source>
-      <translation type="unfinished"> ms</translation>
+      <translation> ms</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui" line="895"/>
@@ -4237,12 +4237,12 @@ forigas sian retan kaŝmemoron el disko dum eliro.</translation>
     <message>
       <location filename="../src/ui/preferences.ui" line="1302"/>
       <source>Applies only to new incoming dictionaries.</source>
-      <translation type="unfinished">Applies only to new incoming dictionaries.</translation>
+      <translation>Validas nur por novaj alvenantaj vortaroj.</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui" line="1305"/>
       <source>Enable positional information in the fulltext</source>
-      <translation type="unfinished">Enable positional information in the fulltext</translation>
+      <translation>Ebligu poziciajn informojn en la plenteksto</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui" line="1370"/>
@@ -4620,12 +4620,12 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     <message>
       <location filename="../src/main.cc" line="190"/>
       <source>Reset window state.</source>
-      <translation type="unfinished">Reset window state.</translation>
+      <translation>Restarigi fenestrostaton.</translation>
     </message>
     <message>
       <location filename="../src/main.cc" line="192"/>
       <source>Disable tts.</source>
-      <translation type="unfinished">Disable tts.</translation>
+      <translation>Malebligu tts.</translation>
     </message>
     <message>
       <location filename="../src/main.cc" line="196"/>
@@ -4645,7 +4645,7 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     <message>
       <location filename="../src/main.cc" line="209"/>
       <source>Print version and diagnosis info.</source>
-      <translation type="unfinished">Print version and diagnosis info.</translation>
+      <translation>Presa versio kaj diagnozaj informoj.</translation>
     </message>
   </context>
   <context>
@@ -4773,7 +4773,7 @@ povus esti regrandigita aŭ administrita alimaniere.</translation>
     <message>
       <location filename="../src/ui/scanpopup.cc" line="943"/>
       <source>WARNING: %1</source>
-      <translation type="unfinished">WARNING: %1</translation>
+      <translation>AVERTO: %1</translation>
     </message>
   </context>
   <context>
