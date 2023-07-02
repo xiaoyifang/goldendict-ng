25976426be69b8886a01a2e46e5118ef6289be35
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-02 09:00:34 +0800

    New translations Greek from Crowdin

--- locale/el_GR.ts
+++ locale/el_GR.ts
@@ -1034,7 +1034,7 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <location filename="../src/fulltextsearch.cc" line="230"/>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>Προκαθορισμένο</translation>
     </message>
     <message>
       <location filename="../src/fulltextsearch.cc" line="243"/>
@@ -4459,12 +4459,12 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
-      <translation type="unfinished">Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</translation>
+      <translation>Απαιτούνται πληροφορίες θέσης για τη χρήση της αναζήτησης φράσεων Xapian&apos;s και του τελεστή NEAR, αλλά το μέγεθος της βάσης δεδομένων θα είναι πολύ μεγαλύτερο. Ισχύει μόνο για νέα εισερχόμενα λεξικά.</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Enable index with positional information</source>
-      <translation type="unfinished">Enable index with positional information</translation>
+      <translation>Ενεργοποίηση ευρετηρίου με πληροφορίες θέσης</translation>
     </message>
   </context>
   <context>
