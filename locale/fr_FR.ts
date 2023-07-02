99b6629c686b73864c8297c6812f845f608997c3
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-02 09:00:26 +0800

    New translations French from Crowdin

--- locale/fr_FR.ts
+++ locale/fr_FR.ts
@@ -1037,7 +1037,7 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <location filename="../src/fulltextsearch.cc" line="230"/>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>Défaut</translation>
     </message>
     <message>
       <location filename="../src/fulltextsearch.cc" line="243"/>
@@ -4458,12 +4458,12 @@ des dictionnaires Stardict, Babylon et GLS</translation>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
-      <translation type="unfinished">Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</translation>
+      <translation>Les informations de position sont nécessaires pour utiliser la recherche de phrases Xapian&apos;et l'opérateur NEAR, mais la taille de la base de données sera beaucoup plus grande. S'applique uniquement aux nouveaux dictionnaires entrants.</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Enable index with positional information</source>
-      <translation type="unfinished">Enable index with positional information</translation>
+      <translation>Activer l'index avec les informations de position</translation>
     </message>
   </context>
   <context>
