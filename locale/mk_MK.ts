02e5389fa6cf6cec0878bda0f212f32fdf11b928
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-02 09:00:24 +0800

    New translations Macedonian from Crowdin

--- locale/mk_MK.ts
+++ locale/mk_MK.ts
@@ -1039,7 +1039,7 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <location filename="../src/fulltextsearch.cc" line="230"/>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>Стандардно</translation>
     </message>
     <message>
       <location filename="../src/fulltextsearch.cc" line="243"/>
@@ -4465,12 +4465,12 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
-      <translation type="unfinished">Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</translation>
+      <translation>Позиционите информации се потребни за користење на пребарување на фрази Xapian&apos;s и оператор NEAR, но големината на базата на податоци ќе биде многу поголема. Важи само за новите дојдовни речници.</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Enable index with positional information</source>
-      <translation type="unfinished">Enable index with positional information</translation>
+      <translation>Овозможете индекс со позициони информации</translation>
     </message>
   </context>
   <context>
