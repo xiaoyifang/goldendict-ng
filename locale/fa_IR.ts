9305eb5a4694ee5eb108ac1e1f8612b613a4e1a1
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-02 09:00:25 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -1032,7 +1032,7 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <location filename="../src/fulltextsearch.cc" line="230"/>
       <source>Default</source>
-      <translation type="unfinished">Default</translation>
+      <translation>پیش فرض</translation>
     </message>
     <message>
       <location filename="../src/fulltextsearch.cc" line="243"/>
@@ -4454,12 +4454,12 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
-      <translation type="unfinished">Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</translation>
+      <translation>برای استفاده از جستجوی عبارت Xapian&apos;و عملگر NEAR به اطلاعات موقعیتی نیاز است، اما اندازه پایگاه داده بسیار بزرگتر خواهد بود. فقط برای فرهنگ لغت‌های ورودی جدید اعمال می‌شود.</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source>Enable index with positional information</source>
-      <translation type="unfinished">Enable index with positional information</translation>
+      <translation>فهرست را با اطلاعات موقعیتی فعال کنید</translation>
     </message>
   </context>
   <context>
