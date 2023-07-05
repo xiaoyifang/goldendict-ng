1a11ecc2a1393a7bb0fcc2d939f69f40d524faba
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-05 10:46:34 +0800

    New translations Serbian (Cyrillic) from Crowdin

--- locale/sr_SP.ts
+++ locale/sr_SP.ts
@@ -4112,7 +4112,7 @@ however, the article from the topmost dictionary is shown.</source>
     <message>
       <location filename="../src/ui/preferences.ui"/>
       <source> ms</source>
-      <translation> Госпођа</translation>
+      <translation type="unfinished"> ms</translation>
     </message>
     <message>
       <location filename="../src/ui/preferences.ui"/>
