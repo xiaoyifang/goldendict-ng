0102a87a4f8c3ee5db055b521883ceb5e58cb7b2
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:12 +0800

    New translations Lithuanian from Crowdin

--- locale/lt_LT.ts
+++ locale/lt_LT.ts
@@ -651,11 +651,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Įjungta viso teksto paieška</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Viso teksto paieška išjungta</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3575,11 +3575,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Reikia paleisti iš naujo</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Paleiskite iš naujo, kad pritaikytumėte sąsajos stiliaus pakeitimą.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3619,11 +3619,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Sąsajos stilius:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Sukurkite viso teksto rodyklę su lygiagrečiomis gijomis </translation>
     </message>
   </context>
   <context>
