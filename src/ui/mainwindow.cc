6539775c4fe8a2ce00e09c6a1e2ab0f442143616
shenleban tongying <shenlebantongying@gmail.com>
2023-07-26 20:48:50 -0400

    use qt header

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -1,7 +1,7 @@
 /* This file is (c) 2008-2012 Konstantin Isakov <ikm@goldendict.org>
  * Part of GoldenDict. Licensed under GPLv3 or later, see the LICENSE file */
 
-#include "qnamespace.h"
+#include <Qt>
 #include <QScopeGuard>
 #ifndef NO_EPWING_SUPPORT
   #include "dict/epwing_book.hh"
