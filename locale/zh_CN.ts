50816163cf92d437098380f46b4c3fb5297b3b4b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:23 +0800

    New translations Chinese Simplified from Crowdin

--- locale/zh_CN.ts
+++ locale/zh_CN.ts
@@ -651,11 +651,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>启用全文搜索</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>全文搜索已禁用</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3561,11 +3561,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>需要重启</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>重新启动以应用界面风格更改。</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3605,11 +3605,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>界面风格：</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>使用并行线程创建全文索引 </translation>
     </message>
   </context>
   <context>
