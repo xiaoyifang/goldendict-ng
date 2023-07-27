7d2c83d6efc23c31415f044b18acc0aa6c91459e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:22 +0800

    New translations Ukrainian from Crowdin

--- locale/uk_UA.ts
+++ locale/uk_UA.ts
@@ -652,11 +652,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Увімкнено повнотекстовий пошук</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Повнотекстовий пошук вимкнено</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3579,11 +3579,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Потрібен перезапуск</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Перезапустіть, щоб застосувати зміну стилю інтерфейсу.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3623,11 +3623,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Стиль інтерфейсу:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Створіть повнотекстовий індекс із паралельними потоками </translation>
     </message>
   </context>
   <context>
