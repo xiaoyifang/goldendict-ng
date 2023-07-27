768e1c6fde5a018b5cff472e711598300af9f82f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:27 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -652,11 +652,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>جستجوی متن کامل فعال شد</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>جستجوی متن کامل غیرفعال شد</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3575,11 +3575,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>نیاز به راه اندازی مجدد</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>برای اعمال تغییر سبک رابط، راه اندازی مجدد کنید.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3619,11 +3619,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>سبک رابط:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>نمایه تمام متن را با رشته های موازی ایجاد کنید </translation>
     </message>
   </context>
   <context>
