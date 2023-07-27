81278d1a35c5ef4fc417fec912ecc8ae158b2dbe
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:33 +0800

    New translations Tajik from Crowdin

--- locale/tg_TJ.ts
+++ locale/tg_TJ.ts
@@ -652,11 +652,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Ҷустуҷӯи пурраи матн фаъол карда шудааст</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Ҷустуҷӯи пурраи матн ғайрифаъол аст</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3578,11 +3578,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Аз нав оғоз кардан лозим аст</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Барои татбиқи тағири услуби интерфейс аз нав оғоз кунед.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3622,11 +3622,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Услуби интерфейс:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Бо риштаҳои мувозӣ индекси пурраи матн эҷод кунед </translation>
     </message>
   </context>
   <context>
