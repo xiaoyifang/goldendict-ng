12efe9a8d52aaa7b552023b84e6c22ff3af94b02
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:01 +0800

    New translations French from Crowdin

--- locale/fr_FR.ts
+++ locale/fr_FR.ts
@@ -656,11 +656,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Recherche plein texte activée</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Recherche plein texte désactivée</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3579,11 +3579,11 @@ des dictionnaires Stardict, Babylon et GLS</translation>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Redémarrage nécessaire</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Redémarrez pour appliquer le changement de style d'interface.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3623,11 +3623,11 @@ des dictionnaires Stardict, Babylon et GLS</translation>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Style d'interface :</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Créer un index de texte intégral avec des threads parallèles </translation>
     </message>
   </context>
   <context>
