28e8d5811ab9f2d1e49dd4ac5608e2534a993448
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:13 +0800

    New translations Macedonian from Crowdin

--- locale/mk_MK.ts
+++ locale/mk_MK.ts
@@ -658,11 +658,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Овозможено е пребарување во цел текст</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Пребарувањето во цел текст е оневозможено</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3586,11 +3586,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Потребно е рестартирање</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Рестартирајте за да ја примените промената на стилот на интерфејсот.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3630,11 +3630,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Стил на интерфејс:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Креирајте индекс на целосен текст со паралелни нишки </translation>
     </message>
   </context>
   <context>
