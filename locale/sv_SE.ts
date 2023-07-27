729eb8b8a8662130f63369d2ef8c27d9db5b0748
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:20 +0800

    New translations Swedish from Crowdin

--- locale/sv_SE.ts
+++ locale/sv_SE.ts
@@ -652,11 +652,11 @@ klassisk rättstavning och skolrättstavning i kyrillisk skrift)</translation>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Fulltextsökning aktiverad</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Fulltextsökning avaktiverad</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3580,11 +3580,11 @@ från Stardict, Babylon och GLS ordböcker</translation>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Omstart behövs</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Starta om för att tillämpa ändringen av gränssnittsstilen.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3624,11 +3624,11 @@ från Stardict, Babylon och GLS ordböcker</translation>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Gränssnittsstil:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Skapa fulltextindex med parallella trådar </translation>
     </message>
   </context>
   <context>
