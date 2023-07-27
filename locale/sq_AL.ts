993f3e65eae5d56ae78b1c4cf48044eaa3cb58ce
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:18 +0800

    New translations Albanian from Crowdin

--- locale/sq_AL.ts
+++ locale/sq_AL.ts
@@ -651,11 +651,11 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Kërkimi me tekst të plotë u aktivizua</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Kërkimi në tekst të plotë është çaktivizuar</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3570,11 +3570,11 @@ nga fjalorët Stardict, Babylon dhe GLS</translation>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Nevojitet rinisja</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Rinisni për të aplikuar ndryshimin e stilit të ndërfaqes.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3614,11 +3614,11 @@ nga fjalorët Stardict, Babylon dhe GLS</translation>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Stili i ndërfaqes:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Krijo indeksin e tekstit të plotë me fije paralele </translation>
     </message>
   </context>
   <context>
