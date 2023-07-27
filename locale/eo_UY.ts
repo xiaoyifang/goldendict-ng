0b415cb2eb5625f2a85cc75d9ec069a73486db45
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-07-27 09:56:30 +0800

    New translations Esperanto from Crowdin

--- locale/eo_UY.ts
+++ locale/eo_UY.ts
@@ -652,11 +652,11 @@ inter klasika kaj lerneja ortografio en cirila)</translation>
     </message>
     <message>
       <source>Full-text search enabled</source>
-      <translation type="unfinished">Full-text search enabled</translation>
+      <translation>Plenteksta serĉo ebligita</translation>
     </message>
     <message>
       <source>Full-text search disabled</source>
-      <translation type="unfinished">Full-text search disabled</translation>
+      <translation>Plenteksta serĉo malŝaltita</translation>
     </message>
     <message>
       <source>Edit the dictionary via command:
@@ -3578,11 +3578,11 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     </message>
     <message>
       <source>Restart needed</source>
-      <translation type="unfinished">Restart needed</translation>
+      <translation>Rekomenco necesas</translation>
     </message>
     <message>
       <source>Restart to apply the interface style change.</source>
-      <translation type="unfinished">Restart to apply the interface style change.</translation>
+      <translation>Rekomencu por apliki la interfacan stilŝanĝon.</translation>
     </message>
     <message>
       <source>Positional information is required to use Xapian&apos;s phrase searching and NEAR operator, but the database size will be much bigger. Applies only to new incoming dictionaries.</source>
@@ -3622,11 +3622,11 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     </message>
     <message>
       <source>Interface Style:</source>
-      <translation type="unfinished">Interface Style:</translation>
+      <translation>Interfaca Stilo:</translation>
     </message>
     <message>
       <source>Create fulltext index with parallel threads </source>
-      <translation type="unfinished">Create fulltext index with parallel threads </translation>
+      <translation>Kreu plentekstan indekson kun paralelaj fadenoj </translation>
     </message>
   </context>
   <context>
