091f758bd714f1d8b9b4751f398cc71e0eee9738
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-01 21:00:55 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -1200,7 +1200,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Loading...</source>
-      <translation type="unfinished">Loading...</translation>
+      <translation>بارگذاری...</translation>
     </message>
   </context>
   <context>
@@ -3627,7 +3627,7 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>When using clipboard, strip everything after newline</source>
-      <translation type="unfinished">When using clipboard, strip everything after newline</translation>
+      <translation>هنگام استفاده از کلیپ بورد، همه چیز را بعد از خط جدید پاک کنید</translation>
     </message>
   </context>
   <context>
