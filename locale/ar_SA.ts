9bff2d612f44cd0742613cc6d857c671844662aa
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-01 20:57:24 +0800

    New translations Arabic, Saudi Arabia from Crowdin

--- locale/ar_SA.ts
+++ locale/ar_SA.ts
@@ -1197,6 +1197,10 @@ between classic and school orthography in cyrillic)</source>
       <source>Indexing...</source>
       <translation type="unfinished">Indexing...</translation>
     </message>
+    <message>
+      <source>Loading...</source>
+      <translation type="unfinished">Loading...</translation>
+    </message>
   </context>
   <context>
     <name>Language</name>
@@ -3414,10 +3418,6 @@ clears its network cache from disk during exit.</source>
       <translation>انهيار الجلسة</translation>
     </message>
     <message>
-      <source>When using clipboard,strip everything after newline</source>
-      <translation>عند استخدام الحافظة ، اخلع كل شيء بعد سطر جديد</translation>
-    </message>
-    <message>
       <source>On a new search, focus the main or popup window even if it&apos;s visible</source>
       <translation>في بحث جديد ، ركز على النافذة الرئيسية أو النافذة المنبثقة حتى لو كانت مرئية</translation>
     </message>
@@ -3624,6 +3624,10 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Create fulltext index with parallel threads </source>
       <translation type="unfinished">Create fulltext index with parallel threads </translation>
+    </message>
+    <message>
+      <source>When using clipboard, strip everything after newline</source>
+      <translation type="unfinished">When using clipboard, strip everything after newline</translation>
     </message>
   </context>
   <context>
