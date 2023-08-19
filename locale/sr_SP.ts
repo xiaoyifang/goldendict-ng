a772fed5d4069610c003658b375a28758fbb650e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-19 13:39:52 +0800

    New translations Serbian (Cyrillic) from Crowdin

--- locale/sr_SP.ts
+++ locale/sr_SP.ts
@@ -3636,11 +3636,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Remove invalid index on exit</source>
-      <translation type="unfinished">Remove invalid index on exit</translation>
+      <translation>Уклоните неважећи индекс на излазу</translation>
     </message>
     <message>
       <source>Miscellaneous</source>
-      <translation type="unfinished">Miscellaneous</translation>
+      <translation>Остало</translation>
     </message>
   </context>
   <context>
