b9c24626b4d323b0fed5377d171895adf6877c12
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-19 12:16:36 +0800

    New translations Turkmen from Crowdin

--- locale/tk_TM.ts
+++ locale/tk_TM.ts
@@ -3631,6 +3631,14 @@ arkaly goşmaça makalalary gözlemek üçin bu opsiýany açyň</translation>
       <source>When using clipboard, strip everything after newline</source>
       <translation>Paneli ulananyňyzda, täze setirden soň hemme zady aýyryň</translation>
     </message>
+    <message>
+      <source>Remove invalid index on exit</source>
+      <translation type="unfinished">Remove invalid index on exit</translation>
+    </message>
+    <message>
+      <source>Miscellaneous</source>
+      <translation type="unfinished">Miscellaneous</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
