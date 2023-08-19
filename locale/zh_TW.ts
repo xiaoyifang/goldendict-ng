0b0bf8f9acd9fa8cc7c87cdb73b57a6fae3357dc
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-19 13:39:58 +0800

    New translations Chinese Traditional from Crowdin

--- locale/zh_TW.ts
+++ locale/zh_TW.ts
@@ -3625,11 +3625,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Remove invalid index on exit</source>
-      <translation type="unfinished">Remove invalid index on exit</translation>
+      <translation>退出時刪除無效索引</translation>
     </message>
     <message>
       <source>Miscellaneous</source>
-      <translation type="unfinished">Miscellaneous</translation>
+      <translation>各種各樣的</translation>
     </message>
   </context>
   <context>
