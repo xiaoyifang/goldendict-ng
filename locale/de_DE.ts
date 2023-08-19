30472956b7a697594f8a1c02db70b183db9f40a6
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-19 13:39:35 +0800

    New translations German from Crowdin

--- locale/de_DE.ts
+++ locale/de_DE.ts
@@ -3629,11 +3629,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Remove invalid index on exit</source>
-      <translation type="unfinished">Remove invalid index on exit</translation>
+      <translation>Entfernen Sie den ungültigen Index beim Beenden</translation>
     </message>
     <message>
       <source>Miscellaneous</source>
-      <translation type="unfinished">Miscellaneous</translation>
+      <translation>Verschiedenes</translation>
     </message>
   </context>
   <context>
