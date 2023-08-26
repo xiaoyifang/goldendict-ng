65892c5a0adddfbf2ed8b8a55b1c10bca576441c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-26 21:35:35 +0800

    New translations Slovak from Crowdin

--- locale/sk_SK.ts
+++ locale/sk_SK.ts
@@ -28,10 +28,6 @@
       <translation>Poďakovanie:</translation>
     </message>
     <message>
-      <source>(c) 2008-2013 Konstantin Isakov (ikm@goldendict.org)</source>
-      <translation>(c) 2008 – 2013 Konstantin Isakov (ikm@goldendict.org)</translation>
-    </message>
-    <message>
       <source>Based on Qt %1 (%2, %3)</source>
       <translation>Na základe Qt %1 (%2, %3)</translation>
     </message>
@@ -3628,6 +3624,14 @@ zo slovníkov Stardict, Babylon a GLS.</translation>
     <message>
       <source>Miscellaneous</source>
       <translation>Zmiešaný</translation>
+    </message>
+    <message>
+      <source>When debugging with dictionary css/js, disable certain cache to make it easier.</source>
+      <translation type="unfinished">When debugging with dictionary css/js, disable certain cache to make it easier.</translation>
+    </message>
+    <message>
+      <source>Dictionary debug</source>
+      <translation type="unfinished">Dictionary debug</translation>
     </message>
   </context>
   <context>
