a894bb28d4b35bff910d2f62598a7588a3087fd5
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-26 21:35:26 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -28,10 +28,6 @@
       <translation>クレジット:</translation>
     </message>
     <message>
-      <source>(c) 2008-2013 Konstantin Isakov (ikm@goldendict.org)</source>
-      <translation>(c) 2008-2013 Konstantin Isakov (ikm@goldendict.org)</translation>
-    </message>
-    <message>
       <source>Based on Qt %1 (%2, %3)</source>
       <translation>Qt %1 (%2, %3) に基づく</translation>
     </message>
@@ -3634,6 +3630,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Miscellaneous</source>
       <translation>その他</translation>
+    </message>
+    <message>
+      <source>When debugging with dictionary css/js, disable certain cache to make it easier.</source>
+      <translation type="unfinished">When debugging with dictionary css/js, disable certain cache to make it easier.</translation>
+    </message>
+    <message>
+      <source>Dictionary debug</source>
+      <translation type="unfinished">Dictionary debug</translation>
     </message>
   </context>
   <context>
