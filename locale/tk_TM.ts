c46322656f770c64eee9d248f1090cef8e3478b4
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-08-26 21:36:00 +0800

    New translations Turkmen from Crowdin

--- locale/tk_TM.ts
+++ locale/tk_TM.ts
@@ -28,10 +28,6 @@
       <translation>Hormatlar:</translation>
     </message>
     <message>
-      <source>(c) 2008-2013 Konstantin Isakov (ikm@goldendict.org)</source>
-      <translation>(c) 2008-2011 Konstantin Isakow (ikm@goldendict.org)</translation>
-    </message>
-    <message>
       <source>Based on Qt %1 (%2, %3)</source>
       <translation>Qt %1 (%2, %3) esasynda</translation>
     </message>
@@ -3634,6 +3630,14 @@ arkaly goşmaça makalalary gözlemek üçin bu opsiýany açyň</translation>
     <message>
       <source>Miscellaneous</source>
       <translation>Dürli</translation>
+    </message>
+    <message>
+      <source>When debugging with dictionary css/js, disable certain cache to make it easier.</source>
+      <translation type="unfinished">When debugging with dictionary css/js, disable certain cache to make it easier.</translation>
+    </message>
+    <message>
+      <source>Dictionary debug</source>
+      <translation type="unfinished">Dictionary debug</translation>
     </message>
   </context>
   <context>
