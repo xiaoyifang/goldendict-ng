cb33a198436a75c86c17c7beb7df11b92d7324b5
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 17:05:06 +0800

    New translations Albanian from Crowdin

--- locale/sq_AL.ts
+++ locale/sq_AL.ts
@@ -3630,11 +3630,11 @@ nga fjalorët Stardict, Babylon dhe GLS</translation>
     </message>
     <message>
       <source>Interface Font</source>
-      <translation type="unfinished">Interface Font</translation>
+      <translation>Fonti i ndërfaqes</translation>
     </message>
     <message>
       <source>Restart to apply the interface font change.</source>
-      <translation type="unfinished">Restart to apply the interface font change.</translation>
+      <translation>Rinisni për të aplikuar ndryshimin e fontit të ndërfaqes.</translation>
     </message>
   </context>
   <context>
