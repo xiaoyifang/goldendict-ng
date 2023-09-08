07ad00c243fab752fb00af73dde9dd64ee80e926
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 17:05:04 +0800

    New translations Russian from Crowdin

--- locale/ru_RU.ts
+++ locale/ru_RU.ts
@@ -3643,11 +3643,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Font</source>
-      <translation type="unfinished">Interface Font</translation>
+      <translation>Шрифт интерфейса</translation>
     </message>
     <message>
       <source>Restart to apply the interface font change.</source>
-      <translation type="unfinished">Restart to apply the interface font change.</translation>
+      <translation>Перезапустите, чтобы применить изменение шрифта интерфейса.</translation>
     </message>
   </context>
   <context>
