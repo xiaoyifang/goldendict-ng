9005b66192a2e2ebf12e9474e9ba0f0117a5af7a
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 17:05:17 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -3635,11 +3635,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Interface Font</source>
-      <translation type="unfinished">Interface Font</translation>
+      <translation>فونت رابط</translation>
     </message>
     <message>
       <source>Restart to apply the interface font change.</source>
-      <translation type="unfinished">Restart to apply the interface font change.</translation>
+      <translation>برای اعمال تغییر فونت رابط، راه اندازی مجدد کنید.</translation>
     </message>
   </context>
   <context>
