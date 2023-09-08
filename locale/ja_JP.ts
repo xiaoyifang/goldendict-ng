7071ad0bbc5b9814904123de61cf744d4142a30d
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 08:53:59 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -3068,10 +3068,6 @@ download page.</source>
       <translation>インターフェイス(&amp;I)</translation>
     </message>
     <message>
-      <source>Changing Language</source>
-      <translation>言語の変更</translation>
-    </message>
-    <message>
       <source>Ctrl key</source>
       <translation>Ctrl キー</translation>
     </message>
@@ -3638,6 +3634,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Dictionary debug</source>
       <translation>辞書デバッグ</translation>
+    </message>
+    <message>
+      <source>Interface Font</source>
+      <translation type="unfinished">Interface Font</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface font change.</source>
+      <translation type="unfinished">Restart to apply the interface font change.</translation>
     </message>
   </context>
   <context>
