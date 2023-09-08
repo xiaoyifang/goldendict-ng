17d8b446cc91328a8cfb1fd68e05029adf223e3a
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 17:05:01 +0800

    New translations Polish from Crowdin

--- locale/pl_PL.ts
+++ locale/pl_PL.ts
@@ -3637,11 +3637,11 @@ ze słowników Stardict, Babylon i GLS</translation>
     </message>
     <message>
       <source>Interface Font</source>
-      <translation type="unfinished">Interface Font</translation>
+      <translation>Czcionka interfejsu</translation>
     </message>
     <message>
       <source>Restart to apply the interface font change.</source>
-      <translation type="unfinished">Restart to apply the interface font change.</translation>
+      <translation>Uruchom ponownie, aby zastosować zmianę czcionki interfejsu.</translation>
     </message>
   </context>
   <context>
