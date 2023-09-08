f944faf74fdb4b84caecfb21991459242d4d8cfe
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 08:54:34 +0800

    New translations Turkmen from Crowdin

--- locale/tk_TM.ts
+++ locale/tk_TM.ts
@@ -3068,10 +3068,6 @@ göçürip alyş sahypasyny açmagy teklip edýär.</translation>
       <translation>&amp;Interfeýs</translation>
     </message>
     <message>
-      <source>Changing Language</source>
-      <translation>Dili çalşmak</translation>
-    </message>
-    <message>
       <source>Ctrl key</source>
       <translation>Ctrl düwmesi</translation>
     </message>
@@ -3638,6 +3634,14 @@ arkaly goşmaça makalalary gözlemek üçin bu opsiýany açyň</translation>
     <message>
       <source>Dictionary debug</source>
       <translation>Sözlük düzediş</translation>
+    </message>
+    <message>
+      <source>Interface Font</source>
+      <translation type="unfinished">Interface Font</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface font change.</source>
+      <translation type="unfinished">Restart to apply the interface font change.</translation>
     </message>
   </context>
   <context>
