a3163d4898fa893ceb944334fa3a29e893fbf503
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 08:53:56 +0800

    New translations Greek from Crowdin

--- locale/el_GR.ts
+++ locale/el_GR.ts
@@ -3068,10 +3068,6 @@ GoldenDict. Αν ναι, το πρόγραμμα θα ειδοποιεί το
       <translation>&amp;Εφαρμογή</translation>
     </message>
     <message>
-      <source>Changing Language</source>
-      <translation>Αλλαγή γλώσσας</translation>
-    </message>
-    <message>
       <source>Ctrl key</source>
       <translation>Πλήκτρο Ctrl</translation>
     </message>
@@ -3641,6 +3637,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Dictionary debug</source>
       <translation>Αποσφαλμάτωση λεξικού</translation>
+    </message>
+    <message>
+      <source>Interface Font</source>
+      <translation type="unfinished">Interface Font</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface font change.</source>
+      <translation type="unfinished">Restart to apply the interface font change.</translation>
     </message>
   </context>
   <context>
