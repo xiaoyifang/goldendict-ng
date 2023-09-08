826b8015e265674799c1641467643ad1426762cd
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 17:04:37 +0800

    New translations Spanish from Crowdin

--- locale/es_ES.ts
+++ locale/es_ES.ts
@@ -3635,11 +3635,11 @@ de diccionarios Stardict, Babylon y GLS</translation>
     </message>
     <message>
       <source>Interface Font</source>
-      <translation type="unfinished">Interface Font</translation>
+      <translation>Fuente de interfaz</translation>
     </message>
     <message>
       <source>Restart to apply the interface font change.</source>
-      <translation type="unfinished">Restart to apply the interface font change.</translation>
+      <translation>Reinicie para aplicar el cambio de fuente de la interfaz.</translation>
     </message>
   </context>
   <context>
