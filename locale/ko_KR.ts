c44a19d4ecae5c84e912b307d7817a1c115320ae
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-09-08 08:54:00 +0800

    New translations Korean from Crowdin

--- locale/ko_KR.ts
+++ locale/ko_KR.ts
@@ -3065,10 +3065,6 @@ download page.</source>
       <translation>화면(&amp;I)</translation>
     </message>
     <message>
-      <source>Changing Language</source>
-      <translation>언어 변경</translation>
-    </message>
-    <message>
       <source>Ctrl key</source>
       <translation>Ctrl 키</translation>
     </message>
@@ -3635,6 +3631,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Dictionary debug</source>
       <translation>사전 디버그</translation>
+    </message>
+    <message>
+      <source>Interface Font</source>
+      <translation type="unfinished">Interface Font</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface font change.</source>
+      <translation type="unfinished">Restart to apply the interface font change.</translation>
     </message>
   </context>
   <context>
