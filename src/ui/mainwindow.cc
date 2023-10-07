b754a24c6c253c296cba2afbf353d214966ebebe
YiFang Xiao <yifang.xiao@foxmail.com>
2023-10-07 21:36:32 +0800

    refactor: reduce nested loop depth

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -1224,16 +1224,16 @@ void MainWindow::commitData()
     for ( auto & file : entries ) {
       QString const fileName = file.fileName();
 
+      if ( dictMap.contains( fileName.toStdString() ) )
+        continue;
       //remove both normal index and fts index.
-      if ( !dictMap.contains( fileName.toStdString() ) ) {
-        auto filePath = file.absoluteFilePath();
-        qDebug() << "remove invalid index files & fts dirs";
+      auto filePath = file.absoluteFilePath();
+      qDebug() << "remove invalid index files & fts dirs";
 
-        QFile::remove( filePath );
-        QDir d( filePath + "_FTS_x" );
-        if ( d.exists() ) {
-          d.removeRecursively();
-        }
+      QFile::remove( filePath );
+      QDir d( filePath + "_FTS_x" );
+      if ( d.exists() ) {
+        d.removeRecursively();
       }
     }
   }
