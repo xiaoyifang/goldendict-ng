c8bb526c6e294856c4f8d58475e91dd4f8bc5f58
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-12-10 17:25:48 +0800

    New translations Swedish from Crowdin

--- locale/sv_SE.ts
+++ locale/sv_SE.ts
@@ -204,10 +204,6 @@
       <translation>Text till tal-modul</translation>
     </message>
     <message>
-      <source>Picture</source>
-      <translation>Bild</translation>
-    </message>
-    <message>
       <source>Video</source>
       <translatorcomment>视频: %1</translatorcomment>
       <translation>Video</translation>
@@ -299,6 +295,10 @@
     <message>
       <source>&amp;Create Anki note</source>
       <translation>&amp;Skapa Anki anteckning</translation>
+    </message>
+    <message>
+      <source>Open image in system viewer...</source>
+      <translation type="unfinished">Open image in system viewer...</translation>
     </message>
   </context>
   <context>
@@ -3629,14 +3629,6 @@ från Stardict, Babylon och GLS ordböcker</translation>
     <message>
       <source>Miscellaneous</source>
       <translation>Diverse</translation>
-    </message>
-    <message>
-      <source>When debugging with dictionary css/js, disable certain cache to make it easier.</source>
-      <translation>Vid felsökning med ordboken css/js, inaktivera vissa cache för att underlätta det.</translation>
-    </message>
-    <message>
-      <source>Dictionary debug</source>
-      <translation>Ordbok debug</translation>
     </message>
     <message>
       <source>Interface Font</source>
