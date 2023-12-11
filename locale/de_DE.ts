3ef65569b547fd1ebab8892366115168f35a2c6f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-12-11 10:29:19 +0800

    New translations German from Crowdin

--- locale/de_DE.ts
+++ locale/de_DE.ts
@@ -298,7 +298,7 @@
     </message>
     <message>
       <source>Open image in system viewer...</source>
-      <translation type="unfinished">Open image in system viewer...</translation>
+      <translation>Bild im System Viewer öffnen...</translation>
     </message>
   </context>
   <context>
