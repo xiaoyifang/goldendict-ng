d7f64ab4dc899fc612eb44bf7c7f673279bbb33b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-12-11 10:29:16 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -298,7 +298,7 @@
     </message>
     <message>
       <source>Open image in system viewer...</source>
-      <translation type="unfinished">Open image in system viewer...</translation>
+      <translation>Адкрыць выяву ў сістэмным аглядзе...</translation>
     </message>
   </context>
   <context>
