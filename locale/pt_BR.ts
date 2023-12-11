54fccf44854aacca3df678584436425158319771
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-12-11 10:29:43 +0800

    New translations Portuguese, Brazilian from Crowdin

--- locale/pt_BR.ts
+++ locale/pt_BR.ts
@@ -298,7 +298,7 @@
     </message>
     <message>
       <source>Open image in system viewer...</source>
-      <translation type="unfinished">Open image in system viewer...</translation>
+      <translation>Abrir imagem no visualizador do sistema...</translation>
     </message>
   </context>
   <context>
