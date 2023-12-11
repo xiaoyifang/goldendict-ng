d6d9b7d8a1beeb6b56db100f9675357847adee0a
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2023-12-11 10:29:46 +0800

    New translations Hindi from Crowdin

--- locale/hi_IN.ts
+++ locale/hi_IN.ts
@@ -298,7 +298,7 @@
     </message>
     <message>
       <source>Open image in system viewer...</source>
-      <translation type="unfinished">Open image in system viewer...</translation>
+      <translation>सिस्टम व्यूअर में छवि खोलें...</translation>
     </message>
   </context>
   <context>
