b773feffad3d43cfb245e31b11128f16c0a60710
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-01-26 08:10:46 +0800

    New translations Quechua from Crowdin

--- locale/qu_PE.ts
+++ locale/qu_PE.ts
@@ -2756,6 +2756,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
       <source>Icon</source>
       <translation>Icon</translation>
     </message>
+    <message>
+      <source>Language Variant</source>
+      <translation type="unfinished">Language Variant</translation>
+    </message>
   </context>
   <context>
     <name>MultimediaAudioPlayer</name>
