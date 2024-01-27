4637169193c31805c95bfa378525329d39dedb21
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-01-27 12:32:31 +0800

    New translations Turkmen from Crowdin

--- locale/tk_TM.ts
+++ locale/tk_TM.ts
@@ -2757,7 +2757,7 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Language Variant</source>
-      <translation type="unfinished">Language Variant</translation>
+      <translation>Dil görnüşi</translation>
     </message>
   </context>
   <context>
