956af1374fd1a7b845e9069d212e8f1aff7dc505
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-01-28 10:38:49 +0800

    New Crowdin updates (#1388)
    * New translations German, Switzerland from Crowdin
    
    * New translations German, Switzerland from Crowdin

--- locale/zh_CN.ts
+++ locale/zh_CN.ts
@@ -2757,7 +2757,7 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Language Variant</source>
-      <translation type="unfinished">Language Variant</translation>
+      <translation>语言变体</translation>
     </message>
   </context>
   <context>
