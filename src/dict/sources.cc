be22cb22b60b023394fa22a327a0fe9d75f2513a
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-03-20 22:07:58 +0800

    fix: in portable version,the default `content` dir lost recursive attribute. (#1430)
    * fix: allow edit the content path
    
    * fix: portable version ,add default item
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/dict/sources.cc
+++ src/dict/sources.cc
@@ -1115,13 +1115,6 @@ Qt::ItemFlags PathsModel::flags( QModelIndex const & index ) const
 {
   Qt::ItemFlags result = QAbstractItemModel::flags( index );
 
-  if ( Config::isPortableVersion() ) {
-    if ( index.isValid() && index.row() == 0 ) {
-      result &= ~Qt::ItemIsSelectable;
-      result &= ~Qt::ItemIsEnabled;
-    }
-  }
-
   if ( index.isValid() && index.column() == 1 )
     result |= Qt::ItemIsUserCheckable;
 
