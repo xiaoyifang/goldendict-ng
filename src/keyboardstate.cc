06137a48ae5b06a093a8fa708aac7ea7a2841633
Xiao Yi Fang <yifang.xiao@foxmail.com>
2024-06-25 08:28:04 +0800

    opt: remove Left|Right Alt|Shift modifier check

--- src/keyboardstate.cc
+++ src/keyboardstate.cc
@@ -12,10 +12,5 @@ bool KeyboardState::checkModifiersPressed( int mask )
   return !( ( mask & Alt && !( modifiers.testFlag( Qt::AltModifier ) ) )
             || ( mask & Ctrl && !( modifiers.testFlag( Qt::ControlModifier ) ) )
             || ( mask & Shift && !( modifiers.testFlag( Qt::ShiftModifier ) ) )
-            || ( mask & LeftAlt && !( modifiers.testFlag( Qt::AltModifier ) ) )
-            || ( mask & RightAlt && !( modifiers.testFlag( Qt::AltModifier ) ) )
-            || ( mask & LeftCtrl && !( modifiers.testFlag( Qt::ControlModifier ) ) )
-            || ( mask & RightCtrl && !( modifiers.testFlag( Qt::ControlModifier ) ) )
-            || ( mask & LeftShift && !( modifiers.testFlag( Qt::ShiftModifier ) ) )
-            || ( mask & RightShift && !( modifiers.testFlag( Qt::ShiftModifier ) ) ) );
+             );
 }
