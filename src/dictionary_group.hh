7c9c50aad17c1983b14d6f2b4f923db11a45db3b
YiFang Xiao <yifang.xiao@foxmail.com>
2024-07-01 23:18:45 +0800

    opt: dictionary group refactor

--- src/dictionary_group.hh
+++ src/dictionary_group.hh
@@ -1,5 +1,4 @@
-/* This file is (c) 2018 Igor Kushnir <igorkuo@gmail.com>
- * Part of GoldenDict. Licensed under GPLv3 or later, see the LICENSE file */
+/* Licensed under GPLv3 or later, see the LICENSE file */
 
 #ifndef DICTIONARYGROUP_HH_INCLUDED
 #define DICTIONARYGROUP_HH_INCLUDED
