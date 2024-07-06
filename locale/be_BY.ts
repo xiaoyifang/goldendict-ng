338a120ceb2858d7ba62c49966d583413faa119c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-07-06 18:31:33 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -581,19 +581,19 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Specify the maximum filtered headwords returned.</source>
-      <translation type="unfinished">Specify the maximum filtered headwords returned.</translation>
+      <translation>Укажыце максімальную колькасць вернутых адфільтраваных загалоўных слоў.</translation>
     </message>
     <message>
       <source>Filter max results:</source>
-      <translation type="unfinished">Filter max results:</translation>
+      <translation>Максімальныя вынікі фільтра:</translation>
     </message>
     <message>
       <source>Unique headwords total: %1.</source>
-      <translation type="unfinished">Unique headwords total: %1.</translation>
+      <translation>Усяго унікальных загалоўных слоў: %1.</translation>
     </message>
     <message>
       <source>Unique headwords total: %1, filtered(limited): %2</source>
-      <translation type="unfinished">Unique headwords total: %1, filtered(limited): %2</translation>
+      <translation>Усяго унікальных загалоўных слоў: %1, адфільтравана (абмежавана): %2</translation>
     </message>
   </context>
   <context>
