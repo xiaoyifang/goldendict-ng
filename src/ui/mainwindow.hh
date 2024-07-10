0c4483ae02565251d36f43c5a64a47dc68a1ade1
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-07-10 16:28:28 +0800

    Revert "opt: remove wasMaximized logic of mainwindow" (#1664)
    * Revert "opt: remove wasMaximized logic of mainwindow"
    
    This reverts commit 99c221c8549c131afc7526bf8391ecab46a82276.
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/ui/mainwindow.hh
+++ src/ui/mainwindow.hh
@@ -158,6 +158,8 @@ private:
 
   bool wordListSelChanged;
 
+  bool wasMaximized; // Window state before minimization
+
   QPrinter & getPrinter(); // Creates a printer if it's not there and returns it
 
   DictHeadwords * headwordsDlg;
