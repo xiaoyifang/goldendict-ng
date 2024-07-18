24ffcd804af43967d2ca1839e06228535207d8da
Xiao Yi Fang <yifang.xiao@foxmail.com>
2024-07-18 11:36:16 +0800

    opt: remove the existed highlight when no match

--- src/ui/articleview.cc
+++ src/ui/articleview.cc
@@ -2026,13 +2026,13 @@ void ArticleView::performFindOperation( bool backwards )
     f |= QWebEnginePage::FindBackward;
 
   findText( text, f, [ text, this ]( bool match ) {
-    bool setMark = !text.isEmpty() && !match;
-    if ( setMark ) {
+    bool nomatch = !text.isEmpty() && !match;
+    if ( nomatch ) {
       //clear the previous findText results.
       //when the results is empty, the highlight has not been removed.more likely a qt bug.
       webview->findText( "" );
     }
-    Utils::Widget::setNoResultColor( searchPanel->lineEdit, setMark );
+    Utils::Widget::setNoResultColor( searchPanel->lineEdit, nomatch );
   } );
 }
 
