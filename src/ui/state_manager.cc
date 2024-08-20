858fc8d1772999a3ca75c43f0f9540d81eb36267
Xiao Yi Fang <yifang.xiao@foxmail.com>
2024-08-20 11:51:03 +0800

    opt: save state when resize

--- src/ui/state_manager.cc
+++ src/ui/state_manager.cc
@@ -15,16 +15,19 @@ StateManager::StateManager( QObject * parent, Config::Class & cfg ):
 
 void StateManager::setState( const QByteArray & state, const QByteArray & geometry )
 {
-  if ( _dirty )
-    return;
-
   if ( state == _state && _geometry == geometry ) {
     return;
   }
+  _state    = state;
+  _geometry = geometry;
+  //in the save state process already.
+  if ( _dirty )
+    return;
   _dirty = true;
+
   //delay execution
-  QTimer::singleShot( 3000, this, [ = ]() {
-    saveConfigData( state, geometry );
+  QTimer::singleShot( 3000, this, [ &_state, &_geometry, this ]() {
+    saveConfigData( _state, _geometry );
 
     _dirty = false;
   } );
@@ -34,19 +37,10 @@ void StateManager::setState( const QByteArray & state, const QByteArray & geomet
 void StateManager::saveConfigData( QByteArray state, QByteArray geometry )
 {
   try {
-    // Save MainWindow state and geometry
-    _cfg.mainWindowState    = state;
-    _cfg.mainWindowGeometry = geometry;
-
-    // Config::save( _cfg );
-
     _settings.beginGroup( "mainwindow" );
     _settings.setValue( "state", state );
     _settings.setValue( "geometry", geometry );
     _settings.endGroup();
-
-    _state    = state;
-    _geometry = geometry;
   }
   catch ( std::exception & e ) {
     qWarning() << "state manager save config data failed: " << e.what();
