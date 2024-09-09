bb32f102419441e70de8276749eb2adb1516678f
Konstantin <alexandroviski8299@gmail.com>
2024-09-09 03:56:41 +0000

    Update dictionary.cc

--- src/dict/dictionary.cc
+++ src/dict/dictionary.cc
@@ -271,8 +271,6 @@ bool Class::loadIconFromFile( QString const & _filename, bool isFullName )
     if ( !img.isNull() ) {
       // Load successful
 
-      //some icon is very large ,will crash the application.
-      img = img.scaledToWidth( 64 );
       // Apply the color key
 #if ( QT_VERSION >= QT_VERSION_CHECK( 6, 0, 0 ) )
       img.setAlphaChannel( img.createMaskFromColor( QColor( 192, 192, 192 ).rgb(), Qt::MaskOutColor ) );
