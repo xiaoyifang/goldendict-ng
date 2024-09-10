48feac146969d683802234cf6a64f5eb232a383e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-09-11 04:46:17 +0800

    New translations Hungarian from Crowdin

--- locale/hu_HU.ts
+++ locale/hu_HU.ts
@@ -824,7 +824,7 @@ a klasszikus és az iskolai helyesírás közt cirillben)</translation>
     </message>
     <message>
       <source>Support xapian search syntax, such as AND OR +/- etc.</source>
-      <translation type="unfinished">Support xapian search syntax, such as AND OR +/- etc.</translation>
+      <translation>Xapian keresések támogatása, mint például AND OR +/- stb.</translation>
     </message>
     <message>
       <source>Articles found: </source>
@@ -2157,7 +2157,7 @@ a klasszikus és az iskolai helyesírás közt cirillben)</translation>
     </message>
     <message>
       <source>Hungarian</source>
-      <translation type="unfinished">Hungarian</translation>
+      <translation>Magyar</translation>
     </message>
     <message>
       <source>English</source>
@@ -3816,7 +3816,7 @@ could be resized or managed in other ways.</source>
     </message>
     <message>
       <source>%1 - GoldenDict-ng</source>
-      <translation type="unfinished">%1 - GoldenDict-ng</translation>
+      <translation>%1 - GoldenDict-ng</translation>
     </message>
     <message>
       <source>WARNING: %1</source>
