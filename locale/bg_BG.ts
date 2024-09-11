fbb7f61ca6a0560707fc2bfc6d329800beb3406b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-09-11 10:14:42 +0800

    New translations Bulgarian from Crowdin

--- locale/bg_BG.ts
+++ locale/bg_BG.ts
@@ -824,7 +824,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Support xapian search syntax, such as AND OR +/- etc.</source>
-      <translation type="unfinished">Support xapian search syntax, such as AND OR +/- etc.</translation>
+      <translation>Поддържайте xapian синтаксис за търсене, като И ИЛИ +/- и т.н.</translation>
     </message>
     <message>
       <source>Articles found: </source>
@@ -2157,7 +2157,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Hungarian</source>
-      <translation type="unfinished">Hungarian</translation>
+      <translation>унгарски</translation>
     </message>
     <message>
       <source>English</source>
@@ -3816,7 +3816,7 @@ could be resized or managed in other ways.</source>
     </message>
     <message>
       <source>%1 - GoldenDict-ng</source>
-      <translation type="unfinished">%1 - GoldenDict-ng</translation>
+      <translation>%1 - GoldenDict-ng</translation>
     </message>
     <message>
       <source>WARNING: %1</source>
