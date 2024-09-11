60fdd063a0af46019b0c5166bff70f73b27491b7
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-09-11 10:15:06 +0800

    New translations Swedish from Crowdin

--- locale/sv_SE.ts
+++ locale/sv_SE.ts
@@ -824,7 +824,7 @@ klassisk rättstavning och skolrättstavning i kyrillisk skrift)</translation>
     </message>
     <message>
       <source>Support xapian search syntax, such as AND OR +/- etc.</source>
-      <translation type="unfinished">Support xapian search syntax, such as AND OR +/- etc.</translation>
+      <translation>Stöd xapian söksyntax, såsom AND OR +/- etc.</translation>
     </message>
     <message>
       <source>Articles found: </source>
@@ -2157,7 +2157,7 @@ klassisk rättstavning och skolrättstavning i kyrillisk skrift)</translation>
     </message>
     <message>
       <source>Hungarian</source>
-      <translation type="unfinished">Hungarian</translation>
+      <translation>ungerska</translation>
     </message>
     <message>
       <source>English</source>
@@ -3819,7 +3819,7 @@ Rutan kan storleksändras och hanteras på andra sätt.</translation>
     </message>
     <message>
       <source>%1 - GoldenDict-ng</source>
-      <translation type="unfinished">%1 - GoldenDict-ng</translation>
+      <translation>%1 - GoldenDict-ng</translation>
     </message>
     <message>
       <source>WARNING: %1</source>
