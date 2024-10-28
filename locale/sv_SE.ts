900fbd121e88e97352e51cb0af9e27a66afaa9cd
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-10-29 01:06:14 +0800

    New translations Swedish from Crowdin

--- locale/sv_SE.ts
+++ locale/sv_SE.ts
@@ -2686,19 +2686,19 @@ För att hitta &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbol
     </message>
     <message>
       <source>Show &amp;Small Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Small Icons in Toolbars</translation>
+      <translation>Visa &amp;små ikoner i verktygsfält</translation>
     </message>
     <message>
       <source>Show &amp;Large Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Large Icons in Toolbars</translation>
+      <translation>Visa &amp;stora ikoner i verktygsfält</translation>
     </message>
     <message>
       <source>Show &amp;Normal Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Normal Icons in Toolbars</translation>
+      <translation>Visa &amp;normala ikoner i verktygsfält</translation>
     </message>
     <message>
       <source>Toggle clipboard monitoring</source>
-      <translation type="unfinished">Toggle clipboard monitoring</translation>
+      <translation>Växla urklippsövervakning</translation>
     </message>
   </context>
   <context>
@@ -3550,15 +3550,15 @@ från Stardict, Babylon och GLS ordböcker</translation>
     </message>
     <message>
       <source>Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</source>
-      <translation type="unfinished">Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</translation>
+      <translation>Väljer om urklippsövervakningen ska aktiveras som standard vid uppstart av programmet&apos;.</translation>
     </message>
     <message>
       <source>Start with clipboard monitoring turned on</source>
-      <translation type="unfinished">Start with clipboard monitoring turned on</translation>
+      <translation>Börja med urklippsövervakning påslagen</translation>
     </message>
     <message>
       <source>Auto-pronounce words in the popup</source>
-      <translation type="unfinished">Auto-pronounce words in the popup</translation>
+      <translation>Uttala ord automatiskt i popup-fönstret</translation>
     </message>
   </context>
   <context>
