3e4355efa1af1a2e2c59374142241f06c91a1435
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-10-29 01:06:15 +0800

    New translations Turkish from Crowdin

--- locale/tr_TR.ts
+++ locale/tr_TR.ts
@@ -2686,19 +2686,19 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Show &amp;Small Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Small Icons in Toolbars</translation>
+      <translation>Araç Çubuklarında &amp;Küçük Simgeleri Göster</translation>
     </message>
     <message>
       <source>Show &amp;Large Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Large Icons in Toolbars</translation>
+      <translation>Araç Çubuklarında &amp;Büyük Simgeleri Göster</translation>
     </message>
     <message>
       <source>Show &amp;Normal Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Normal Icons in Toolbars</translation>
+      <translation>&amp;Normal Simgeleri Araç Çubuklarında Göster</translation>
     </message>
     <message>
       <source>Toggle clipboard monitoring</source>
-      <translation type="unfinished">Toggle clipboard monitoring</translation>
+      <translation>Pano izlemeyi aç/kapat</translation>
     </message>
   </context>
   <context>
@@ -3543,15 +3543,15 @@ eşanlamlı listeleri aracılığıyla ekstra makale aramasını etkinleştirmek
     </message>
     <message>
       <source>Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</source>
-      <translation type="unfinished">Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</translation>
+      <translation>Program&apos;başlatıldığında pano izleme özelliğinin varsayılan olarak açılıp açılmayacağını seçer.</translation>
     </message>
     <message>
       <source>Start with clipboard monitoring turned on</source>
-      <translation type="unfinished">Start with clipboard monitoring turned on</translation>
+      <translation>Pano izlemeyi açarak başlayın</translation>
     </message>
     <message>
       <source>Auto-pronounce words in the popup</source>
-      <translation type="unfinished">Auto-pronounce words in the popup</translation>
+      <translation>Açılır pencerede kelimeleri otomatik telaffuz et</translation>
     </message>
   </context>
   <context>
