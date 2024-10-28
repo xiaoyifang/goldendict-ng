eddd3b3764fb900405e086712db13e063902483f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-10-29 01:05:57 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -2686,19 +2686,19 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Show &amp;Small Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Small Icons in Toolbars</translation>
+      <translation>ツールバーに小さいアイコンを表示する(&amp;S)</translation>
     </message>
     <message>
       <source>Show &amp;Large Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Large Icons in Toolbars</translation>
+      <translation>ツールバーに大きいアイコンを表示する(&amp;L)</translation>
     </message>
     <message>
       <source>Show &amp;Normal Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Normal Icons in Toolbars</translation>
+      <translation>ツールバーに通常アイコンを表示する(&amp;N)</translation>
     </message>
     <message>
       <source>Toggle clipboard monitoring</source>
-      <translation type="unfinished">Toggle clipboard monitoring</translation>
+      <translation>クリップボード監視の切り替え</translation>
     </message>
   </context>
   <context>
@@ -3551,11 +3551,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Start with clipboard monitoring turned on</source>
-      <translation type="unfinished">Start with clipboard monitoring turned on</translation>
+      <translation>クリップボード監視をオンにして開始します</translation>
     </message>
     <message>
       <source>Auto-pronounce words in the popup</source>
-      <translation type="unfinished">Auto-pronounce words in the popup</translation>
+      <translation>ポップアップで単語を自動発音する</translation>
     </message>
   </context>
   <context>
