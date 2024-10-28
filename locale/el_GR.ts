c9fb3162fdd3b058870c4bd5b34dd29750c5d1ab
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2024-10-29 01:05:51 +0800

    New translations Greek from Crowdin

--- locale/el_GR.ts
+++ locale/el_GR.ts
@@ -2688,19 +2688,19 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Show &amp;Small Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Small Icons in Toolbars</translation>
+      <translation>Εμφάνιση &amp;μικρών εικονιδίων στις γραμμές εργαλείων</translation>
     </message>
     <message>
       <source>Show &amp;Large Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Large Icons in Toolbars</translation>
+      <translation>Εμφάνιση &amp;μεγάλων εικονιδίων στις γραμμές εργαλείων</translation>
     </message>
     <message>
       <source>Show &amp;Normal Icons in Toolbars</source>
-      <translation type="unfinished">Show &amp;Normal Icons in Toolbars</translation>
+      <translation>Εμφάνιση &amp;Κανονικών εικονιδίων στις γραμμές εργαλείων</translation>
     </message>
     <message>
       <source>Toggle clipboard monitoring</source>
-      <translation type="unfinished">Toggle clipboard monitoring</translation>
+      <translation>Εναλλαγή παρακολούθησης προχείρου</translation>
     </message>
   </context>
   <context>
@@ -3550,15 +3550,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</source>
-      <translation type="unfinished">Chooses whether the clipboard monitoring will be turned on by default at the program&apos;s startup.</translation>
+      <translation>Επιλέγει εάν η παρακολούθηση του προχείρου θα ενεργοποιείται από προεπιλογή κατά την εκκίνηση του προγράμματος&apos;.</translation>
     </message>
     <message>
       <source>Start with clipboard monitoring turned on</source>
-      <translation type="unfinished">Start with clipboard monitoring turned on</translation>
+      <translation>Ξεκινήστε με ενεργοποιημένη την παρακολούθηση του προχείρου</translation>
     </message>
     <message>
       <source>Auto-pronounce words in the popup</source>
-      <translation type="unfinished">Auto-pronounce words in the popup</translation>
+      <translation>Αυτόματη προφορά λέξεων στο αναδυόμενο παράθυρο</translation>
     </message>
   </context>
   <context>
