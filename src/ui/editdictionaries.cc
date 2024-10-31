f1aa5551a1653bfc6772f262713ad1589c5c8ecb
YiFang Xiao <yifang.xiao@foxmail.com>
2024-10-31 12:50:37 +0800

    1

--- src/ui/editdictionaries.cc
+++ src/ui/editdictionaries.cc
@@ -240,14 +240,14 @@ void EditDictionaries::acceptChangedSources( bool rebuildGroups )
   Instances::updateNames( savedInactive, dictionaries );
 
   if ( rebuildGroups ) {
-    // ui.tabs->removeTab( 1 );
-    // ui.tabs->removeTab( 1 );
+    ui.tabs->removeTab( 1 );
+    ui.tabs->removeTab( 1 );
 
     orderAndProps = std::make_shared< OrderAndProps >( this, savedOrder, savedInactive, dictionaries );
     groups = std::make_shared< Groups >( this, dictionaries, savedGroups, orderAndProps->getCurrentDictionaryOrder() );
 
-    // ui.tabs->insertTab( 1, orderAndProps.get(), QIcon( ":/icons/book.svg" ), tr( "&Dictionaries" ) );
-    // ui.tabs->insertTab( 2, groups.get(), QIcon( ":/icons/bookcase.svg" ), tr( "&Groups" ) );
+    ui.tabs->insertTab( 1, orderAndProps.get(), QIcon( ":/icons/book.svg" ), tr( "&Dictionaries" ) );
+    ui.tabs->insertTab( 2, groups.get(), QIcon( ":/icons/bookcase.svg" ), tr( "&Groups" ) );
     connect( groups.get(), &Groups::showDictionaryInfo, this, &EditDictionaries::showDictionaryInfo );
     connect( orderAndProps.get(),
              &OrderAndProps::showDictionaryHeadwords,
