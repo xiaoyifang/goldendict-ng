de941ef9dbf26d47257ca03bd8acdfbbfdca19c7
shenleban tongying <shenlebantongying@gmail.com>
2024-11-11 23:10:53 -0500

    refactor: change `DEF_EX` macro's underlying implementation to template

--- src/common/ex.hh
+++ src/common/ex.hh
@@ -4,22 +4,17 @@
 #pragma once
 
 #include <string>
+#include <fmt/format.h>
+
+// clang-format off
 
 /// A way to declare an exception class fast
 /// Do like this:
 /// DEF_EX( exErrorInFoo, "An error in foo encountered", std::exception )
 /// DEF_EX( exFooNotFound, "Foo was not found", exErrorInFoo )
-
-#define DEF_EX( exName, exDescription, exParent ) \
-  class exName: public exParent                   \
-  {                                               \
-  public:                                         \
-    virtual const char * what() const noexcept    \
-    {                                             \
-      return ( exDescription );                   \
-    }                                             \
-    virtual ~exName() noexcept {}                 \
-  };
+#define DEF_EX( exName, exDescription, exParent )         \
+  constexpr static char ExStr_## exName[] = exDescription; \
+  using exName = defineEx< exParent, ExStr_## exName >;
 
 /// Same as DEF_EX, but takes a runtime string argument, which gets concatenated
 /// with the description.
@@ -29,20 +24,36 @@
 ///   throw exCantOpen( "example.txt" );
 ///
 ///   what() would return "can't open file example.txt"
+///
+#define DEF_EX_STR( exName, exDescription, exParent )     \
+  constexpr static char ExStr_## exName[] = exDescription; \
+  using exName = defineExStr< exParent, ExStr_## exName >;
 
-#define DEF_EX_STR( exName, exDescription, exParent )      \
-  class exName: public exParent                            \
-  {                                                        \
-    std::string value;                                     \
-                                                           \
-  public:                                                  \
-    explicit exName( std::string const & value_ ):         \
-      value( std::string( exDescription ) + " " + value_ ) \
-    {                                                      \
-    }                                                      \
-    virtual const char * what() const noexcept             \
-    {                                                      \
-      return value.c_str();                                \
-    }                                                      \
-    virtual ~exName() noexcept {}                          \
-  };
+// clang-format on
+
+template< typename ParentEx, const char * description >
+class defineEx: public ParentEx
+{
+public:
+  virtual const char * what() const noexcept
+  {
+    return description;
+  }
+};
+
+template< typename ParentEx, const char * description >
+class defineExStr: public ParentEx
+{
+public:
+  explicit defineExStr( std::string const & message_ ):
+    message( fmt::format( "{} {}", description, message_ ) )
+  {
+  }
+  virtual const char * what() const noexcept
+  {
+    return message.c_str();
+  }
+
+private:
+  std::string message;
+};
