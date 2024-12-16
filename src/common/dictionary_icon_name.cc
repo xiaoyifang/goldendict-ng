4c82be2a32b7b168f7057c9a13d022e9637109d5
YiFang Xiao <YiFang.xiao@foxmail.com>
2024-12-16 15:40:15 +0800

    opt: optimize the dictionary icon name generation

--- src/common/dictionary_icon_name.cc
+++ src/common/dictionary_icon_name.cc
@@ -3,23 +3,24 @@
 
 QString Icons::DictionaryIconName::getIconName( const QString & dictionaryName ) const
 {
-    if (dictionaryName.isEmpty()) {
-        return {};
-    }
-    std::lock_guard<std::mutex> lock(_mutex);
+  if ( dictionaryName.isEmpty() ) {
+    return {};
+  }
+  std::lock_guard< std::mutex > lock( _mutex );
 
-    auto it = _dictionaryIconNames.find(dictionaryName);
-    if (it != _dictionaryIconNames.end()) {
-        return it->second;
-    } else {
-        //get the first character of the dictionary name
-        QString name = dictionaryName.at(0).toUpper();
-        auto it1         = _iconDictionaryNames.find( name );
-        if (it1 != _iconDictionaryNames.end()) {
-            it1->second.push_back( dictionaryName );
-        }
-
-        name= name+ it1->second.size();
-        return _dictionaryIconNames[dictionaryName] = name;
+  auto it = _dictionaryIconNames.find( dictionaryName );
+  if ( it != _dictionaryIconNames.end() ) {
+    return it->second;
+  }
+  else {
+    //get the first character of the dictionary name
+    QString name = dictionaryName.at( 0 ).toUpper();
+    auto it1     = _iconDictionaryNames.find( name );
+    if ( it1 != _iconDictionaryNames.end() ) {
+      it1->second.push_back( dictionaryName );
     }
+
+    name                                          = name + it1->second.size();
+    return _dictionaryIconNames[ dictionaryName ] = name;
+  }
 }
