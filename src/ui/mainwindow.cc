2b8609547782159aaf59fa6fbc2758aa3ceebcea
YiFang Xiao <yifang.xiao@foxmail.com>
2024-12-18 20:59:03 +0800

    opt: add mhtml

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -3351,8 +3351,8 @@ void MainWindow::on_saveArticle_triggered()
   QFileDialog::Options options = QFileDialog::HideNameFilterDetails;
   QString selectedFilter;
   QStringList filters;
-  filters.push_back( tr( "Article, Complete (*.html *.htm)" ) );
-  filters.push_back( tr( "Article, HTML Only (*.html *.htm)" ) );
+  filters.push_back( tr( "Complete Html (*.html *.htm)" ) );
+  filters.push_back( tr( "Single Html (*.html *.htm)" ) );
   filters.push_back( tr( "Pdf (*.pdf)" ) );
   filters.push_back( tr( "Mime Html (*.mhtml)" ) );
 
