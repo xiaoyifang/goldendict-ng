d4eb1c7c99b23c28e74d11a2b552cbdd3deb1144
YiFang Xiao <YiFang.xiao@foxmail.com>
2024-12-28 10:10:18 +0800

    1

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -3172,8 +3172,7 @@ void MainWindow::iconSizeActionTriggered( QAction * /*action*/ )
   translateBox->completerWidget()->setFont( font );
   groupList->setFont( font );
 
-  groupList->setFixedHeight(extent);
-  translateBox->setFixedHeight(extent);
+  translateBoxWidget->setFixedHeight(extent);
 }
 
 void MainWindow::toggleMenuBarTriggered( bool announce )
