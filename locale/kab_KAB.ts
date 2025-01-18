248c4b7434a0844258991f049fd2e6ec564622b8
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-18 21:48:16 +0800

    New translations Kabyle from Crowdin

--- locale/kab_KAB.ts
+++ locale/kab_KAB.ts
@@ -3523,18 +3523,6 @@ from Stardict, Babylon and GLS dictionaries</translation>
       <translation type="unfinished"> MB</translation>
     </message>
     <message>
-      <source>On</source>
-      <translation type="unfinished">On</translation>
-    </message>
-    <message>
-      <source>Off</source>
-      <translation type="unfinished">Off</translation>
-    </message>
-    <message>
-      <source>Auto</source>
-      <translation type="unfinished">Auto</translation>
-    </message>
-    <message>
       <source>Auto does nothing on some systems.</source>
       <translation type="unfinished">Auto does nothing on some systems.</translation>
     </message>
@@ -3553,6 +3541,18 @@ from Stardict, Babylon and GLS dictionaries</translation>
     <message>
       <source>Restart needed</source>
       <translation type="unfinished">Restart needed</translation>
+    </message>
+    <message>
+      <source>Enable</source>
+      <translation type="unfinished">Enable</translation>
+    </message>
+    <message>
+      <source>Disable</source>
+      <translation type="unfinished">Disable</translation>
+    </message>
+    <message>
+      <source>Automatic</source>
+      <translation type="unfinished">Automatic</translation>
     </message>
   </context>
   <context>
