710c1feb2a542a4fa1bfa6e6114411a232316e06
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-19 09:28:14 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -3543,15 +3543,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable</source>
-      <translation type="unfinished">Enable</translation>
+      <translation>有効にする</translation>
     </message>
     <message>
       <source>Disable</source>
-      <translation type="unfinished">Disable</translation>
+      <translation>無効にする</translation>
     </message>
     <message>
       <source>Automatic</source>
-      <translation type="unfinished">Automatic</translation>
+      <translation>自動</translation>
     </message>
   </context>
   <context>
