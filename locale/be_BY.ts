ea4d9734ee9faab8d5dd7d2f0c399966dae5f2d7
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-19 09:28:00 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -3532,15 +3532,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable</source>
-      <translation type="unfinished">Enable</translation>
+      <translation>Уключыць</translation>
     </message>
     <message>
       <source>Disable</source>
-      <translation type="unfinished">Disable</translation>
+      <translation>Адключыць</translation>
     </message>
     <message>
       <source>Automatic</source>
-      <translation type="unfinished">Automatic</translation>
+      <translation>Аўтаматычны</translation>
     </message>
   </context>
   <context>
