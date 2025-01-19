2876e60267db9fb1ddb21e46a914501fbaa55fc1
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-19 09:28:24 +0800

    New translations Russian from Crowdin

--- locale/ru_RU.ts
+++ locale/ru_RU.ts
@@ -3551,15 +3551,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable</source>
-      <translation type="unfinished">Enable</translation>
+      <translation>Давать возможность</translation>
     </message>
     <message>
       <source>Disable</source>
-      <translation type="unfinished">Disable</translation>
+      <translation>Запрещать</translation>
     </message>
     <message>
       <source>Automatic</source>
-      <translation type="unfinished">Automatic</translation>
+      <translation>Автоматический</translation>
     </message>
   </context>
   <context>
