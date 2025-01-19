80a44aec8de94ca3436f5e1e3c4b8613450beb9c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-19 09:28:29 +0800

    New translations Serbian (Cyrillic) from Crowdin

--- locale/sr_SP.ts
+++ locale/sr_SP.ts
@@ -3547,15 +3547,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable</source>
-      <translation type="unfinished">Enable</translation>
+      <translation>Омогући</translation>
     </message>
     <message>
       <source>Disable</source>
-      <translation type="unfinished">Disable</translation>
+      <translation>Онемогући</translation>
     </message>
     <message>
       <source>Automatic</source>
-      <translation type="unfinished">Automatic</translation>
+      <translation>Аутоматски</translation>
     </message>
   </context>
   <context>
