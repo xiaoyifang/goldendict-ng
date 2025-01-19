9f7ee1eef060100cce443864021560b1e6c7ff51
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-19 09:28:17 +0800

    New translations Lithuanian from Crowdin

--- locale/lt_LT.ts
+++ locale/lt_LT.ts
@@ -3541,15 +3541,15 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable</source>
-      <translation type="unfinished">Enable</translation>
+      <translation>Įgalinti</translation>
     </message>
     <message>
       <source>Disable</source>
-      <translation type="unfinished">Disable</translation>
+      <translation>Išjungti</translation>
     </message>
     <message>
       <source>Automatic</source>
-      <translation type="unfinished">Automatic</translation>
+      <translation>Automatinis</translation>
     </message>
   </context>
   <context>
