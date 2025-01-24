eedfb5ba432a00b52a004a0be89efedb8bda776c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-24 11:16:20 +0800

    New translations Chinese Traditional from Crowdin

--- locale/zh_TW.ts
+++ locale/zh_TW.ts
@@ -3527,7 +3527,7 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable application log</source>
-      <translation type="unfinished">Enable application log</translation>
+      <translation>啟用應用程式日誌</translation>
     </message>
   </context>
   <context>
