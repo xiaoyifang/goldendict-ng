e52af1eebefc9fd7dc3d913858ae8735d8946c9d
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-01-24 11:16:00 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -3536,7 +3536,7 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Enable application log</source>
-      <translation type="unfinished">Enable application log</translation>
+      <translation>アプリケーションログを有効にする</translation>
     </message>
   </context>
   <context>
