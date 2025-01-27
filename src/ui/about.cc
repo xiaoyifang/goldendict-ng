332e56582679569496406174d9b9671e40e8b01d
shenleban tongying <shenlebantongying@gmail.com>
2025-01-26 23:41:38 -0500

    refresh about

--- src/ui/about.cc
+++ src/ui/about.cc
@@ -2,34 +2,20 @@
  * Part of GoldenDict. Licensed under GPLv3 or later, see the LICENSE file */
 
 #include "about.hh"
-#include "utils.hh"
 #include "version.hh"
 
 #include <QClipboard>
-#include <QFile>
 #include <QPushButton>
-#include <QSysInfo>
 
-About::About( QWidget * parent, std::vector< sptr< Dictionary::Class > > * dictonaries ):
+About::About( QWidget * parent):
   QDialog( parent )
 {
   ui.setupUi( this );
 
-  ui.version->setText( Version::version() );
-
-  ui.qtVersion->setText( tr( "Based on Qt %1 (%2, %3)" )
-                           .arg( QLatin1String( qVersion() ), Version::compiler, QSysInfo::currentCpuArchitecture() )
-                         + " (Xapian inside)" );
+  ui.versionInfo->setText( Version::everything() );
 
   connect( ui.copyInfoBtn, &QPushButton::clicked, [] {
     QGuiApplication::clipboard()->setText( Version::everything() );
   } );
 
-  connect( ui.copyDictListBtn, &QPushButton::clicked, [ = ] {
-    QString tempDictList{};
-    for ( auto dict : *dictonaries ) {
-      tempDictList.append( QString::fromStdString( dict->getName() + "\n" ) );
-    }
-    QGuiApplication::clipboard()->setText( tempDictList );
-  } );
 }
