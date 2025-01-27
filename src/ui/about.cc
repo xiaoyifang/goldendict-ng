2361a93836f2bbdd464f0a11e3a59d5228aa3c7e
shenleban tongying <shenlebantongying@gmail.com>
2025-01-26 22:53:19 -0500

    update CREDITS

--- src/ui/about.cc
+++ src/ui/about.cc
@@ -32,37 +32,4 @@ About::About( QWidget * parent, std::vector< sptr< Dictionary::Class > > * dicto
     }
     QGuiApplication::clipboard()->setText( tempDictList );
   } );
-
-  QFile creditsFile( ":/CREDITS.txt" );
-
-  if ( creditsFile.open( QFile::ReadOnly ) ) {
-    QStringList creditsList = QString::fromUtf8( creditsFile.readAll() ).split( '\n', Qt::SkipEmptyParts );
-
-    QString html = "<html><body>";
-
-    for ( int x = 0; x < creditsList.size(); ++x ) {
-      QString str = creditsList[ x ];
-
-      str.replace( "\\", "@" );
-
-      str = Utils::escape( str );
-
-      int colon = str.indexOf( ":" );
-
-      if ( colon != -1 ) {
-        QString name( str.left( colon ) );
-
-        name.replace( ", ", "<br>" );
-
-        str = "<font color='blue'>" + name + "</font><br>&nbsp;&nbsp;&nbsp;&nbsp;" + str.mid( colon + 1 );
-      }
-
-      html += str;
-      html += "<br>";
-    }
-
-    html += "</body></html>";
-
-    ui.credits->setHtml( html );
-  }
 }
