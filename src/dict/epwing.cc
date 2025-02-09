c206d2e4a6213b49694a7d9f223f9ad95db4dbfe
shenleban tongying <shenlebantongying@gmail.com>
2025-02-08 20:56:43 -0500

    fix

--- src/dict/epwing.cc
+++ src/dict/epwing.cc
@@ -239,7 +239,7 @@ void EpwingDictionary::loadIcon() noexcept
     QString::fromStdString( getDictionaryFilenames()[ 0 ] ) + QDir::separator() + eBook.getCurrentSubBookDirectory();
 
   if ( !fileName.isEmpty() )
-    loadIconFromFile( fileName );
+    loadIconFromFileName( fileName );
 
   if ( dictionaryIcon.isNull() ) {
     // Load failed -- use default icons
