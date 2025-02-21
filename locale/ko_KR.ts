95cd031066d0c272a241a361f3a58d35ce9860f2
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-21 21:35:29 +0800

    New translations Korean from Crowdin

--- locale/ko_KR.ts
+++ locale/ko_KR.ts
@@ -2926,22 +2926,6 @@ the application.</source>
       <translation>인터페이스 글꼴</translation>
     </message>
     <message>
-      <source>Interface language:</source>
-      <translation>언어:</translation>
-    </message>
-    <message>
-      <source>Article Display style:</source>
-      <translation>기사 표시 스타일:</translation>
-    </message>
-    <message>
-      <source>Add-on style:</source>
-      <translation>Add-on 스타일:</translation>
-    </message>
-    <message>
-      <source>Interface Style:</source>
-      <translation>인터페이스 스타일:</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>UI를 어둡게 합니다.</translation>
     </message>
@@ -3518,6 +3502,34 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Save debug messages to gd_log.txt in the config folder</source>
       <translation type="unfinished">Save debug messages to gd_log.txt in the config folder</translation>
+    </message>
+    <message>
+      <source>Interface Font Size</source>
+      <translation type="unfinished">Interface Font Size</translation>
+    </message>
+    <message>
+      <source>Interface language</source>
+      <translation type="unfinished">Interface language</translation>
+    </message>
+    <message>
+      <source>Article Display style</source>
+      <translation type="unfinished">Article Display style</translation>
+    </message>
+    <message>
+      <source>Add-on style</source>
+      <translation type="unfinished">Add-on style</translation>
+    </message>
+    <message>
+      <source>Interface Style</source>
+      <translation type="unfinished">Interface Style</translation>
+    </message>
+    <message>
+      <source>Preview Font</source>
+      <translation type="unfinished">Preview Font</translation>
+    </message>
+    <message>
+      <source>Goldendict-ng is a dictionary software</source>
+      <translation type="unfinished">Goldendict-ng is a dictionary software</translation>
     </message>
   </context>
   <context>
