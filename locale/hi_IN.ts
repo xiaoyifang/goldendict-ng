1ce09b74a218b1b9ddf30b1be2d3f64ddcee955c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-21 21:35:56 +0800

    New translations Hindi from Crowdin

--- locale/hi_IN.ts
+++ locale/hi_IN.ts
@@ -2920,22 +2920,6 @@ the application.</source>
       <translation>इंटरफ़ेस फ़ॉन्ट</translation>
     </message>
     <message>
-      <source>Interface language:</source>
-      <translation>अंतरफलक भाषा:</translation>
-    </message>
-    <message>
-      <source>Article Display style:</source>
-      <translation>आलेख प्रदर्शन शैली:</translation>
-    </message>
-    <message>
-      <source>Add-on style:</source>
-      <translation>Add-on शैली:</translation>
-    </message>
-    <message>
-      <source>Interface Style:</source>
-      <translation>इंटरफ़ेस शैली:</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>यूआई को अंधेरे में बदलें।</translation>
     </message>
@@ -3505,6 +3489,34 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Save debug messages to gd_log.txt in the config folder</source>
       <translation type="unfinished">Save debug messages to gd_log.txt in the config folder</translation>
+    </message>
+    <message>
+      <source>Interface Font Size</source>
+      <translation type="unfinished">Interface Font Size</translation>
+    </message>
+    <message>
+      <source>Interface language</source>
+      <translation type="unfinished">Interface language</translation>
+    </message>
+    <message>
+      <source>Article Display style</source>
+      <translation type="unfinished">Article Display style</translation>
+    </message>
+    <message>
+      <source>Add-on style</source>
+      <translation type="unfinished">Add-on style</translation>
+    </message>
+    <message>
+      <source>Interface Style</source>
+      <translation type="unfinished">Interface Style</translation>
+    </message>
+    <message>
+      <source>Preview Font</source>
+      <translation type="unfinished">Preview Font</translation>
+    </message>
+    <message>
+      <source>Goldendict-ng is a dictionary software</source>
+      <translation type="unfinished">Goldendict-ng is a dictionary software</translation>
     </message>
   </context>
   <context>
