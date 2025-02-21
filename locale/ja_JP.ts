360c85c5ee5b6aa593749c4911bce0e8fff3485f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-21 21:35:27 +0800

    New translations Japanese from Crowdin

--- locale/ja_JP.ts
+++ locale/ja_JP.ts
@@ -2928,22 +2928,6 @@ the application.</source>
       <translation>インターフェースフォント</translation>
     </message>
     <message>
-      <source>Interface language:</source>
-      <translation>インターフェイス言語:</translation>
-    </message>
-    <message>
-      <source>Article Display style:</source>
-      <translation>記事表示のスタイル:</translation>
-    </message>
-    <message>
-      <source>Add-on style:</source>
-      <translation>アドオンのスタイル:</translation>
-    </message>
-    <message>
-      <source>Interface Style:</source>
-      <translation>インターフェイスのスタイル:</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>UIを暗くします。</translation>
     </message>
@@ -3521,6 +3505,34 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Save debug messages to gd_log.txt in the config folder</source>
       <translation>設定フォルダ内のgd_log.txtにデバッグメッセージを保存します</translation>
+    </message>
+    <message>
+      <source>Interface Font Size</source>
+      <translation>インターフェースフォントサイズ</translation>
+    </message>
+    <message>
+      <source>Interface language</source>
+      <translation>インターフェース言語</translation>
+    </message>
+    <message>
+      <source>Article Display style</source>
+      <translation>記事表示のスタイル</translation>
+    </message>
+    <message>
+      <source>Add-on style</source>
+      <translation>アドオンのスタイル</translation>
+    </message>
+    <message>
+      <source>Interface Style</source>
+      <translation>インターフェイスのスタイル</translation>
+    </message>
+    <message>
+      <source>Preview Font</source>
+      <translation>フォントをプレビュー</translation>
+    </message>
+    <message>
+      <source>Goldendict-ng is a dictionary software</source>
+      <translation>Goldendict-ng は辞書のソフトウェアです</translation>
     </message>
   </context>
   <context>
