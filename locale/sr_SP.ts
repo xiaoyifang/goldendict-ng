35f2b49301aaaf18ad98d4e985790e6feaa733f0
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-21 21:35:42 +0800

    New translations Serbian (Cyrillic) from Crowdin

--- locale/sr_SP.ts
+++ locale/sr_SP.ts
@@ -2930,22 +2930,6 @@ the application.</source>
       <translation>Интерфејс Фонт</translation>
     </message>
     <message>
-      <source>Interface language:</source>
-      <translation>Језик сучеља:</translation>
-    </message>
-    <message>
-      <source>Article Display style:</source>
-      <translation>Стил приказа чланка:</translation>
-    </message>
-    <message>
-      <source>Add-on style:</source>
-      <translation>Додатак стилу:</translation>
-    </message>
-    <message>
-      <source>Interface Style:</source>
-      <translation>Стил интерфејса:</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>Окрените корисничко сучеље у тамно.</translation>
     </message>
@@ -3525,6 +3509,34 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>Save debug messages to gd_log.txt in the config folder</source>
       <translation type="unfinished">Save debug messages to gd_log.txt in the config folder</translation>
+    </message>
+    <message>
+      <source>Interface Font Size</source>
+      <translation type="unfinished">Interface Font Size</translation>
+    </message>
+    <message>
+      <source>Interface language</source>
+      <translation type="unfinished">Interface language</translation>
+    </message>
+    <message>
+      <source>Article Display style</source>
+      <translation type="unfinished">Article Display style</translation>
+    </message>
+    <message>
+      <source>Add-on style</source>
+      <translation type="unfinished">Add-on style</translation>
+    </message>
+    <message>
+      <source>Interface Style</source>
+      <translation type="unfinished">Interface Style</translation>
+    </message>
+    <message>
+      <source>Preview Font</source>
+      <translation type="unfinished">Preview Font</translation>
+    </message>
+    <message>
+      <source>Goldendict-ng is a dictionary software</source>
+      <translation type="unfinished">Goldendict-ng is a dictionary software</translation>
     </message>
   </context>
   <context>
