b88c58f313b775d789b2c9bbe0e863c09d044efe
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-24 09:54:50 +0800

    New translations Tajik from Crowdin

--- locale/tg_TJ.ts
+++ locale/tg_TJ.ts
@@ -823,6 +823,10 @@ between classic and school orthography in cyrillic)</source>
       <source>Are you sure you want to clear all items?</source>
       <translation>Шумо мутмаин ҳастед, ки мехоҳед ҳамаи ҷузъҳоро тоза кунед?</translation>
     </message>
+    <message>
+      <source>Make this folder the target of adding/removing words actions.</source>
+      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+    </message>
   </context>
   <context>
     <name>Forvo::ForvoArticleRequest</name>
@@ -2336,10 +2340,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>F1</translation>
     </message>
     <message>
-      <source>Show</source>
-      <translation>Намоиш</translation>
-    </message>
-    <message>
       <source>Export</source>
       <translation>Содир кардан</translation>
     </message>
@@ -2644,6 +2644,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     <message>
       <source>Remove headword &quot;%1&quot; from Favorites?</source>
       <translation>Сарлавҳаи &quot;%1&quot; аз дӯстдоштаҳо хориҷ карда шавад?</translation>
+    </message>
+    <message>
+      <source>Show</source>
+      <translation>Намоиш</translation>
     </message>
   </context>
   <context>
