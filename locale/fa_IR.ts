acc2b644f57744679273dce2babfc5ae48fd627c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-24 09:54:42 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -823,6 +823,10 @@ between classic and school orthography in cyrillic)</source>
       <source>Are you sure you want to clear all items?</source>
       <translation>آیا مطمئن هستید که می خواهید همه موارد را پاک کنید؟</translation>
     </message>
+    <message>
+      <source>Make this folder the target of adding/removing words actions.</source>
+      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+    </message>
   </context>
   <context>
     <name>Forvo::ForvoArticleRequest</name>
@@ -2336,10 +2340,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>F1</translation>
     </message>
     <message>
-      <source>Show</source>
-      <translation>نمایش دهید</translation>
-    </message>
-    <message>
       <source>Export</source>
       <translation>صادر کردن</translation>
     </message>
@@ -2644,6 +2644,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     <message>
       <source>Remove headword &quot;%1&quot; from Favorites?</source>
       <translation>عنوان &quot;%1&quot; از موارد دلخواه حذف شود؟</translation>
+    </message>
+    <message>
+      <source>Show</source>
+      <translation>نمایش دهید</translation>
     </message>
   </context>
   <context>
