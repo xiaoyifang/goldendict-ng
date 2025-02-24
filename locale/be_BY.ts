3791645862a14a5506000447628806e20b558921
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-02-24 09:54:11 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -823,6 +823,10 @@ between classic and school orthography in cyrillic)</source>
       <source>Are you sure you want to clear all items?</source>
       <translation>Вы ўпэўнены, што хочаце выдаліць усе элементы?</translation>
     </message>
+    <message>
+      <source>Make this folder the target of adding/removing words actions.</source>
+      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+    </message>
   </context>
   <context>
     <name>Forvo::ForvoArticleRequest</name>
@@ -2336,10 +2340,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>F1</translation>
     </message>
     <message>
-      <source>Show</source>
-      <translation>Паказваць</translation>
-    </message>
-    <message>
       <source>Export</source>
       <translation>Экспартаваць</translation>
     </message>
@@ -2644,6 +2644,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     <message>
       <source>Remove headword &quot;%1&quot; from Favorites?</source>
       <translation>Выдаліць загалоўнае слова &quot;%1&quot; з улюбёнана?</translation>
+    </message>
+    <message>
+      <source>Show</source>
+      <translation>Паказваць</translation>
     </message>
   </context>
   <context>
