b4335fac5149641b43b8895e70b5149d0d49fa54
YiFang Xiao <yifang.xiao@foxmail.com>
2025-02-26 23:44:30 +0800

    Revert "revert: tiff2imag method ,part of commit 5d9b930de5472e933d5260837569e6e68eb6ce6a"
    This reverts commit f8b49dc32abd68d48cf04154d54dda3705019224.

--- src/tiff.cc
+++ src/tiff.cc
@@ -8,22 +8,23 @@
 
 namespace GdTiff {
 
-void tiff2img( std::vector< char > & data, const char * format )
+void tiff2img( std::vector< char > & data )
 {
   QImage img = QImage::fromData( (unsigned char *)&data.front(), data.size() );
 
   if ( !img.isNull() ) {
     QByteArray ba;
-    QBuffer buffer( &ba );
+
+    QBuffer buffer( &ba ); // buffer doesn't own ba
     buffer.open( QIODevice::WriteOnly );
+
     QSize screenSize = QApplication::primaryScreen()->availableSize();
     QSize imgSize    = img.size();
     int scaleSize    = qMin( imgSize.width(), screenSize.width() );
+    img.scaledToWidth( scaleSize ).save( &buffer, "webp" );
 
-    img.scaledToWidth( scaleSize ).save( &buffer, format );
-
-    data.resize( buffer.size() );
-    memcpy( &data.front(), buffer.data(), data.size() );
+    memcpy( data.data(), ba.data(), ba.size() );
+    data.resize( ba.size() );
     buffer.close();
   }
 }
