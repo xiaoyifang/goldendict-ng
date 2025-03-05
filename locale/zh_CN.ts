54fe62ab26e9358f70707d4555a41253d2e1bdee
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-03-06 04:28:19 +0800

    New translations Chinese Simplified from Crowdin

--- locale/zh_CN.ts
+++ locale/zh_CN.ts
@@ -3720,20 +3720,23 @@ from Stardict, Babylon and GLS dictionaries</source>
   <context>
     <name>ScanPopup</name>
     <message>
-      <source>Dialog</source>
-      <translation>对话框</translation>
+      <source>WARNING: %1</source>
+      <translation>警告： %1</translation>
     </message>
+  </context>
+  <context>
+    <name>ScanPopupToolBar</name>
     <message>
       <source>Back</source>
-      <translation>回退</translation>
+      <translation>后退</translation>
     </message>
     <message>
       <source>Forward</source>
-      <translation>前进</translation>
+      <translation>转发方式</translation>
     </message>
     <message>
       <source>Pronounce Word (Alt+S)</source>
-      <translation>朗读词条(Alt+S)</translation>
+      <translation>弹出单词(Alt+S)</translation>
     </message>
     <message>
       <source>Alt+S</source>
@@ -3741,7 +3744,7 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Send word to main window (Alt+W)</source>
-      <translation>将词条发送到主窗口 (Alt+W)</translation>
+      <translation>发送单词到主窗口 (Alt+W)</translation>
     </message>
     <message>
       <source>Alt+W</source>
@@ -3749,24 +3752,17 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Add word to Favorites (Ctrl+E)</source>
-      <translation>将词条添加至收藏 (Ctrl+E)</translation>
-    </message>
-    <message>
-      <source>Shows or hides the dictionary bar</source>
-      <translation>显示或隐藏词典栏</translation>
+      <translation>添加单词到收藏夹 (Ctrl+E)</translation>
     </message>
     <message>
       <source>Always stay on top of all other windows</source>
-      <translation>总是位于其它窗口上方</translation>
+      <translation>总是在所有其他窗口的顶端</translation>
     </message>
     <message>
       <source>Use this to pin down the window so it would stay on screen,
 could be resized or managed in other ways.</source>
-      <translation>使用此按钮可以固定窗口，以便它能停留在屏幕上进行改变大小等相关窗口管理操作。</translation>
-    </message>
-    <message>
-      <source>WARNING: %1</source>
-      <translation>警告： %1</translation>
+      <translation>用它来固定窗口，使它保持在屏幕上，
+可以调整大小或以其他方式管理。</translation>
     </message>
   </context>
   <context>
