23d2c6b2be71fe4e276e39448158d747488654fa
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-03-06 13:55:18 +0800

    New translations Spanish, Bolivia from Crowdin

--- locale/es_BO.ts
+++ locale/es_BO.ts
@@ -825,7 +825,7 @@ entre la ortografía clásica y la escolar en cirílico)</translation>
     </message>
     <message>
       <source>Make this folder the target of adding/removing words actions.</source>
-      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+      <translation>Make this folder the target of adding/removing words actions.</translation>
     </message>
   </context>
   <context>
