e4a9ce263bc7017a0a1e5bacae7dcc6483fd4a28
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-03-06 13:55:11 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -825,7 +825,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Make this folder the target of adding/removing words actions.</source>
-      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+      <translation>Make this folder the target of adding/removing words actions.</translation>
     </message>
   </context>
   <context>
