19670972b2cb768d2f4267e0825212d9ce5c9471
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-03-06 13:54:57 +0800

    New translations Portuguese from Crowdin

--- locale/pt_PT.ts
+++ locale/pt_PT.ts
@@ -825,7 +825,7 @@ entre ortografia clássica e escolar em ciílico)</translation>
     </message>
     <message>
       <source>Make this folder the target of adding/removing words actions.</source>
-      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+      <translation>Make this folder the target of adding/removing words actions.</translation>
     </message>
   </context>
   <context>
