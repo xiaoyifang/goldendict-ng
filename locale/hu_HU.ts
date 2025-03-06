14e445a6c105740f8bce03c3844294b316c519a4
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-03-06 13:54:47 +0800

    New translations Hungarian from Crowdin

--- locale/hu_HU.ts
+++ locale/hu_HU.ts
@@ -825,7 +825,7 @@ a klasszikus és az iskolai helyesírás közt cirillben)</translation>
     </message>
     <message>
       <source>Make this folder the target of adding/removing words actions.</source>
-      <translation type="unfinished">Make this folder the target of adding/removing words actions.</translation>
+      <translation>Make this folder the target of adding/removing words actions.</translation>
     </message>
   </context>
   <context>
