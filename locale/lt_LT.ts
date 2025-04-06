31ac56f26faa87c93e7f705d9ec41d091545b89d
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-07 02:47:10 +0800

    New translations Lithuanian from Crowdin

--- locale/lt_LT.ts
+++ locale/lt_LT.ts
@@ -688,6 +688,10 @@ between classic and school orthography in cyrillic)</source>
       <source>Open dictionary folder</source>
       <translation>Atverti žodyno aplanką</translation>
     </message>
+    <message>
+      <source>Restore selection</source>
+      <translation type="unfinished">Restore selection</translation>
+    </message>
   </context>
   <context>
     <name>EditDictionaries</name>
