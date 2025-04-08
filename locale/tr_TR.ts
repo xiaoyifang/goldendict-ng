c46ee1d57b8baefbaa2d84ba9bedaa253b5a3162
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-08 17:58:06 +0800

    New translations Turkish from Crowdin

--- locale/tr_TR.ts
+++ locale/tr_TR.ts
@@ -291,24 +291,12 @@ arasındaki farkı giderir)</translation>
       <translation>Basitleştirilmiş karakterlerden geleneksel (Tayvan değişkeni) karakterlere dönüştürmeyi etkinleştir</translation>
     </message>
     <message>
-      <source>SC to TC (Taiwan variant)</source>
-      <translation>TC&apos;den SC&apos;ye (Tayvan varyantı)</translation>
-    </message>
-    <message>
       <source>Enable conversion from simplified characters to traditional (Hong Kong variant) characters</source>
       <translation>Basitleştirilmiş karakterlerden geleneksel (Hong Kong değişken) karakterlere dönüştürmeyi etkinleştir</translation>
     </message>
     <message>
-      <source>SC to TC (Hong Kong variant)</source>
-      <translation>TC&apos;den SC&apos;ye (Hong Kong değişkeni)</translation>
-    </message>
-    <message>
       <source>Enable conversion from traditional characters to simplified characters</source>
       <translation>Geleneksel karakterlerden basitleştirilmiş karakterlere dönüştürmeyi etkinleştir</translation>
-    </message>
-    <message>
-      <source>TC to SC</source>
-      <translation>TC&apos;den SC&apos;ye</translation>
     </message>
     <message>
       <source>Simplified to traditional Chinese (Taiwan variant) conversion</source>
@@ -321,6 +309,18 @@ arasındaki farkı giderir)</translation>
     <message>
       <source>Traditional to simplified Chinese conversion</source>
       <translation>Basitleştirilmiş Çince dönüşümü için geleneksel</translation>
+    </message>
+    <message>
+      <source>Taiwan variant</source>
+      <translation type="unfinished">Taiwan variant</translation>
+    </message>
+    <message>
+      <source>Hong Kong variant</source>
+      <translation type="unfinished">Hong Kong variant</translation>
+    </message>
+    <message>
+      <source>Mainland variant</source>
+      <translation type="unfinished">Mainland variant</translation>
     </message>
   </context>
   <context>
