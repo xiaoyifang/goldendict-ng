d05838d62a97876b57eac9ce3a9afcb2367755a8
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-15 21:25:56 +0800

    New translations Interlingue from Crowdin

--- locale/ie_001.ts
+++ locale/ie_001.ts
@@ -3935,12 +3935,6 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
       <translation>Sistemas:</translation>
     </message>
     <message>
-      <source>The most widely used method of transcription of Japanese,
-based on English phonology</source>
-      <translation>The most widely used method of transcription of Japanese,
-based on English phonology</translation>
-    </message>
-    <message>
       <source>Hepburn</source>
       <translation>Hepburn</translation>
     </message>
@@ -3999,6 +3993,10 @@ based on English phonology</translation>
     <message>
       <source>Remove program &lt;b&gt;%1&lt;/b&gt; from the list?</source>
       <translation>Remover li programa &lt;b&gt;%1&lt;/b&gt; ex li liste?</translation>
+    </message>
+    <message>
+      <source>The most widely used method of transcription of Japanese,based on English phonology</source>
+      <translation type="unfinished">The most widely used method of transcription of Japanese,based on English phonology</translation>
     </message>
   </context>
   <context>
