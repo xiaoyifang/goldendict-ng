8453292f2849b5969555c4e4c1aeabe04fc1f3b7
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 14:23:12 +0800

    New translations Hungarian from Crowdin

--- locale/hu_HU.ts
+++ locale/hu_HU.ts
@@ -19,20 +19,20 @@
   <context>
     <name>AnkiConnector</name>
     <message>
-      <source>anki: can&apos;t create a card without a word</source>
-      <translation>anki: szó nélkül nem hozható létre a kártya</translation>
+      <source>Anki: can&apos;t create a card without a word</source>
+      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
       <translation>Anki kereső: Az AnkiConnect nincs bekapcsolva.</translation>
     </message>
     <message>
-      <source>anki: post to anki failed</source>
-      <translation>anki: beküldés nem sikerült</translation>
+      <source>Anki: post to Anki failed</source>
+      <translation type="unfinished">Anki: post to Anki failed</translation>
     </message>
     <message>
-      <source>anki: post to anki success</source>
-      <translation>anki: beküldés sikeres</translation>
+      <source>Anki: post to Anki success</source>
+      <translation type="unfinished">Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
