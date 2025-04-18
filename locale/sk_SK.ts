c490fce0478f24153eca78b3a762b18e9288eabc
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 12:00:12 +0800

    New translations Slovak from Crowdin

--- locale/sk_SK.ts
+++ locale/sk_SK.ts
@@ -3986,8 +3986,8 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
       <translation>Odobrať program &lt;b&gt;%1&lt;/b&gt; zo zoznamu?</translation>
     </message>
     <message>
-      <source>The most widely used method of transcription of Japanese,based on English phonology</source>
-      <translation type="unfinished">The most widely used method of transcription of Japanese,based on English phonology</translation>
+      <source>The most widely used method of transcription of Japanese, based on English phonology</source>
+      <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
     </message>
   </context>
   <context>
