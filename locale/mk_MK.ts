12874bf1a63ec420a6e5032c7e33417b7a62f700
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 12:00:03 +0800

    New translations Macedonian from Crowdin

--- locale/mk_MK.ts
+++ locale/mk_MK.ts
@@ -4000,8 +4000,8 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
       <translation>Отстрани програм &lt;b&gt;%1&lt;/b&gt; ид список?</translation>
     </message>
     <message>
-      <source>The most widely used method of transcription of Japanese,based on English phonology</source>
-      <translation type="unfinished">The most widely used method of transcription of Japanese,based on English phonology</translation>
+      <source>The most widely used method of transcription of Japanese, based on English phonology</source>
+      <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
     </message>
   </context>
   <context>
