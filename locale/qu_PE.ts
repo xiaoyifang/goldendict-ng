cde062fb140fe307d66bf9a667f7df08f6010e0c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 14:23:45 +0800

    New translations Quechua from Crowdin

--- locale/qu_PE.ts
+++ locale/qu_PE.ts
@@ -19,20 +19,20 @@
   <context>
     <name>AnkiConnector</name>
     <message>
-      <source>anki: can&apos;t create a card without a word</source>
-      <translation>anki: mana simiyuq tarjetata&apos;t ruwayta atin</translation>
+      <source>Anki: can&apos;t create a card without a word</source>
+      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
       <translation>Anki maskana: AnkiConnect mana atichisqachu.</translation>
     </message>
     <message>
-      <source>anki: post to anki failed</source>
-      <translation>anki: post a anki fracasado</translation>
+      <source>Anki: post to Anki failed</source>
+      <translation type="unfinished">Anki: post to Anki failed</translation>
     </message>
     <message>
-      <source>anki: post to anki success</source>
-      <translation>anki: post a anki exito</translation>
+      <source>Anki: post to Anki success</source>
+      <translation type="unfinished">Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
