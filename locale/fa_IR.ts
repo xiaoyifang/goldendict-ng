45c1f026d597aced24a19e907e6b26604965db28
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 14:23:36 +0800

    New translations Persian from Crowdin

--- locale/fa_IR.ts
+++ locale/fa_IR.ts
@@ -19,20 +19,20 @@
   <context>
     <name>AnkiConnector</name>
     <message>
-      <source>anki: can&apos;t create a card without a word</source>
-      <translation>anki: نمی توان بدون کلمه کارت ایجاد کرد</translation>
+      <source>Anki: can&apos;t create a card without a word</source>
+      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
       <translation>جستجوی Anki: AnkiConnect فعال نیست.</translation>
     </message>
     <message>
-      <source>anki: post to anki failed</source>
-      <translation>anki: ارسال به anki ناموفق بود</translation>
+      <source>Anki: post to Anki failed</source>
+      <translation type="unfinished">Anki: post to Anki failed</translation>
     </message>
     <message>
-      <source>anki: post to anki success</source>
-      <translation>anki: ارسال به موفقیت anki</translation>
+      <source>Anki: post to Anki success</source>
+      <translation type="unfinished">Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
