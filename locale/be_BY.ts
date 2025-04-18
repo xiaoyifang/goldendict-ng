97410323ee0cb0a27a1f545bdd73f2d26ec9b830
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 14:23:05 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -19,20 +19,20 @@
   <context>
     <name>AnkiConnector</name>
     <message>
-      <source>anki: can&apos;t create a card without a word</source>
-      <translation>anki: Вы не можаце стварыць картку без слова</translation>
+      <source>Anki: can&apos;t create a card without a word</source>
+      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
       <translation>Пошук у Anki: AnkiConnect не ўключаны.</translation>
     </message>
     <message>
-      <source>anki: post to anki failed</source>
-      <translation>anki: не ўдалося адправіць у anki</translation>
+      <source>Anki: post to Anki failed</source>
+      <translation type="unfinished">Anki: post to Anki failed</translation>
     </message>
     <message>
-      <source>anki: post to anki success</source>
-      <translation>anki: паспяхова адпраўлена ў anki</translation>
+      <source>Anki: post to Anki success</source>
+      <translation type="unfinished">Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
