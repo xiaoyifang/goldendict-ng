3f78d79c434ad1e5089d9bbd1707b9ba54bd919f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 15:58:58 +0800

    New translations English from Crowdin

--- locale/crowdin.ts
+++ locale/crowdin.ts
@@ -19,7 +19,7 @@
 <context>
     <name>AnkiConnector</name>
     <message>
-        <source>anki: can&apos;t create a card without a word</source>
+        <source>Anki: can&apos;t create a card without a word</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -27,11 +27,11 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>anki: post to anki failed</source>
+        <source>Anki: post to Anki failed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>anki: post to anki success</source>
+        <source>Anki: post to Anki success</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
