8d01f379627fb0d2d7ce15f8c8cedf2a05783c6c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-04-18 11:59:49 +0800

    New translations Belarusian from Crowdin

--- locale/be_BY.ts
+++ locale/be_BY.ts
@@ -3979,8 +3979,8 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
       <translation>Выдаліць праграму &lt;b&gt;%1&lt;/b&gt; са спіса?</translation>
     </message>
     <message>
-      <source>The most widely used method of transcription of Japanese,based on English phonology</source>
-      <translation type="unfinished">The most widely used method of transcription of Japanese,based on English phonology</translation>
+      <source>The most widely used method of transcription of Japanese, based on English phonology</source>
+      <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
     </message>
   </context>
   <context>
