149a7677e62ca59479066041a840a2c42ab0ed07
Xiao YiFang <yifang.xiao@foxmail.com>
2025-04-21 10:38:22 +0800

    opt:add prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'ignore',
+    endOfLine: 'lf',
+    embeddedLanguageFormatting: 'auto',
+};
