807ab77dd419b86d726bbedfa161878dc90972c5
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-05-07 04:21:38 +0800

    New translations Lithuanian from Crowdin

--- locale/lt_LT.ts
+++ locale/lt_LT.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation>Anki: gali&apos;t sukurti kortelę be žodžio</translation>
+      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
@@ -28,11 +28,11 @@
     </message>
     <message>
       <source>Anki: post to Anki failed</source>
-      <translation>Anki: išsiųsti Anki nepavyko</translation>
+      <translation type="unfinished">Anki: post to Anki failed</translation>
     </message>
     <message>
       <source>Anki: post to Anki success</source>
-      <translation>Anki: paskelbti Anki sėkmės</translation>
+      <translation type="unfinished">Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
@@ -3461,8 +3461,8 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Preview Font</translation>
     </message>
     <message>
-      <source>Goldendict-ng is a dictionary software</source>
-      <translation>Goldendict-ng is a dictionary software</translation>
+      <source>GoldenDict-ng is a dictionary software.</source>
+      <translation type="unfinished">GoldenDict-ng is a dictionary software.</translation>
     </message>
   </context>
   <context>
@@ -3589,14 +3589,6 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Pakeiskite iššokančiųjų langų grupę.</translation>
     </message>
     <message>
-      <source>Force the word to be translated in scanpopup</source>
-      <translation>Priverskite žodį išversti „scanpopup“.</translation>
-    </message>
-    <message>
-      <source>Force the word to be translated in the mainwindow</source>
-      <translation>Priverskite žodį išversti pagrindiniame lange</translation>
-    </message>
-    <message>
       <source>Toggle popup.</source>
       <translation>Perjungti nuskaitymo iššokantįjį langą.</translation>
     </message>
@@ -3605,8 +3597,16 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Spausdinimo versija ir diagnostikos informacija.</translation>
     </message>
     <message>
-      <source>Save debug messages to gd_log.txt in the config folder</source>
-      <translation>Save debug messages to gd_log.txt in the config folder</translation>
+      <source>Save debug messages to gd_log.txt in the config folder.</source>
+      <translation>Išsaugokite derinimo pranešimus gd_log.txt konfigūracijos aplanke.</translation>
+    </message>
+    <message>
+      <source>Force the word to be translated in Popup.</source>
+      <translation type="unfinished">Force the word to be translated in Popup.</translation>
+    </message>
+    <message>
+      <source>Force the word to be translated in the mainwindow.</source>
+      <translation type="unfinished">Force the word to be translated in the mainwindow.</translation>
     </message>
   </context>
   <context>
