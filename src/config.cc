099d7dfe8263343ed3714375ca6fe863b9d2461a
Xiao YiFang <yifang.xiao@foxmail.com>
2025-05-19 17:01:13 +0800

    1

--- src/config.cc
+++ src/config.cc
@@ -832,7 +832,7 @@ Class load()
       c.preferences.interfaceFontSize = fontSize.toElement().text().toInt();
     }
     else {
-      c.preferences.interfaceFontSize = Config::DEFAULT_INTERFACE_FONT_SIZE;
+      c.preferences.interfaceFontSize = Config::DEFAULT_FONT_SIZE;
     }
 #if !defined( Q_OS_WIN )
     c.preferences.interfaceStyle = preferences.namedItem( "interfaceStyle" ).toElement().text();
