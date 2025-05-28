cfc4b9cf569ee8128d9271654f05ed20b7712d52
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-05-28 09:06:00 +0800

    opt:register custom protocol (#2359)

--- src/main.cc
+++ src/main.cc
@@ -324,8 +324,18 @@ int main( int argc, char ** argv )
 
 #endif
 
-  const QStringList localSchemes =
-    { "gdlookup", "gdau", "gico", "qrcx", "bres", "bword", "gdprg", "gdvideo", "gdtts", "entry" };
+  const QStringList localSchemes = { "gdlookup",
+                                     "gdau",
+                                     "gico",
+                                     "qrcx",
+                                     "bres",
+                                     "bword",
+                                     "gdprg",
+                                     "gdvideo",
+                                     "gdtts",
+                                     "entry",
+                                     "iframe-http",
+                                     "iframe-https" };
 
   for ( const auto & localScheme : localSchemes ) {
     QWebEngineUrlScheme webUiScheme( localScheme.toLatin1() );
