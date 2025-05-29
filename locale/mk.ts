2c1dbb7c70e37d8b3845d3b26aa773de446f0120
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-05-29 11:33:21 +0800

    New translations Macedonian from Crowdin

--- locale/mk.ts
+++ locale/mk.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
+      <translation>Anki: can&apos;t create a card without a word</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
@@ -28,11 +28,11 @@
     </message>
     <message>
       <source>Anki: post to Anki failed</source>
-      <translation type="unfinished">Anki: post to Anki failed</translation>
+      <translation>Anki: post to Anki failed</translation>
     </message>
     <message>
       <source>Anki: post to Anki success</source>
-      <translation type="unfinished">Anki: post to Anki success</translation>
+      <translation>Anki: post to Anki success</translation>
     </message>
   </context>
   <context>
@@ -312,15 +312,15 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Taiwan variant</source>
-      <translation type="unfinished">Taiwan variant</translation>
+      <translation>Taiwan variant</translation>
     </message>
     <message>
       <source>Hong Kong variant</source>
-      <translation type="unfinished">Hong Kong variant</translation>
+      <translation>Hong Kong variant</translation>
     </message>
     <message>
       <source>Mainland variant</source>
-      <translation type="unfinished">Mainland variant</translation>
+      <translation>Mainland variant</translation>
     </message>
   </context>
   <context>
@@ -540,7 +540,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Regular Expression</source>
-      <translation type="unfinished">Regular Expression</translation>
+      <translation>Regular Expression</translation>
     </message>
   </context>
   <context>
@@ -691,7 +691,7 @@ between classic and school orthography in cyrillic)</source>
     </message>
     <message>
       <source>Restore selection</source>
-      <translation type="unfinished">Restore selection</translation>
+      <translation>Restore selection</translation>
     </message>
   </context>
   <context>
@@ -2007,10 +2007,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>Албански</translation>
     </message>
     <message>
-      <source>Serbian (Cyrillic)</source>
-      <translation>Српски (Кирилица)</translation>
-    </message>
-    <message>
       <source>Swedish</source>
       <translation>шведски</translation>
     </message>
@@ -2075,10 +2071,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>Ајмара</translation>
     </message>
     <message>
-      <source>Arabic, Saudi Arabia</source>
-      <translation>Арапски, Саудиска Арабија</translation>
-    </message>
-    <message>
       <source>Turkmen</source>
       <translation>Туркменистански</translation>
     </message>
@@ -2097,6 +2089,18 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <source>English</source>
       <translation>Англиски</translation>
+    </message>
+    <message>
+      <source>Arabic</source>
+      <translation>Арапски</translation>
+    </message>
+    <message>
+      <source>Serbian</source>
+      <translation>Српски</translation>
+    </message>
+    <message>
+      <source>Kabyle</source>
+      <translation type="unfinished">Kabyle</translation>
     </message>
   </context>
   <context>
@@ -2592,11 +2596,11 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     </message>
     <message>
       <source>Save PDF complete</source>
-      <translation type="unfinished">Save PDF complete</translation>
+      <translation>Save PDF complete</translation>
     </message>
     <message>
       <source>Save PDF failed</source>
-      <translation type="unfinished">Save PDF failed</translation>
+      <translation>Save PDF failed</translation>
     </message>
   </context>
   <context>
@@ -3468,7 +3472,7 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>GoldenDict-ng is a dictionary software.</source>
-      <translation type="unfinished">GoldenDict-ng is a dictionary software.</translation>
+      <translation>GoldenDict-ng is a dictionary software.</translation>
     </message>
   </context>
   <context>
@@ -3608,11 +3612,11 @@ from Stardict, Babylon and GLS dictionaries</source>
     </message>
     <message>
       <source>Force the word to be translated in Popup.</source>
-      <translation type="unfinished">Force the word to be translated in Popup.</translation>
+      <translation>Force the word to be translated in Popup.</translation>
     </message>
     <message>
       <source>Force the word to be translated in the mainwindow.</source>
-      <translation type="unfinished">Force the word to be translated in the mainwindow.</translation>
+      <translation>Force the word to be translated in the mainwindow.</translation>
     </message>
   </context>
   <context>
@@ -4001,7 +4005,7 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
     </message>
     <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
-      <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
+      <translation>The most widely used method of transcription of Japanese, based on English phonology</translation>
     </message>
   </context>
   <context>
