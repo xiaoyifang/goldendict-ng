ce253dc229dfd1922dfce7468588de3f7e4ae6c5
shenleban tongying <shenlebantongying@gmail.com>
2025-06-07 16:42:20 -0400

    Update mainwindow.cc

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -3079,7 +3079,7 @@ void MainWindow::trayIconActivated( QSystemTrayIcon::ActivationReason r )
 
 void MainWindow::visitHomepage()
 {
-  QDesktopServices::openUrl( QUrl( "https://github.com/xiaoyifang/goldendict-ng" ) );
+  QDesktopServices::openUrl( QUrl( "https://xiaoyifang.github.io/goldendict-ng/" ) );
 }
 
 void MainWindow::openConfigFolder()
