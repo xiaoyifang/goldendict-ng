50552caa7d976775eb9b8d5c0c31903561c7c8b0
Xiao YiFang <yifang.xiao@foxmail.com>
2025-06-26 14:05:42 +0800

    1

---
+++ src/ui/custom_font_combobox.cc
@@ -0,0 +1,14 @@
+#include "custom_font_combobox.hh"
+
+CustomFontComboBox::CustomFontComboBox(QWidget *parent)
+    : QFontComboBox(parent)
+{
+}
+
+void CustomFontComboBox::insertCustomItem(const QString &item,int index=0)
+{
+    if (index < 0 || index > this->count()) {
+        index = 0; // Default to the beginning if index is out of bounds
+    }
+    insertItem(index, item);
+}
