c50983a29a8fe39991b40b7b5cc12430e8800bf2
Xiao YiFang <yifang.xiao@foxmail.com>
2025-06-26 13:58:32 +0800

    feat:add custom fontcombobox

---
+++ src/ui/custom_font_comboBox.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+
+#include <QFontComboBox>
+#include <QStringList>
+
+class CustomFontComboBox : public QFontComboBox
+{
+    Q_OBJECT
+
+public:
+    explicit CustomFontComboBox(QWidget *parent = nullptr);
+    void insertCustomItem(const QString &item,int index=0);
+};
