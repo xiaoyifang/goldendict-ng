784542e74e177d78e2fc356d3abcc1cd63f392b0
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 20:14:35 +0800

    New translations Esperanto from Crowdin

--- locale/eo.ts
+++ locale/eo.ts
@@ -2880,10 +2880,6 @@ la aplikaĵon.</translation>
       <translation>Aspekto</translation>
     </message>
     <message>
-      <source>Interface Font</source>
-      <translation>Interfaco Tiparo</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>Turnu la UI al mallumo.</translation>
     </message>
@@ -3444,10 +3440,6 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
       <translation>Save debug messages to gd_log.txt in the config folder</translation>
     </message>
     <message>
-      <source>Interface Font Size</source>
-      <translation>Interface Font Size</translation>
-    </message>
-    <message>
       <source>Interface language</source>
       <translation>Interface language</translation>
     </message>
@@ -3470,6 +3462,18 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     <message>
       <source>GoldenDict-ng is a dictionary software.</source>
       <translation type="unfinished">GoldenDict-ng is a dictionary software.</translation>
+    </message>
+    <message>
+      <source>Enable Interface Font</source>
+      <translation type="unfinished">Enable Interface Font</translation>
+    </message>
+    <message>
+      <source>Font</source>
+      <translation type="unfinished">Font</translation>
+    </message>
+    <message>
+      <source>Font Size</source>
+      <translation type="unfinished">Font Size</translation>
     </message>
     <message>
       <source>None</source>
