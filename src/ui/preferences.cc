15845c00940b9d59f7e0affb0a789674bd0492c0
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 19:19:50 +0800

    opt: allow enable interface font (#2384)

--- src/ui/preferences.cc
+++ src/ui/preferences.cc
@@ -83,6 +83,7 @@ Preferences::Preferences( QWidget * parent, Config::Class & cfg_ ):
   if ( !p.interfaceFont.isEmpty() ) {
     ui.systemFont->setCurrentText( p.interfaceFont );
   }
+  ui.enableInterfaceFont->setChecked( p.enableInterfaceFont );
 
   if ( p.interfaceFontSize > 0 ) {
     ui.interfaceFontSize->setValue( p.interfaceFontSize );
@@ -417,6 +418,9 @@ Preferences::Preferences( QWidget * parent, Config::Class & cfg_ ):
 }
 void Preferences::previewInterfaceFont( QString family, int size )
 {
+  if ( !cfg.preferences.enableInterfaceFont ) {
+    return;
+  }
   QFont f = QApplication::font();
   f.setFamily( family );
   f.setPixelSize( size );
@@ -441,6 +445,7 @@ Config::Preferences Preferences::getPreferences()
 
   p.interfaceFont = ui.systemFont->currentText();
   p.interfaceFontSize = ui.interfaceFontSize->value();
+  p.enableInterfaceFont = ui.enableInterfaceFont->isChecked();
 
   Config::CustomFonts c;
   c.standard    = ui.font_standard->currentText();
