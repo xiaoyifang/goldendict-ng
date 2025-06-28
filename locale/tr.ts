c0b1408bc682dcebb4cc977f9c1a280b25c2fed8
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 19:16:51 +0800

    New translations Turkish from Crowdin

--- locale/tr.ts
+++ locale/tr.ts
@@ -3985,20 +3985,28 @@ Mevcut dillerin tam listesi burada bulunabilir &lt;a href=&quot;https://linguali
       <translation>Kaldırmayı Onayla</translation>
     </message>
     <message>
-      <source>Remove directory &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Listeden &lt;b&gt;%1 &lt;/b&gt; dizini kaldırılsın mı?</translation>
-    </message>
-    <message>
-      <source>Remove site &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Listeden &lt;b&gt;%1 &lt;/b&gt; sitesi kaldırılsın mı?</translation>
-    </message>
-    <message>
-      <source>Remove program &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Programı &lt;b&gt;%1&lt;/b&gt; listeden çıkar?</translation>
-    </message>
-    <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
       <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
+    </message>
+    <message>
+      <source>Remove selected directories from the list?</source>
+      <translation type="unfinished">Remove selected directories from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 directories from the list?</source>
+      <translation type="unfinished">Remove %1 directories from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 sites from the list?</source>
+      <translation type="unfinished">Remove %1 sites from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 servers from the list?</source>
+      <translation type="unfinished">Remove %1 servers from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 programs from the list?</source>
+      <translation type="unfinished">Remove %1 programs from the list?</translation>
     </message>
   </context>
   <context>
