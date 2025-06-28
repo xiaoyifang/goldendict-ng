4409c2e4c6fae47d61339294c5ea580006305d99
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 19:17:12 +0800

    New translations Interlingue from Crowdin

--- locale/ie.ts
+++ locale/ie.ts
@@ -3991,20 +3991,28 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
       <translation>Confirm removal</translation>
     </message>
     <message>
-      <source>Remove directory &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Remover li fólder &lt;b&gt;%1&lt;/b&gt; ex li list?</translation>
-    </message>
-    <message>
-      <source>Remove site &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Remover li site &lt;b&gt;%1&lt;/b&gt; ex li liste?</translation>
-    </message>
-    <message>
-      <source>Remove program &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-      <translation>Remover li programa &lt;b&gt;%1&lt;/b&gt; ex li liste?</translation>
-    </message>
-    <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
       <translation/>
+    </message>
+    <message>
+      <source>Remove selected directories from the list?</source>
+      <translation type="unfinished">Remove selected directories from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 directories from the list?</source>
+      <translation type="unfinished">Remove %1 directories from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 sites from the list?</source>
+      <translation type="unfinished">Remove %1 sites from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 servers from the list?</source>
+      <translation type="unfinished">Remove %1 servers from the list?</translation>
+    </message>
+    <message>
+      <source>Remove %1 programs from the list?</source>
+      <translation type="unfinished">Remove %1 programs from the list?</translation>
     </message>
   </context>
   <context>
