c4ff15b0b1c875773e0821cc89b136cdb2f638a9
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 20:14:33 +0800

    New translations Spanish, Argentina from Crowdin

--- locale/es_AR.ts
+++ locale/es_AR.ts
@@ -2882,10 +2882,6 @@ the application.</source>
       <translation>Apariencia</translation>
     </message>
     <message>
-      <source>Interface Font</source>
-      <translation>Fuente de interfaz</translation>
-    </message>
-    <message>
       <source>Turn the UI to dark.</source>
       <translation>Pon la interfaz de usuario en oscura.</translation>
     </message>
@@ -3443,10 +3439,6 @@ de los diccionarios Stardict, Babylon y GLS.</translation>
       <translation>Save debug messages to gd_log.txt in the config folder</translation>
     </message>
     <message>
-      <source>Interface Font Size</source>
-      <translation>Interface Font Size</translation>
-    </message>
-    <message>
       <source>Interface language</source>
       <translation>Interface language</translation>
     </message>
@@ -3469,6 +3461,18 @@ de los diccionarios Stardict, Babylon y GLS.</translation>
     <message>
       <source>GoldenDict-ng is a dictionary software.</source>
       <translation>GoldenDict-ng is a dictionary software.</translation>
+    </message>
+    <message>
+      <source>Enable Interface Font</source>
+      <translation type="unfinished">Enable Interface Font</translation>
+    </message>
+    <message>
+      <source>Font</source>
+      <translation type="unfinished">Font</translation>
+    </message>
+    <message>
+      <source>Font Size</source>
+      <translation type="unfinished">Font Size</translation>
     </message>
     <message>
       <source>None</source>
