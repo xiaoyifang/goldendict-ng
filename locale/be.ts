5089910d186175e721f100974bc0a726eb6cec18
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 11:32:31 +0800

    New translations Belarusian from Crowdin

--- locale/be.ts
+++ locale/be.ts
@@ -3459,6 +3459,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>GoldenDict-ng is a dictionary software.</source>
       <translation type="unfinished">GoldenDict-ng is a dictionary software.</translation>
     </message>
+    <message>
+      <source>None</source>
+      <translation>Няма</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
@@ -3985,13 +3989,6 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
     <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
       <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
-    </message>
-  </context>
-  <context>
-    <name>StylesComboBox</name>
-    <message>
-      <source>None</source>
-      <translation>Няма</translation>
     </message>
   </context>
   <context>
