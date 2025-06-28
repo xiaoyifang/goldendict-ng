61bd09e8dae43bdfa27dd38c71c1ea0657c63cc6
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 11:32:51 +0800

    New translations Serbian (Cyrillic) from Crowdin

--- locale/sr.ts
+++ locale/sr.ts
@@ -3474,6 +3474,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>GoldenDict-ng is a dictionary software.</source>
       <translation type="unfinished">GoldenDict-ng is a dictionary software.</translation>
     </message>
+    <message>
+      <source>None</source>
+      <translation>Ништа</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
@@ -4006,13 +4010,6 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
     <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
       <translation type="unfinished">The most widely used method of transcription of Japanese, based on English phonology</translation>
-    </message>
-  </context>
-  <context>
-    <name>StylesComboBox</name>
-    <message>
-      <source>None</source>
-      <translation>Ништа</translation>
     </message>
   </context>
   <context>
