0c401b5549b5097a97dbb00f80c3b1a34f7f79a9
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 11:33:09 +0800

    New translations Tajik from Crowdin

--- locale/tg.ts
+++ locale/tg.ts
@@ -3471,6 +3471,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>GoldenDict-ng is a dictionary software.</source>
       <translation>GoldenDict-ng is a dictionary software.</translation>
     </message>
+    <message>
+      <source>None</source>
+      <translation>Ҳеҷ чиз</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
@@ -4001,13 +4005,6 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
     <message>
       <source>The most widely used method of transcription of Japanese, based on English phonology</source>
       <translation>The most widely used method of transcription of Japanese, based on English phonology</translation>
-    </message>
-  </context>
-  <context>
-    <name>StylesComboBox</name>
-    <message>
-      <source>None</source>
-      <translation>Ҳеҷ чиз</translation>
     </message>
   </context>
   <context>
