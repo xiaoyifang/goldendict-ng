b99b4ccb9675062be55d2f1c434ff180fec9264e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-06-28 20:13:42 +0800

    New translations English from Crowdin

--- locale/crowdin.ts
+++ locale/crowdin.ts
@@ -3954,19 +3954,27 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Remove directory &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Remove site &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Remove program &lt;b&gt;%1&lt;/b&gt; from the list?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>The most widely used method of transcription of Japanese, based on English phonology</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove selected directories from the list?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove %1 directories from the list?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove %1 sites from the list?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove %1 servers from the list?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove %1 programs from the list?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
