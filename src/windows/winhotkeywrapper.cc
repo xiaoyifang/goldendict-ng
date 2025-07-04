9206baae9b2b86b24e997c6940a04b7de8d23b91
shenleban tongying <shenlebantongying@gmail.com>
2025-07-04 19:33:14 -0400

    decommission hotkeywrapper.cc, split implementations

--- src/windows/winhotkeywrapper.cc
+++ src/windows/winhotkeywrapper.cc
@@ -4,9 +4,142 @@
   #include "winhotkeyapplication.hh"
   #include <windows.h>
   #include <QWidget>
+  #include <QTimer>
 
 /// Implementation is pretty much using RegisterHotKey & UnregisterHotKey
 /// https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerhotkey
+
+
+HotkeyWrapper::HotkeyWrapper( QObject * parent ):
+  QThread( parent ),
+  state2( false )
+{
+  init();
+  ( static_cast< QHotkeyApplication * >( qApp ) )->registerWrapper( this );
+}
+
+HotkeyWrapper::~HotkeyWrapper()
+{
+  unregister();
+}
+
+void HotkeyWrapper::waitKey2()
+{
+  state2 = false;
+}
+
+bool HotkeyWrapper::checkState( quint32 vk, quint32 mod )
+{
+  if ( state2 ) { // wait for 2nd key
+
+    waitKey2(); // Cancel the 2nd-key wait stage
+
+    if ( state2waiter.key2 == vk && state2waiter.modifier == mod ) {
+      emit hotkeyActivated( state2waiter.handle );
+      return true;
+    }
+  }
+
+  for ( int i = 0; i < hotkeys.count(); i++ ) {
+
+    const HotkeyStruct & hs = hotkeys.at( i );
+
+    if ( hs.key == vk && hs.modifier == mod ) {
+
+      // If that was a copy-to-clipboard shortcut, re-emit it back so it could
+      // reach its original destination so it could be acted upon.
+      if ( hs.key2 != 0 || ( mod == MOD_CONTROL && ( vk == VK_INSERT || vk == 'c' || vk == 'C' ) ) ) {
+        // Pass-through first part of compound hotkey or clipdoard copy command
+
+        INPUT i[ 10 ];
+        memset( i, 0, sizeof( i ) );
+        int nextKeyNom       = 0;
+        short emulateModKeys = 0;
+
+        // If modifier keys aren't pressed it looks like emulation
+        // We then also emulate full sequence
+
+        if ( ( mod & MOD_ALT ) != 0 && ( GetAsyncKeyState( VK_MENU ) & 0x8000 ) == 0 ) {
+          emulateModKeys |= MOD_ALT;
+          i[ nextKeyNom ].type   = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk = VK_MENU;
+          nextKeyNom += 1;
+        }
+        if ( ( mod & MOD_CONTROL ) != 0 && ( GetAsyncKeyState( VK_CONTROL ) & 0x8000 ) == 0 ) {
+          emulateModKeys |= MOD_CONTROL;
+          i[ nextKeyNom ].type   = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk = VK_CONTROL;
+          nextKeyNom += 1;
+        }
+        if ( ( mod & MOD_SHIFT ) != 0 && ( GetAsyncKeyState( VK_SHIFT ) & 0x8000 ) == 0 ) {
+          emulateModKeys |= MOD_SHIFT;
+          i[ nextKeyNom ].type   = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk = VK_SHIFT;
+          nextKeyNom += 1;
+        }
+        if ( ( mod & MOD_WIN ) != 0 && ( GetAsyncKeyState( VK_LWIN ) & 0x8000 ) == 0
+             && ( GetAsyncKeyState( VK_RWIN ) & 0x8000 ) == 0 ) {
+          emulateModKeys |= MOD_WIN;
+          i[ nextKeyNom ].type   = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk = VK_LWIN;
+          nextKeyNom += 1;
+        }
+
+        i[ nextKeyNom ].type   = INPUT_KEYBOARD;
+        i[ nextKeyNom ].ki.wVk = vk;
+        nextKeyNom += 1;
+        i[ nextKeyNom ].type       = INPUT_KEYBOARD;
+        i[ nextKeyNom ].ki.wVk     = vk;
+        i[ nextKeyNom ].ki.dwFlags = KEYEVENTF_KEYUP;
+        nextKeyNom += 1;
+
+        if ( emulateModKeys & MOD_WIN ) {
+          i[ nextKeyNom ].type       = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk     = VK_LWIN;
+          i[ nextKeyNom ].ki.dwFlags = KEYEVENTF_KEYUP;
+          nextKeyNom += 1;
+        }
+        if ( emulateModKeys & MOD_SHIFT ) {
+          i[ nextKeyNom ].type       = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk     = VK_SHIFT;
+          i[ nextKeyNom ].ki.dwFlags = KEYEVENTF_KEYUP;
+          nextKeyNom += 1;
+        }
+        if ( emulateModKeys & MOD_CONTROL ) {
+          i[ nextKeyNom ].type       = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk     = VK_CONTROL;
+          i[ nextKeyNom ].ki.dwFlags = KEYEVENTF_KEYUP;
+          nextKeyNom += 1;
+        }
+        if ( emulateModKeys & MOD_ALT ) {
+          i[ nextKeyNom ].type       = INPUT_KEYBOARD;
+          i[ nextKeyNom ].ki.wVk     = VK_MENU;
+          i[ nextKeyNom ].ki.dwFlags = KEYEVENTF_KEYUP;
+          nextKeyNom += 1;
+        }
+
+        UnregisterHotKey( hwnd, hs.id );
+        SendInput( nextKeyNom, i, sizeof( *i ) );
+        RegisterHotKey( hwnd, hs.id, hs.modifier, hs.key );
+      }
+
+      if ( hs.key2 == 0 ) {
+        emit hotkeyActivated( hs.handle );
+        return true;
+      }
+
+      state2       = true;
+      state2waiter = hs;
+      QTimer::singleShot( 500, this, &HotkeyWrapper::waitKey2 );
+
+      return true;
+    }
+  }
+
+  state2 = false;
+  return false;
+}
+
 
 void HotkeyWrapper::init()
 {
