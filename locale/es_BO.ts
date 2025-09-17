c4ee1a5a3f90c35c4b221670e729cdd08ef649d4
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-17 09:51:44 +0800

    New translations Spanish, Bolivia from Crowdin

--- locale/es_BO.ts
+++ locale/es_BO.ts
@@ -254,6 +254,18 @@
       <source>&amp;Create Anki note</source>
       <translation>&amp;Crear nota de Anki</translation>
     </message>
+    <message>
+      <source>Error Loading</source>
+      <translation type="unfinished">Error Loading</translation>
+    </message>
+    <message>
+      <source>Page Load Failed</source>
+      <translation type="unfinished">Page Load Failed</translation>
+    </message>
+    <message>
+      <source>Unable to load the requested page content</source>
+      <translation type="unfinished">Unable to load the requested page content</translation>
+    </message>
   </context>
   <context>
     <name>BelarusianTranslit</name>
@@ -3473,6 +3485,10 @@ de los diccionarios Stardict, Babylon y GLS.</translation>
     <message>
       <source>None</source>
       <translation>Ningún</translation>
+    </message>
+    <message>
+      <source>Open website dictionary in seperate tab</source>
+      <translation type="unfinished">Open website dictionary in seperate tab</translation>
     </message>
   </context>
   <context>
