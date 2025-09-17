03e9d5c591cca72f4cbb8eace75caa686fa166dd
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-17 09:51:34 +0800

    New translations Chinese Traditional from Crowdin

--- locale/zh_TW.ts
+++ locale/zh_TW.ts
@@ -254,6 +254,18 @@
       <source>&amp;Create Anki note</source>
       <translation>製作anki note</translation>
     </message>
+    <message>
+      <source>Error Loading</source>
+      <translation type="unfinished">Error Loading</translation>
+    </message>
+    <message>
+      <source>Page Load Failed</source>
+      <translation type="unfinished">Page Load Failed</translation>
+    </message>
+    <message>
+      <source>Unable to load the requested page content</source>
+      <translation type="unfinished">Unable to load the requested page content</translation>
+    </message>
   </context>
   <context>
     <name>BelarusianTranslit</name>
@@ -3468,6 +3480,10 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>None</source>
       <translation>無</translation>
+    </message>
+    <message>
+      <source>Open website dictionary in seperate tab</source>
+      <translation type="unfinished">Open website dictionary in seperate tab</translation>
     </message>
   </context>
   <context>
