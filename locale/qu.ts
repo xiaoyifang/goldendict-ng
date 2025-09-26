c524d52a7e096a20a57bfbac8d5d6ebd3f1f701f
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-26 17:55:36 +0800

    New translations Quechua from Crowdin

--- locale/qu.ts
+++ locale/qu.ts
@@ -4125,18 +4125,17 @@ Lliw simikuna tarikuq listataqa tarinki &lt;a href=&quot;https://lingualibre.org
     </message>
   </context>
   <context>
-    <name>WebSitesModel</name>
+    <name>WebSite</name>
     <message>
-      <source>Insert article as link inside &lt;iframe&gt; tag</source>
-      <translation>&lt;iframe&gt; etiqueta ukhupi t'inki hina qillqasqata churay</translation>
+      <source>This website dictionary is opened in a new tab</source>
+      <translation type="unfinished">This website dictionary is opened in a new tab</translation>
     </message>
+  </context>
+  <context>
+    <name>WebSitesModel</name>
     <message>
       <source>Enabled</source>
       <translation>Atichisqa</translation>
-    </message>
-    <message>
-      <source>As link</source>
-      <translation>Enlace hina</translation>
     </message>
     <message>
       <source>Name</source>
