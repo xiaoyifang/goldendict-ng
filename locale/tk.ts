657482159a7135b19744a1eec3970494315abcb5
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-26 17:55:38 +0800

    New translations Turkmen from Crowdin

--- locale/tk.ts
+++ locale/tk.ts
@@ -4126,18 +4126,17 @@ tapyp bilersiňiz &lt;/a&gt;</translation>
     </message>
   </context>
   <context>
-    <name>WebSitesModel</name>
+    <name>WebSite</name>
     <message>
-      <source>Insert article as link inside &lt;iframe&gt; tag</source>
-      <translation>Makalany &lt;iframe&gt; belligiň içinde baglanyşyk hökmünde salyň</translation>
+      <source>This website dictionary is opened in a new tab</source>
+      <translation type="unfinished">This website dictionary is opened in a new tab</translation>
     </message>
+  </context>
+  <context>
+    <name>WebSitesModel</name>
     <message>
       <source>Enabled</source>
       <translation>Işledilen</translation>
-    </message>
-    <message>
-      <source>As link</source>
-      <translation>Baglanyşyk hökmünde</translation>
     </message>
     <message>
       <source>Name</source>
