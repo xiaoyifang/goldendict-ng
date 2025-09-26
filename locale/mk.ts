16a8a1b8f49ecebecdf926deecc9ea9a5883c063
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-26 17:55:09 +0800

    New translations Macedonian from Crowdin

--- locale/mk.ts
+++ locale/mk.ts
@@ -4134,18 +4134,17 @@ Full list of availiable languages can be found &lt;a href=&quot;https://linguali
     </message>
   </context>
   <context>
-    <name>WebSitesModel</name>
+    <name>WebSite</name>
     <message>
-      <source>Insert article as link inside &lt;iframe&gt; tag</source>
-      <translation>Вметнете статија како врска во ознаката &lt;iframe&gt;</translation>
+      <source>This website dictionary is opened in a new tab</source>
+      <translation type="unfinished">This website dictionary is opened in a new tab</translation>
     </message>
+  </context>
+  <context>
+    <name>WebSitesModel</name>
     <message>
       <source>Enabled</source>
       <translation>Вклучено</translation>
-    </message>
-    <message>
-      <source>As link</source>
-      <translation>Како врска</translation>
     </message>
     <message>
       <source>Name</source>
