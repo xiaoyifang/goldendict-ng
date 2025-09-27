04d8f79f8ffa60aaa27b62fb9996a97acc0d1ce8
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-27 20:37:51 +0800

    New translations English from Crowdin

--- locale/crowdin.ts
+++ locale/crowdin.ts
@@ -3610,6 +3610,14 @@ from Stardict, Babylon and GLS dictionaries</source>
         <source>Force the word to be translated in the mainwindow.</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Website Url:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>In the Url, &quot;%GDWORD%&quot; will be replaced with the word being searched.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QuickFilterLine</name>
