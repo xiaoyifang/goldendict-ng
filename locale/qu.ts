ce9897dccc499851c452628b3bfb7b2543eb90a0
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-27 20:38:24 +0800

    New translations Quechua from Crowdin

--- locale/qu.ts
+++ locale/qu.ts
@@ -3635,6 +3635,14 @@ kaqninta Stardict, Babylon chaymanta GLS simi pirwakunamanta</translation>
       <source>Force the word to be translated in the mainwindow.</source>
       <translation type="unfinished">Force the word to be translated in the mainwindow.</translation>
     </message>
+    <message>
+      <source>Website Url:</source>
+      <translation type="unfinished">Website Url:</translation>
+    </message>
+    <message>
+      <source>In the Url, &quot;%GDWORD%&quot; will be replaced with the word being searched.</source>
+      <translation type="unfinished">In the Url, &quot;%GDWORD%&quot; will be replaced with the word being searched.</translation>
+    </message>
   </context>
   <context>
     <name>QuickFilterLine</name>
