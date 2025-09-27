42753d399838614f930d881edba8dbd26002e142
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-27 20:38:16 +0800

    New translations Hindi from Crowdin

--- locale/hi.ts
+++ locale/hi.ts
@@ -3622,6 +3622,14 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>Force the word to be translated in the mainwindow.</source>
       <translation type="unfinished">Force the word to be translated in the mainwindow.</translation>
     </message>
+    <message>
+      <source>Website Url:</source>
+      <translation type="unfinished">Website Url:</translation>
+    </message>
+    <message>
+      <source>In the Url, &quot;%GDWORD%&quot; will be replaced with the word being searched.</source>
+      <translation type="unfinished">In the Url, &quot;%GDWORD%&quot; will be replaced with the word being searched.</translation>
+    </message>
   </context>
   <context>
     <name>QuickFilterLine</name>
