3b0b16a1f04547e9b13778ee69f5644448194191
xiaoyifang <yifang.xiao@foxmail.com>
2025-09-28 17:14:00 +0800

    1

--- src/ui/edit_sources_models.cc
+++ src/ui/edit_sources_models.cc
@@ -2,7 +2,6 @@
  * Part of GoldenDict. Licensed under GPLv3 or later, see the LICENSE file */
 
 #include "edit_sources_models.hh"
-#include "gddebug.hh"
 #include "globalbroadcaster.hh"
 #include <QFileDialog>
 #include <QMessageBox>
