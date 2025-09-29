c560eb9c827275f6d4491de8870be785f7c252a9
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-29 10:58:25 +0800

    New translations Spanish, Bolivia from Crowdin

--- locale/es_BO.ts
+++ locale/es_BO.ts
@@ -4156,6 +4156,18 @@ Lista completa de idiomas disponibles puede encontrarse &lt;a href=&quot;https:/
       <source>Icon</source>
       <translation>Icono</translation>
     </message>
+    <message>
+      <source>Script</source>
+      <translation type="unfinished">Script</translation>
+    </message>
+    <message>
+      <source>Icon file name. Relative to the config directory.</source>
+      <translation type="unfinished">Icon file name. Relative to the config directory.</translation>
+    </message>
+    <message>
+      <source>Only available when opening websites in separate tabs. Can be a file path (relative to config directory or absolute) or direct script content.</source>
+      <translation type="unfinished">Only available when opening websites in separate tabs. Can be a file path (relative to config directory or absolute) or direct script content.</translation>
+    </message>
   </context>
   <context>
     <name>WordFinder</name>
