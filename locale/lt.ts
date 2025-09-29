49ac4ff35cf66cc0140d734841ffbdef4db8fbcd
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-09-29 10:58:00 +0800

    New translations Lithuanian from Crowdin

--- locale/lt.ts
+++ locale/lt.ts
@@ -4157,6 +4157,18 @@ visą galimų kalbų sąrašą rasite &lt;a href=&quot;https://lingualibre.org/w
       <source>Icon</source>
       <translation>Ženkliukas</translation>
     </message>
+    <message>
+      <source>Script</source>
+      <translation type="unfinished">Script</translation>
+    </message>
+    <message>
+      <source>Icon file name. Relative to the config directory.</source>
+      <translation type="unfinished">Icon file name. Relative to the config directory.</translation>
+    </message>
+    <message>
+      <source>Only available when opening websites in separate tabs. Can be a file path (relative to config directory or absolute) or direct script content.</source>
+      <translation type="unfinished">Only available when opening websites in separate tabs. Can be a file path (relative to config directory or absolute) or direct script content.</translation>
+    </message>
   </context>
   <context>
     <name>WordFinder</name>
