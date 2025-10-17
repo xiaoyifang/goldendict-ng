a2d76825642ff4e02c6f3109aa108b0191044a76
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-10-17 19:21:57 +0800

    New translations Greek from Crowdin

--- locale/el.ts
+++ locale/el.ts
@@ -2681,6 +2681,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
       <source>PDF document (*.pdf *.PDF)</source>
       <translation>PDF έγγραφο (*.pdf *.PDF)</translation>
     </message>
+    <message>
+      <source>Lock Panels</source>
+      <translation>Κλείδωμα Πάνελ</translation>
+    </message>
   </context>
   <context>
     <name>Mdx::MdxArticleRequest</name>
