ac11ceefde8284f890df65a9ae1a9734440d8063
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-01 12:50:19 +0800

    New translations German from Crowdin (#2565)

--- locale/de.ts
+++ locale/de.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation>Anki: kann&apos;t eine Karte ohne Wort erstellen</translation>
+      <translation>Anki: Ohne Wort kann keine Karte erstellt werden</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
