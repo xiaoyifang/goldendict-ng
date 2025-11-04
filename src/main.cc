9fa317e59f22cca564af8e172512fc5a69d72a63
xiaoyifang <yifang.xiao@foxmail.com>
2025-11-04 18:12:18 +0800

    Improve translation loading logic to ensure translator is only installed if loading succeeds

--- src/main.cc
+++ src/main.cc
@@ -484,15 +484,15 @@ int main( int argc, char ** argv )
     // If interfaceLanguage is explicitly set, uses filename-based loading, because GD have more languages than Qt & its locale database.
     // If not, then let Qt's qlocale mechanism decide which one to use, because "locale" handling is different in all 3 platforms, and we don't want to deal with that.
 
-    if ( cfg.preferences.interfaceLanguage.isEmpty() ) {
-      loadTranslation_qlocale( *gd_ts, QString(), QString(), Config::getLocDir() );
-    }
-    else if ( cfg.preferences.interfaceLanguage != "en" ) {
-      gd_ts->load( cfg.preferences.interfaceLanguage, Config::getLocDir() );
+    bool loaded = false;
+    if (cfg.preferences.interfaceLanguage.isEmpty()) {
+        loaded = loadTranslation_qlocale(*gd_ts, QString(), QString(), Config::getLocDir());
+    } else if (cfg.preferences.interfaceLanguage != "en") {
+        loaded = gd_ts->load(cfg.preferences.interfaceLanguage, Config::getLocDir());
     }
 
     // Only install translator if loading succeeds
-    if ( !gd_ts->isEmpty() ) {
+    if (loaded) {
       QCoreApplication::installTranslator( gd_ts );
       qDebug() << "TS found: " << gd_ts->filePath();
 
