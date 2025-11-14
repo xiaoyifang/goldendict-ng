90b5c04ab055b12a2a1d679f6b13d61b2de92727
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-14 13:58:13 +0800

    fix(utils): correct URL encoding in urlReplaceWord function (#2587)
    * fix(utils): correct URL encoding in urlReplaceWord function
    
    - Remove extra percent encoding in URL template replacement
    - Simplify URL handling in website dictionary implementation
    - Ensure proper encoding of search terms in URLs

--- src/dict/website.cc
+++ src/dict/website.cc
@@ -32,7 +32,7 @@ public:
     iconFilename( iconFilename_ )
   {
     dictionaryName        = name_;
-    urlTemplate           = QUrl( urlTemplate_ ).toEncoded();
+    urlTemplate           = urlTemplate_.toUtf8();
     dictionaryDescription = QObject::tr( "Website Url:" ) + urlTemplate_ + "<br><br>"
       + QObject::tr( "In the Url, \"%GDWORD%\" will be replaced with the word being searched." );
   }
