f438a478055b6eda0d26a4312ea2ac22847d46bb
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-14 21:49:08 +0800

    New translations English from Crowdin

--- locale/en.ts
+++ locale/en.ts
@@ -489,6 +489,14 @@ between classic and school orthography in cyrillic)</translation>
       <source>Combine groups with &quot;%1&quot;</source>
       <translation type="unfinished">Combine groups with &quot;%1&quot;</translation>
     </message>
+    <message>
+      <source>Remove group</source>
+      <translation type="unfinished">Remove group</translation>
+    </message>
+    <message>
+      <source>Are you sure you want to remove the group &lt;b&gt;%1&lt;/b&gt;?</source>
+      <translation type="unfinished">Are you sure you want to remove the group &lt;b&gt;%1&lt;/b&gt;?</translation>
+    </message>
   </context>
   <context>
     <name>DictHeadwords</name>
@@ -1035,28 +1043,12 @@ between classic and school orthography in cyrillic)</translation>
       <translation type="unfinished">Tab 2</translation>
     </message>
     <message>
-      <source>Create new dictionary group</source>
-      <translation type="unfinished">Create new dictionary group</translation>
-    </message>
-    <message>
-      <source>&amp;Add group</source>
-      <translation type="unfinished">&amp;Add group</translation>
-    </message>
-    <message>
       <source>Rename current dictionary group</source>
       <translation type="unfinished">Rename current dictionary group</translation>
     </message>
     <message>
       <source>Re&amp;name group</source>
       <translation type="unfinished">Re&amp;name group</translation>
-    </message>
-    <message>
-      <source>Remove current dictionary group</source>
-      <translation type="unfinished">Remove current dictionary group</translation>
-    </message>
-    <message>
-      <source>&amp;Remove group</source>
-      <translation type="unfinished">&amp;Remove group</translation>
     </message>
     <message>
       <source>Remove all dictionary groups</source>
