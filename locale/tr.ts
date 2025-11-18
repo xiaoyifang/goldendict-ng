0a516b5f6d7871ac53ed5b2a3038f064c5e85b11
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-18 17:53:02 +0800

    New translations Turkish from Crowdin

--- locale/tr.ts
+++ locale/tr.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
+      <translation>Anki: kelime olmadan kart oluşturulamaz</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
