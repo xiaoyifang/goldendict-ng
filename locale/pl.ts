bfd236be3840745390e319ca4f8cc8ef6fe9722e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-18 17:52:58 +0800

    New translations Polish from Crowdin

--- locale/pl.ts
+++ locale/pl.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation>Anki: można&apos;t utworzyć kartę bez słowa</translation>
+      <translation>Anki: nie można utworzyć karty bez słowa</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
