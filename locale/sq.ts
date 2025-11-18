626ee94e44807c5c96f3c8a1639963ee97096942
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-18 12:15:22 +0800

    New translations Albanian from Crowdin

--- locale/sq.ts
+++ locale/sq.ts
@@ -20,7 +20,7 @@
     <name>AnkiConnector</name>
     <message>
       <source>Anki: can&apos;t create a card without a word</source>
-      <translation type="unfinished">Anki: can&apos;t create a card without a word</translation>
+      <translation>Anki: nuk mund të krijosh një kartë pa një fjalë</translation>
     </message>
     <message>
       <source>Anki search: AnkiConnect is not enabled.</source>
