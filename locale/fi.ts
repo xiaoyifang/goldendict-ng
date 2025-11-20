a6438ce04e48c4d91d0796d333aaa1fd647cf860
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-20 17:56:52 +0800

    New translations Finnish from Crowdin

--- locale/fi.ts
+++ locale/fi.ts
@@ -2792,14 +2792,6 @@ vaihtamista niihin.</translation>
       <translation>Avaa uudet välilehdet taustalla</translation>
     </message>
     <message>
-      <source>With this on, new tabs are opened just after the
-current, active one. Otherwise they are added to
-be the last ones.</source>
-      <translation>Tämän avulla avataan uudet välilehdet heti
-virtauksen jälkeen, aktiivinen välilehti. Muuten ne lisätään
-ovat viimeisiä.</translation>
-    </message>
-    <message>
       <source>Open new tabs after the current one</source>
       <translation>Avaa uudet välilehdet nykyisen välilehtien jälkeen</translation>
     </message>
@@ -2898,8 +2890,124 @@ sovellus.</translation>
       <translation>Kontekstivalikon sanakirjojen raja:</translation>
     </message>
     <message>
+      <source>System default</source>
+      <translation>Järjestelmän oletus</translation>
+    </message>
+    <message>
+      <source>Default</source>
+      <translation>Oletus</translation>
+    </message>
+    <message>
+      <source>Classic</source>
+      <translation>Klassinen</translation>
+    </message>
+    <message>
+      <source>Modern</source>
+      <translation>Moderni</translation>
+    </message>
+    <message>
+      <source>Lingvo</source>
+      <translation>Lingvo</translation>
+    </message>
+    <message>
+      <source>Babylon</source>
+      <translation>Babylon</translation>
+    </message>
+    <message>
+      <source>Lingoes</source>
+      <translation>Lingot</translation>
+    </message>
+    <message>
+      <source>Lingoes-Blue</source>
+      <translation>Lingo-sininen</translation>
+    </message>
+    <message>
+      <source>Auto does nothing on some systems.</source>
+      <translation>Auto ei tee mitään joissain järjestelmissä.</translation>
+    </message>
+    <message>
+      <source>Restart the program to apply the language change.</source>
+      <translation>Ohjelma pitää käynnistää uudelleen jotta kieli voidaan vaihtaa.</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface style change.</source>
+      <translation>Käynnistä käyttöliittymän tyylin muutos uudelleen.</translation>
+    </message>
+    <message>
+      <source>Restart to apply the interface font change.</source>
+      <translation>Käynnistä uudelleen ottaaksesi käyttöön käyttöliittymän kirjasimen muutokset.</translation>
+    </message>
+    <message>
+      <source>Restart needed</source>
+      <translation>Uudelleenkäynnistys tarvitaan</translation>
+    </message>
+    <message>
+      <source>Enable</source>
+      <translation>Ota käyttöön</translation>
+    </message>
+    <message>
+      <source>Disable</source>
+      <translation>Poista käytöstä</translation>
+    </message>
+    <message>
+      <source>Automatic</source>
+      <translation>Automaattinen</translation>
+    </message>
+    <message>
+      <source>None</source>
+      <translation>Tyhjä</translation>
+    </message>
+    <message>
+      <source>Select this option if you don&apos;t want to see the main tab bar when only a single tab is
+                  opened.
+                 </source>
+      <translation type="unfinished">Select this option if you don&apos;t want to see the main tab bar when only a single tab is
+                  opened.
+                 </translation>
+    </message>
+    <message>
+      <source>With this on, new tabs are opened just after the
+current, active one. Otherwise, they are added to
+be the last ones.</source>
+      <translation>Tämän avulla uudet välilehdet avataan juuri
+-virran jälkeen. Muuten ne lisätään
+ovat viimeisiä.</translation>
+    </message>
+    <message>
+      <source>&amp;Style</source>
+      <translation>&amp;Tyyli</translation>
+    </message>
+    <message>
       <source>Appearance</source>
       <translation>Ulkomuoto</translation>
+    </message>
+    <message>
+      <source>Enable Interface Font</source>
+      <translation>Ota Käyttöön Käyttöliittymän Kirjasin</translation>
+    </message>
+    <message>
+      <source>Font</source>
+      <translation>Font</translation>
+    </message>
+    <message>
+      <source>Font Size</source>
+      <translation>Font Size</translation>
+    </message>
+    <message>
+      <source>Interface language</source>
+      <translation>Käyttöliittymän kieli</translation>
+    </message>
+    <message>
+      <source>Article Display style</source>
+      <translation>Artikkelin esitystyyli</translation>
+    </message>
+    <message>
+      <source>Add-on style</source>
+      <translation>Lisäosan tyyli</translation>
+    </message>
+    <message>
+      <source>Interface Style</source>
+      <translation>Käyttöliittymän Tyyli</translation>
     </message>
     <message>
       <source>Turn the UI to dark.</source>
@@ -2916,6 +3024,14 @@ sovellus.</translation>
     <message>
       <source>Dark Reader Mode</source>
       <translation>Tumma Lukija -tila</translation>
+    </message>
+    <message>
+      <source>Preview Font</source>
+      <translation>Esikatsele Kirjasinta</translation>
+    </message>
+    <message>
+      <source>GoldenDict-ng is a dictionary software.</source>
+      <translation>GoldenDict-ng on sanakirja ohjelmisto.</translation>
     </message>
     <message>
       <source>These fonts will be applied when the fonts specified by a dictionary are not found.</source>
@@ -3278,20 +3394,20 @@ lataussivun.</translation>
       <translation>Historian enimmäiskoko:</translation>
     </message>
     <message>
-      <source>Save every</source>
-      <translation>Tallenna joka</translation>
-    </message>
-    <message>
-      <source>minutes</source>
-      <translation>minuuttia</translation>
-    </message>
-    <message>
       <source>Favorites</source>
       <translation>Suosikit</translation>
     </message>
     <message>
       <source>Favorites saving interval. If set to 0 Favorites will be saved only during exit.</source>
       <translation>Suosikkien tallennusväli. Jos arvo on 0 Suosikkia, tallennetaan vain ulostulon aikana.</translation>
+    </message>
+    <message>
+      <source>Save every</source>
+      <translation>Tallenna joka</translation>
+    </message>
+    <message>
+      <source>minutes</source>
+      <translation>minuuttia</translation>
     </message>
     <message>
       <source>Turn this option on to confirm every operation of items deletion</source>
@@ -3390,124 +3506,12 @@ alkaen alkaen alkupisteestä, Babylonista ja GLS-sanakirjoista</translation>
       <translation>Poista virheellinen indeksi poistuessasi</translation>
     </message>
     <message>
-      <source>System default</source>
-      <translation>Järjestelmän oletus</translation>
-    </message>
-    <message>
-      <source>Default</source>
-      <translation>Oletus</translation>
-    </message>
-    <message>
-      <source>Classic</source>
-      <translation>Klassinen</translation>
-    </message>
-    <message>
-      <source>Modern</source>
-      <translation>Moderni</translation>
-    </message>
-    <message>
-      <source>Lingvo</source>
-      <translation>Lingvo</translation>
-    </message>
-    <message>
-      <source>Babylon</source>
-      <translation>Babylon</translation>
-    </message>
-    <message>
-      <source>Lingoes</source>
-      <translation>Lingot</translation>
-    </message>
-    <message>
-      <source>Lingoes-Blue</source>
-      <translation>Lingo-sininen</translation>
-    </message>
-    <message>
-      <source>Auto does nothing on some systems.</source>
-      <translation>Auto ei tee mitään joissain järjestelmissä.</translation>
-    </message>
-    <message>
-      <source>Restart the program to apply the language change.</source>
-      <translation>Ohjelma pitää käynnistää uudelleen jotta kieli voidaan vaihtaa.</translation>
-    </message>
-    <message>
-      <source>Restart to apply the interface style change.</source>
-      <translation>Käynnistä käyttöliittymän tyylin muutos uudelleen.</translation>
-    </message>
-    <message>
-      <source>Restart to apply the interface font change.</source>
-      <translation>Käynnistä uudelleen ottaaksesi käyttöön käyttöliittymän kirjasimen muutokset.</translation>
-    </message>
-    <message>
-      <source>Restart needed</source>
-      <translation>Uudelleenkäynnistys tarvitaan</translation>
-    </message>
-    <message>
-      <source>Enable</source>
-      <translation>Ota käyttöön</translation>
-    </message>
-    <message>
-      <source>Disable</source>
-      <translation>Poista käytöstä</translation>
-    </message>
-    <message>
-      <source>Automatic</source>
-      <translation>Automaattinen</translation>
-    </message>
-    <message>
       <source>Save debug messages to gd_log.txt in the config folder</source>
       <translation>Tallenna debug viestit gd_log.txt asetuskansioon</translation>
     </message>
     <message>
-      <source>Interface language</source>
-      <translation>Käyttöliittymän kieli</translation>
-    </message>
-    <message>
-      <source>Article Display style</source>
-      <translation>Artikkelin esitystyyli</translation>
-    </message>
-    <message>
-      <source>Add-on style</source>
-      <translation>Lisäosan tyyli</translation>
-    </message>
-    <message>
-      <source>Interface Style</source>
-      <translation>Käyttöliittymän Tyyli</translation>
-    </message>
-    <message>
-      <source>Preview Font</source>
-      <translation>Esikatsele Kirjasinta</translation>
-    </message>
-    <message>
-      <source>GoldenDict-ng is a dictionary software.</source>
-      <translation>GoldenDict-ng on sanakirja ohjelmisto.</translation>
-    </message>
-    <message>
-      <source>Enable Interface Font</source>
-      <translation>Ota Käyttöön Käyttöliittymän Kirjasin</translation>
-    </message>
-    <message>
-      <source>Font</source>
-      <translation>Font</translation>
-    </message>
-    <message>
-      <source>Font Size</source>
-      <translation>Font Size</translation>
-    </message>
-    <message>
-      <source>None</source>
-      <translation>Tyhjä</translation>
-    </message>
-    <message>
       <source>Open website dictionary in seperate tab</source>
       <translation>Avaa verkkosivuston sanakirja erillisessä välilehdessä</translation>
-    </message>
-    <message>
-      <source>Select this option if you don&apos;t want to see the main tab bar when only a single tab is
-                  opened.
-                 </source>
-      <translation type="unfinished">Select this option if you don&apos;t want to see the main tab bar when only a single tab is
-                  opened.
-                 </translation>
     </message>
   </context>
   <context>
