d05fb441ce91a6ff6b88e5653893aed73bae4db2
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-21 12:05:25 +0800

    New translations Hungarian from Crowdin

--- locale/hu.ts
+++ locale/hu.ts
@@ -779,6 +779,14 @@ a klasszikus és az iskolai helyesírás közt cirillben)</translation>
       <source>The dictionary has been scheduled for reindexing. The index will be rebuilt on the next application restart.</source>
       <translation type="unfinished">The dictionary has been scheduled for reindexing. The index will be rebuilt on the next application restart.</translation>
     </message>
+    <message>
+      <source>Change display name</source>
+      <translation type="unfinished">Change display name</translation>
+    </message>
+    <message>
+      <source>New display name:</source>
+      <translation type="unfinished">New display name:</translation>
+    </message>
   </context>
   <context>
     <name>EditDictionaries</name>
@@ -2748,6 +2756,14 @@ A &apos;*&apos;, &apos;?&apos;, &apos;[&apos; és &apos;]&apos; karakterek keres
     <message>
       <source>Dictionaries active: %1, inactive: %2</source>
       <translation>Aktív szótárak: %1, inaktív: %2</translation>
+    </message>
+    <message>
+      <source>Change display name</source>
+      <translation type="unfinished">Change display name</translation>
+    </message>
+    <message>
+      <source>New display name:</source>
+      <translation type="unfinished">New display name:</translation>
     </message>
   </context>
   <context>
