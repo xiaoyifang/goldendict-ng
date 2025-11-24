2bdbe5367557dc49732e9cf646826b9a91ffb5b1
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-24 17:49:04 +0800

    New translations Esperanto from Crowdin

--- locale/eo.ts
+++ locale/eo.ts
@@ -285,18 +285,6 @@
       <translation>&amp;Krei Anki-noton</translation>
     </message>
     <message>
-      <source>Error Loading</source>
-      <translation type="unfinished">Error Loading</translation>
-    </message>
-    <message>
-      <source>Page Load Failed</source>
-      <translation type="unfinished">Page Load Failed</translation>
-    </message>
-    <message>
-      <source>Unable to load the requested page content</source>
-      <translation type="unfinished">Unable to load the requested page content</translation>
-    </message>
-    <message>
       <source>&amp;Copy sound to clipboard</source>
       <translation type="unfinished">&amp;Copy sound to clipboard</translation>
     </message>
@@ -3528,6 +3516,14 @@ el Stardict, Babylon kaj GLS-vortaroj</translation>
     <message>
       <source>S&amp;can</source>
       <translation type="unfinished">S&amp;can</translation>
+    </message>
+    <message>
+      <source>Suppress JavaScript alerts, confirms, and prompts in the article view.</source>
+      <translation type="unfinished">Suppress JavaScript alerts, confirms, and prompts in the article view.</translation>
+    </message>
+    <message>
+      <source>Suppress JavaScript dialogs</source>
+      <translation type="unfinished">Suppress JavaScript dialogs</translation>
     </message>
   </context>
   <context>
