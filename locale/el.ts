efab87928fe45d3414c012cd7819612ca3165841
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-11-30 14:35:25 +0800

    New translations Greek from Crowdin

--- locale/el.ts
+++ locale/el.ts
@@ -3711,16 +3711,16 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Αδύνατη η αποθήκευση του άρθρου: %1</translation>
     </message>
     <message>
-      <source>Saving article...</source>
-      <translation>Αποθήκευση άρθρου...</translation>
-    </message>
-    <message>
       <source>Save PDF complete</source>
       <translation>Αποθήκευση PDF ολοκληρώθηκε</translation>
     </message>
     <message>
       <source>Save PDF failed</source>
       <translation>Αποτυχία αποθήκευσης PDF</translation>
+    </message>
+    <message>
+      <source>Saving article... (%1/%2)</source>
+      <translation>Αποθήκευση άρθρου... (%1/%2)</translation>
     </message>
   </context>
   <context>
