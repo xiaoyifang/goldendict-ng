d32a753918bd7660cc64772399b7c99d9d7252e6
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2025-12-05 09:05:49 +0800

    fix: systemtrayicon warning on start (#2617)

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -1450,17 +1450,23 @@ void MainWindow::trayIconUpdateOrInit()
     }
   }
   else {
+    // Update the icon to reflect the scanning mode
+    QIcon icon = enableScanningAction->isChecked() ?
+      QIcon::fromTheme( "goldendict-scan-tray", QIcon( ":/icons/programicon_scan.png" ) ) :
+      QIcon::fromTheme( "goldendict-tray", QIcon( ":/icons/programicon_old.png" ) );
+
     if ( !trayIcon ) {
       trayIcon = new QSystemTrayIcon( this );
       trayIcon->setContextMenu( &trayIconMenu );
       trayIcon->setToolTip( QApplication::applicationName() );
+      trayIcon->setIcon( icon );
       connect( trayIcon, &QSystemTrayIcon::activated, this, &MainWindow::trayIconActivated );
       trayIcon->show();
     }
-    // Update the icon to reflect the scanning mode
-    trayIcon->setIcon( enableScanningAction->isChecked() ?
-                         QIcon::fromTheme( "goldendict-scan-tray", QIcon( ":/icons/programicon_scan.png" ) ) :
-                         QIcon::fromTheme( "goldendict-tray", QIcon( ":/icons/programicon_old.png" ) ) );
+    else {
+      // Update existing tray icon
+      trayIcon->setIcon( icon );
+    }
   }
 
   // The 'Close to tray' action is associated with the tray icon, so we hide
