3aeb11ed8217c592148369656e056624ae622049
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-01-05 08:53:00 +0800

    New translations Tajik from Crowdin

--- locale/tg.ts
+++ locale/tg.ts
@@ -3776,6 +3776,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>WARNING: %1</source>
       <translation>Огоҳӣ: %1</translation>
     </message>
+    <message>
+      <source>Definition</source>
+      <translation type="unfinished">Definition</translation>
+    </message>
   </context>
   <context>
     <name>ScanPopupToolBar</name>
