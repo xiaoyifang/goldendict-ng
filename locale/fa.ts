f0770379368e2f16ff3af741dd7b0fb3122f72ec
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-01-05 08:52:53 +0800

    New translations Persian from Crowdin

--- locale/fa.ts
+++ locale/fa.ts
@@ -3774,6 +3774,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>WARNING: %1</source>
       <translation>هشدار: %1</translation>
     </message>
+    <message>
+      <source>Definition</source>
+      <translation type="unfinished">Definition</translation>
+    </message>
   </context>
   <context>
     <name>ScanPopupToolBar</name>
