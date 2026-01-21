41573c5fcb7996e1ca5b41a0735bf83f722a2f53
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-01-21 12:39:46 +0800

    New translations German from Crowdin

--- locale/de.ts
+++ locale/de.ts
@@ -3310,32 +3310,6 @@ nicht mehr funktionieren, kann das Deaktivieren dieser Option helfen.</translati
       <translation>Inhalte nicht von fremden Seiten laden (unterbindet meist Werbung)</translation>
     </message>
     <message>
-      <source>Maximum network cache size:</source>
-      <translation>Maximale Netzwerk-Cache-Größe:</translation>
-    </message>
-    <message>
-      <source>Maximum disk space occupied by GoldenDict&apos;s network cache in
-%1
-If set to 0 the network disk cache will be disabled.</source>
-      <translation>Maximaler Speicherplatz für GoldenDict&apos;s Netzwerk-Cache in
-%1
-Wenn auf 0 gesetzt, wird der Netzwerk-Festplatten-Cache deaktiviert.</translation>
-    </message>
-    <message>
-      <source> MiB</source>
-      <translation> MiB</translation>
-    </message>
-    <message>
-      <source>When this option is enabled, GoldenDict
-clears its network cache from disk during exit.</source>
-      <translation>Wenn diese Option aktiviert ist, löscht GoldenDict
-den Netzwerk-Cache beim Beenden von der Festplatte.</translation>
-    </message>
-    <message>
-      <source>Clear network cache on exit</source>
-      <translation>Netzwerk-Cache beim Beenden löschen</translation>
-    </message>
-    <message>
       <source>When this is enabled, the program periodically
 checks if a new, updated version of GoldenDict
 is available for download. If it is so, the program
