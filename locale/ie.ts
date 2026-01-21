d678219ea36de8751f059f63f7ded1ba131bc32c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-01-21 12:40:24 +0800

    New translations Interlingue from Crowdin

--- locale/ie.ts
+++ locale/ie.ts
@@ -3314,32 +3314,6 @@ you are browsing. If some site breaks because of this, try disabling this.</tran
       <translation>Ne cargar contenete de altri sites (cela pluparte de reclams)</translation>
     </message>
     <message>
-      <source>Maximum network cache size:</source>
-      <translation>Maximum network cache size:</translation>
-    </message>
-    <message>
-      <source>Maximum disk space occupied by GoldenDict&apos;s network cache in
-%1
-If set to 0 the network disk cache will be disabled.</source>
-      <translation>Maximum disk space occupied by GoldenDict&apos;s network cache in
-%1
-If set to 0 the network disk cache will be disabled.</translation>
-    </message>
-    <message>
-      <source> MiB</source>
-      <translation> Mio</translation>
-    </message>
-    <message>
-      <source>When this option is enabled, GoldenDict
-clears its network cache from disk during exit.</source>
-      <translation>When this option is enabled, GoldenDict
-clears its network cache from disk during exit.</translation>
-    </message>
-    <message>
-      <source>Clear network cache on exit</source>
-      <translation>Clear network cache on exit</translation>
-    </message>
-    <message>
       <source>When this is enabled, the program periodically
 checks if a new, updated version of GoldenDict
 is available for download. If it is so, the program
