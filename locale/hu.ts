4c945080fc84c01ecc619f7c34be520ab778eb4e
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-03-16 12:15:09 +0800

    New translations Hungarian from Crowdin

--- locale/hu.ts
+++ locale/hu.ts
@@ -1122,6 +1122,14 @@ a klasszikus és az iskolai helyesírás közt cirillben)</translation>
       <source>&amp;Add group</source>
       <translation>Új &amp;csoport</translation>
     </message>
+    <message>
+      <source>Group already exists</source>
+      <translation type="unfinished">Group already exists</translation>
+    </message>
+    <message>
+      <source>A group with the name &apos;%1&apos; already exists.</source>
+      <translation type="unfinished">A group with the name &apos;%1&apos; already exists.</translation>
+    </message>
   </context>
   <context>
     <name>HistoryPaneWidget</name>
