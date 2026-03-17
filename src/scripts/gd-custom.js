c8d1d05f416ababaf3a041977c8332749784f604
xiaoyifang <yifang.xiao@foxmail.com>
2026-03-17 11:31:19 +0800

    opt: js file lint check

--- src/scripts/gd-custom.js
+++ src/scripts/gd-custom.js
@@ -43,12 +43,10 @@
 
         if (link.indexOf("?gdanchor") > -1) {
           newLink = "gdlookup://localhost/" + link;
+        } else if (index > -1) {
+          newLink = href.substring(0, index) + "?word=" + link;
         } else {
-          if (index > -1) {
-            newLink = href.substring(0, index) + "?word=" + link;
-          } else {
-            newLink = href + "?word=" + link;
-          }
+          newLink = href + "?word=" + link;
         }
       }
       $(this).attr("href", newLink);
