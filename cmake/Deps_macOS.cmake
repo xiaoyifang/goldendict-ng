8cfe25dc9ecd50d54fe50e65815816a223c00b16
xiaoyifang <yifang.xiao@foxmail.com>
2026-03-18 16:19:12 +0800

    build: simplify tomlplusplus dependency and remove USE_SYSTEM_TOML
    - Remove USE_SYSTEM_TOML option, favoring system/vcpkg version by default.
    - Remove conditional inclusion of bundled thirdparty/tomlplusplus.
    - Update vcpkg, linux, and macOS dependency configurations to require tomlplusplus.
    - Update GitHub Actions workflows to install tomlplusplus across all platforms:
      - Ubuntu/Debian: libtomlplusplus-dev
      - macOS: tomlplusplus (brew)
      - Arch Linux: tomlplusplus (pacman)

--- cmake/Deps_macOS.cmake
+++ cmake/Deps_macOS.cmake
@@ -7,9 +7,6 @@ target_link_libraries(${GOLDENDICT} PRIVATE ${CARBON_LIBRARY})
 find_package(PkgConfig REQUIRED)
 
 set(Optional_Pkgs "")
-if (USE_SYSTEM_TOML)
-    list(APPEND Optional_Pkgs "tomlplusplus")
-endif ()
 if (WITH_ZIM)
     list(APPEND Optional_Pkgs "libzim")
 endif ()
@@ -41,6 +38,7 @@ pkg_check_modules(DEPS REQUIRED IMPORTED_TARGET
         liblzma
         lzo2
         opencc
+        tomlplusplus
         vorbis # .ogg
         vorbisfile
         xapian-core
