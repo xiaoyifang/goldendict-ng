81fb65661ef5c518e5886a4dc334e892a2fe1ffe
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-04-07 19:56:28 +0800

    New translations Vietnamese from Crowdin

--- locale/vi.ts
+++ locale/vi.ts
@@ -3511,8 +3511,12 @@ từ các từ điển Stardict, Babylon và GLS</translation>
       <translation type="unfinished">Allow JavaScript to access clipboard in the article view.</translation>
     </message>
     <message>
-      <source>Enable JavaScript clipboard access</source>
-      <translation type="unfinished">Enable JavaScript clipboard access</translation>
+      <source>Enable JavaScript clipboard access (need restart)</source>
+      <translation type="unfinished">Enable JavaScript clipboard access (need restart)</translation>
+    </message>
+    <message>
+      <source>Restart to apply the JavaScript clipboard access change.</source>
+      <translation type="unfinished">Restart to apply the JavaScript clipboard access change.</translation>
     </message>
   </context>
   <context>
