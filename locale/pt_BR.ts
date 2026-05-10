4c74d9dd531d5cb64ffce912face1e4374afc51b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-10 20:09:01 +0800

    New translations Portuguese, Brazilian from Crowdin

--- locale/pt_BR.ts
+++ locale/pt_BR.ts
@@ -3124,8 +3124,8 @@ in the pressed state when the word selection changes.</source>
 teclas do atalho estiverem premidas quando a seleção de palavra mudar.</translation>
     </message>
     <message>
-      <source>Only tack selection when all selected keys are kept pressed:</source>
-      <translation>Apenas seleção tack quando todas as chaves selecionadas forem pressionadas:</translation>
+      <source>Only track selection when all selected keys are kept pressed:</source>
+      <translation>Apenas rastrear a seleção quando todas as chaves selecionadas forem pressionadas:</translation>
     </message>
     <message>
       <source>Alt key</source>
