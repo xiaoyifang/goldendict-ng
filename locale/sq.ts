7bb6c760e9a2b3d11ca34826d42cf9be35068b4b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-11 00:04:33 +0800

    New translations Albanian from Crowdin

--- locale/sq.ts
+++ locale/sq.ts
@@ -306,6 +306,13 @@
     </message>
   </context>
   <context>
+    <name>ArticleWebView</name>
+    <message>
+      <source>GoldenDict Resource Viewer</source>
+      <translation type="unfinished">GoldenDict Resource Viewer</translation>
+    </message>
+  </context>
+  <context>
     <name>BelarusianTranslit</name>
     <message>
       <source>Belarusian transliteration from latin to cyrillic (classic orthography)</source>
@@ -2646,6 +2653,10 @@ Për të gjetur &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; simbo
       <source>Are you sure you want to clear all history items?</source>
       <translation type="unfinished">Are you sure you want to clear all history items?</translation>
     </message>
+    <message>
+      <source>Save File</source>
+      <translation type="unfinished">Save File</translation>
+    </message>
   </context>
   <context>
     <name>Mdx::MdxArticleRequest</name>
@@ -3116,7 +3127,7 @@ janë në gjendjen e shtypur.</translation>
     </message>
     <message>
       <source>Only track selection when all selected keys are kept pressed:</source>
-      <translation>Zgjedhja e ngjitjes vetëm kur të gjithë tastet e zgjedhur mbahen të shtypur:</translation>
+      <translation type="unfinished">Only track selection when all selected keys are kept pressed:</translation>
     </message>
     <message>
       <source>Alt key</source>
