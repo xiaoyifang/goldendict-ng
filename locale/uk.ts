f0fc2541574ddad794fbc945a2dccad69378701b
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-10 23:01:41 +0800

    New translations Ukrainian from Crowdin

--- locale/uk.ts
+++ locale/uk.ts
@@ -305,6 +305,13 @@
     </message>
   </context>
   <context>
+    <name>ArticleWebView</name>
+    <message>
+      <source>GoldenDict Resource Viewer</source>
+      <translation>Переглядач золотих Dict</translation>
+    </message>
+  </context>
+  <context>
     <name>BelarusianTranslit</name>
     <message>
       <source>Belarusian transliteration from latin to cyrillic (classic orthography)</source>
@@ -2646,6 +2653,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     <message>
       <source>Are you sure you want to clear all history items?</source>
       <translation>Ви впевнені, що хочете очистити всі елементи історії?</translation>
+    </message>
+    <message>
+      <source>Save File</source>
+      <translation>Зберегти</translation>
     </message>
   </context>
   <context>
