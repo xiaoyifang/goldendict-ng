f9bec9464cb45014d0ab1ce7acaab40ef77bcffa
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-11 00:04:40 +0800

    New translations Chinese Traditional from Crowdin

--- locale/zh_TW.ts
+++ locale/zh_TW.ts
@@ -305,6 +305,13 @@
     </message>
   </context>
   <context>
+    <name>ArticleWebView</name>
+    <message>
+      <source>GoldenDict Resource Viewer</source>
+      <translation type="unfinished">GoldenDict Resource Viewer</translation>
+    </message>
+  </context>
+  <context>
     <name>BelarusianTranslit</name>
     <message>
       <source>Belarusian transliteration from latin to cyrillic (classic orthography)</source>
@@ -2645,6 +2652,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
     <message>
       <source>Are you sure you want to clear all history items?</source>
       <translation>您確定要清除所有歷史記錄嗎？</translation>
+    </message>
+    <message>
+      <source>Save File</source>
+      <translation type="unfinished">Save File</translation>
     </message>
   </context>
   <context>
