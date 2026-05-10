38d602f538c23d7b410e09deaddbb937b4e6c8fc
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-10 23:01:21 +0800

    New translations Italian from Crowdin

--- locale/it.ts
+++ locale/it.ts
@@ -306,6 +306,13 @@
     </message>
   </context>
   <context>
+    <name>ArticleWebView</name>
+    <message>
+      <source>GoldenDict Resource Viewer</source>
+      <translation>Visualizzatore Risorse GoldenDict</translation>
+    </message>
+  </context>
+  <context>
     <name>BelarusianTranslit</name>
     <message>
       <source>Belarusian transliteration from latin to cyrillic (classic orthography)</source>
@@ -2647,6 +2654,10 @@ Clicca &lt;b&gt;Scarica&lt;/b&gt; per accedere alla pagina di scaricamento.</tra
     <message>
       <source>Are you sure you want to clear all history items?</source>
       <translation>Sei sicuro di voler cancellare tutti gli elementi della cronologia?</translation>
+    </message>
+    <message>
+      <source>Save File</source>
+      <translation>Salva File</translation>
     </message>
   </context>
   <context>
