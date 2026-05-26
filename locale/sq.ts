af919c1a244c5acc18e8794b696d7e07f6f22739
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 16:17:52 +0800

    New translations Albanian from Crowdin

--- locale/sq.ts
+++ locale/sq.ts
@@ -3205,20 +3205,8 @@ janë në gjendjen e shtypur.</translation>
       <translation>Luaj skedarë audio përmes mbështetjes së integruar audio</translation>
     </message>
     <message>
-      <source>Use internal player:</source>
-      <translation>Përdorni luajtësin e brendshëm:</translation>
-    </message>
-    <message>
       <source>Choose audio back end</source>
       <translation>Zgjidhni fundin e pasëm audio</translation>
-    </message>
-    <message>
-      <source>Use any external program to play audio files</source>
-      <translation>Riprodhon skedat zanore me një program tjetër</translation>
-    </message>
-    <message>
-      <source>Use external program:</source>
-      <translation>Përdor programin e jashtëm:</translation>
     </message>
     <message>
       <source>Enter audio player command line</source>
@@ -3527,6 +3515,26 @@ nga fjalorët Stardict, Babylon dhe GLS</translation>
     <message>
       <source>Hide tab bar when only one tab is open.</source>
       <translation>Fshih shiritin e skedave kur është hapur vetëm një skedë.</translation>
+    </message>
+    <message>
+      <source>Internal Player:</source>
+      <translation>Lojtari i brendshëm:</translation>
+    </message>
+    <message>
+      <source>Use any external audio player to play audio files</source>
+      <translation>Përdorni çdo luajtës audio të jashtëm për të luajtur skedarë audio</translation>
+    </message>
+    <message>
+      <source>External Player:</source>
+      <translation>Lojtar i jashtëm:</translation>
+    </message>
+    <message>
+      <source>View detailed configuration guide</source>
+      <translation>Shikoni udhëzuesin e detajuar të konfigurimit</translation>
+    </message>
+    <message>
+      <source>The audio path will be passed as a command-line argument to the external player. </source>
+      <translation>Rruga audio do të kalojë si një argument i linjës së komandës tek luajtësi i jashtëm. </translation>
     </message>
   </context>
   <context>
