147a1038af639f6456f1933de2c4b7b534092867
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 16:18:12 +0800

    New translations Spanish, Argentina from Crowdin

--- locale/es_AR.ts
+++ locale/es_AR.ts
@@ -3212,20 +3212,8 @@ in the pressed state when the word selection changes.</source>
       <translation>Reproduzca archivos de audio a través del soporte de audio incorporado</translation>
     </message>
     <message>
-      <source>Use internal player:</source>
-      <translation>Usar reproductor interno:</translation>
-    </message>
-    <message>
       <source>Choose audio back end</source>
       <translation>Elija el back-end de audio</translation>
-    </message>
-    <message>
-      <source>Use any external program to play audio files</source>
-      <translation>Utilice un programa externo para reproducir archivos de audio</translation>
-    </message>
-    <message>
-      <source>Use external program:</source>
-      <translation>Utilizar un programa externo:</translation>
     </message>
     <message>
       <source>Enter audio player command line</source>
@@ -3535,6 +3523,26 @@ de los diccionarios Stardict, Babylon y GLS.</translation>
     <message>
       <source>Hide tab bar when only one tab is open.</source>
       <translation type="unfinished">Hide tab bar when only one tab is open.</translation>
+    </message>
+    <message>
+      <source>Internal Player:</source>
+      <translation type="unfinished">Internal Player:</translation>
+    </message>
+    <message>
+      <source>Use any external audio player to play audio files</source>
+      <translation type="unfinished">Use any external audio player to play audio files</translation>
+    </message>
+    <message>
+      <source>External Player:</source>
+      <translation type="unfinished">External Player:</translation>
+    </message>
+    <message>
+      <source>View detailed configuration guide</source>
+      <translation type="unfinished">View detailed configuration guide</translation>
+    </message>
+    <message>
+      <source>The audio path will be passed as a command-line argument to the external player. </source>
+      <translation type="unfinished">The audio path will be passed as a command-line argument to the external player. </translation>
     </message>
   </context>
   <context>
