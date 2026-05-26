9d771146229c5b384a2ecb01ed736f04cb64b057
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 18:30:33 +0800

    New translations Lojban from Crowdin

--- locale/jbo.ts
+++ locale/jbo.ts
@@ -3523,24 +3523,24 @@ from Stardict, Babylon and GLS dictionaries</translation>
       <translation type="unfinished">Hide tab bar when only one tab is open.</translation>
     </message>
     <message>
-      <source>Internal Player:</source>
-      <translation type="unfinished">Internal Player:</translation>
-    </message>
-    <message>
       <source>Use any external audio player to play audio files</source>
-      <translation type="unfinished">Use any external audio player to play audio files</translation>
-    </message>
-    <message>
-      <source>External Player:</source>
-      <translation type="unfinished">External Player:</translation>
+      <translation>zukte lo drata snimei ple'i fi'o ple'i lo snimei fleku</translation>
     </message>
     <message>
       <source>View detailed configuration guide</source>
-      <translation type="unfinished">View detailed configuration guide</translation>
+      <translation>viska le slabu ciste cuxna jmaji</translation>
     </message>
     <message>
       <source>The audio path will be passed as a command-line argument to the external player. </source>
-      <translation type="unfinished">The audio path will be passed as a command-line argument to the external player. </translation>
+      <translation>le snimei cadzu co'a se benji fi'o lisri selcu'a le drata ple'i.</translation>
+    </message>
+    <message>
+      <source>Internal Audio Player:</source>
+      <translation type="unfinished">Internal Audio Player:</translation>
+    </message>
+    <message>
+      <source>External Audio Player:</source>
+      <translation type="unfinished">External Audio Player:</translation>
     </message>
   </context>
   <context>
