1c0ee977c60e4aeaf32557a00bfa6dc6ece968e9
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 18:29:56 +0800

    New translations Lithuanian from Crowdin

--- locale/lt.ts
+++ locale/lt.ts
@@ -3522,16 +3522,8 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Slėpti skirtukų juostą, kai atidarytas tik vienas skirtukas.</translation>
     </message>
     <message>
-      <source>Internal Player:</source>
-      <translation>Vidinis grotuvas:</translation>
-    </message>
-    <message>
       <source>Use any external audio player to play audio files</source>
       <translation>Garso failams leisti naudokite bet kurį išorinį garso grotuvą</translation>
-    </message>
-    <message>
-      <source>External Player:</source>
-      <translation>Išorinis grotuvas:</translation>
     </message>
     <message>
       <source>View detailed configuration guide</source>
@@ -3540,6 +3532,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>The audio path will be passed as a command-line argument to the external player. </source>
       <translation>Garso kelias bus perduotas kaip komandų eilutės argumentas išoriniam leistuvui. </translation>
+    </message>
+    <message>
+      <source>Internal Audio Player:</source>
+      <translation>Vidinis garso grotuvas:</translation>
+    </message>
+    <message>
+      <source>External Audio Player:</source>
+      <translation>Išorinis garso grotuvas:</translation>
     </message>
   </context>
   <context>
