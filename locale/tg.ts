64cf976e560e06b0d7bcba0ab2651cda5662badb
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 18:30:37 +0800

    New translations Tajik from Crowdin

--- locale/tg.ts
+++ locale/tg.ts
@@ -3524,16 +3524,8 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation type="unfinished">Hide tab bar when only one tab is open.</translation>
     </message>
     <message>
-      <source>Internal Player:</source>
-      <translation type="unfinished">Internal Player:</translation>
-    </message>
-    <message>
       <source>Use any external audio player to play audio files</source>
       <translation type="unfinished">Use any external audio player to play audio files</translation>
-    </message>
-    <message>
-      <source>External Player:</source>
-      <translation type="unfinished">External Player:</translation>
     </message>
     <message>
       <source>View detailed configuration guide</source>
@@ -3542,6 +3534,14 @@ from Stardict, Babylon and GLS dictionaries</source>
     <message>
       <source>The audio path will be passed as a command-line argument to the external player. </source>
       <translation type="unfinished">The audio path will be passed as a command-line argument to the external player. </translation>
+    </message>
+    <message>
+      <source>Internal Audio Player:</source>
+      <translation type="unfinished">Internal Audio Player:</translation>
+    </message>
+    <message>
+      <source>External Audio Player:</source>
+      <translation type="unfinished">External Audio Player:</translation>
     </message>
   </context>
   <context>
