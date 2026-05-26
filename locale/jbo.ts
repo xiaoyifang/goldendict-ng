cc98ee8beeca28bdaafed5e678e508dd1a0dab1c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-26 16:18:18 +0800

    New translations Lojban from Crowdin

--- locale/jbo.ts
+++ locale/jbo.ts
@@ -3209,20 +3209,8 @@ in the pressed state when the word selection changes.</translation>
       <translation>Play audio files via built-in audio support</translation>
     </message>
     <message>
-      <source>Use internal player:</source>
-      <translation>Use internal player:</translation>
-    </message>
-    <message>
       <source>Choose audio back end</source>
       <translation>Choose audio back end</translation>
-    </message>
-    <message>
-      <source>Use any external program to play audio files</source>
-      <translation>Use any external program to play audio files</translation>
-    </message>
-    <message>
-      <source>Use external program:</source>
-      <translation>Use external program:</translation>
     </message>
     <message>
       <source>Enter audio player command line</source>
@@ -3533,6 +3521,26 @@ from Stardict, Babylon and GLS dictionaries</translation>
     <message>
       <source>Hide tab bar when only one tab is open.</source>
       <translation type="unfinished">Hide tab bar when only one tab is open.</translation>
+    </message>
+    <message>
+      <source>Internal Player:</source>
+      <translation type="unfinished">Internal Player:</translation>
+    </message>
+    <message>
+      <source>Use any external audio player to play audio files</source>
+      <translation type="unfinished">Use any external audio player to play audio files</translation>
+    </message>
+    <message>
+      <source>External Player:</source>
+      <translation type="unfinished">External Player:</translation>
+    </message>
+    <message>
+      <source>View detailed configuration guide</source>
+      <translation type="unfinished">View detailed configuration guide</translation>
+    </message>
+    <message>
+      <source>The audio path will be passed as a command-line argument to the external player. </source>
+      <translation type="unfinished">The audio path will be passed as a command-line argument to the external player. </translation>
     </message>
   </context>
   <context>
