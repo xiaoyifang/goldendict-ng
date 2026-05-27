5d28da95c0520d85de7c1a2fbc2114f521dd72ac
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-05-27 16:29:08 +0800

    New translations Japanese from Crowdin

--- locale/ja.ts
+++ locale/ja.ts
@@ -3542,6 +3542,10 @@ from Stardict, Babylon and GLS dictionaries</source>
       <source>External Audio Player:</source>
       <translation>外部オーディオプレーヤー:</translation>
     </message>
+    <message>
+      <source>Follow system dark mode setting.</source>
+      <translation>システムのダークモード設定に従ってください。</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
