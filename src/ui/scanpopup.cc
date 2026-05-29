d93758b1bd483dd48a0d19e3d5ac46830c082c05
xiaoyifang <yifang.xiao@foxmail.com>
2026-05-29 16:15:40 +0800

    1

--- src/ui/scanpopup.cc
+++ src/ui/scanpopup.cc
@@ -88,6 +88,8 @@ ScanPopup::ScanPopup( QWidget * parent,
     ui.goForwardButton->setIcon( QIcon( ":/icons/previous.svg" ) );
   }
 
+  setStatusBar( nullptr );
+
   mainStatusBar = new MainStatusBar( this );
 
   tabWidget = new MainTabWidget( this );
@@ -343,7 +345,6 @@ ScanPopup::ScanPopup( QWidget * parent,
     connect( &selectionDelayTimer, &QTimer::timeout, this, &ScanPopup::translateWordFromSelection );
   }
 #endif
-  setStatusBar( nullptr );
   applyZoomFactor();
 }
 
