0815fff4d3afbd6b865b8ddd08ecc847105f9621
xiaoyifang <yifang.xiao@foxmail.com>
2026-06-02 10:38:56 +0800

    Simplify icon resources: use programicon.png for all UI

--- src/ui/mainwindow.cc
+++ src/ui/mainwindow.cc
@@ -1568,8 +1568,8 @@ void MainWindow::trayIconUpdateOrInit()
   else {
     // Update the icon to reflect the scanning mode
     QIcon icon = enableScanningAction->isChecked() ?
-      QIcon::fromTheme( "goldendict-scan-tray", QIcon( ":/icons/programicon_scan.png" ) ) :
-      QIcon::fromTheme( "goldendict-tray", QIcon( ":/icons/programicon_old.png" ) );
+      QIcon::fromTheme( "goldendict-scan-tray", QIcon( ":/icons/programicon.png" ) ) :
+      QIcon::fromTheme( "goldendict-tray", QIcon( ":/icons/programicon.png" ) );
 
     if ( !trayIcon ) {
       trayIcon = new QSystemTrayIcon( this );
