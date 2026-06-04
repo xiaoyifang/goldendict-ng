2dbd1370b2805464c9e4312925fb822b47e6bb0a
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-06-04 08:30:37 +0800

    New translations Vietnamese from Crowdin

--- locale/vi.ts
+++ locale/vi.ts
@@ -2650,6 +2650,10 @@ To find &apos;*&apos;, &apos;?&apos;, &apos;[&apos;, &apos;]&apos; symbols use &
       <source>Save File</source>
       <translation>Lưu tệp</translation>
     </message>
+    <message>
+      <source>Application is still running in the background. Click the tray icon to show the window.</source>
+      <translation>Ứng dụng vẫn đang chạy trong nền. Nhấp vào biểu tượng khay để hiển thị cửa sổ.</translation>
+    </message>
   </context>
   <context>
     <name>Mdx::MdxArticleRequest</name>
