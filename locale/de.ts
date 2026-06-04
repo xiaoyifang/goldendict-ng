2e116ff637952b9985477af32b726482f0304610
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-06-04 08:30:02 +0800

    New translations German from Crowdin

--- locale/de.ts
+++ locale/de.ts
@@ -2666,6 +2666,10 @@ Um &apos;*&apos;, &apos;zu finden?&apos;, &apos;[&apos;, &apos;]&apos; Symbole v
       <source>Save File</source>
       <translation>Datei speichern</translation>
     </message>
+    <message>
+      <source>Application is still running in the background. Click the tray icon to show the window.</source>
+      <translation>Die Anwendung läuft noch im Hintergrund. Klicken Sie auf das Tray-Symbol, um das Fenster anzuzeigen.</translation>
+    </message>
   </context>
   <context>
     <name>Mdx::MdxArticleRequest</name>
