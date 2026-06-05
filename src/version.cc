fbe130a29511ea708aeb907fca12ecc6b28c7325
xiaoyifang <yifang.xiao@foxmail.com>
2026-06-05 16:14:00 +0800

    fix(cmake): remove redundant GIT_BRANCH initialization

--- src/version.cc
+++ src/version.cc
@@ -26,26 +26,31 @@ QString getVersionTag()
 {
   QString ver = version();
   
-  QRegularExpression re( R"(^([\d.]+)(?:\.([a-f0-9]+))?\s+at)" );
+  QRegularExpression re( R"(^([\d.]+)-([a-f0-9]+)\s*\([^)]+\)\s+at)" );
   QRegularExpressionMatch match = re.match( ver );
   
   if ( match.hasMatch() ) {
     QString baseVersion = match.captured( 1 );
     QString gitHash = match.captured( 2 );
     
-    if ( !gitHash.isEmpty() ) {
-      return QStringLiteral( "v%1_alpha.%2" ).arg( baseVersion, gitHash );
-    } else {
-      return QStringLiteral( "v%1" ).arg( baseVersion );
-    }
+    return QStringLiteral( "v%1_alpha.%2" ).arg( baseVersion, gitHash );
   }
   
-  int spacePos = ver.indexOf( ' ' );
-  if ( spacePos > 0 ) {
-    return QStringLiteral( "v%1" ).arg( ver.left( spacePos ) );
-  } else {
-    return QStringLiteral( "v%1" ).arg( ver );
+  QRegularExpression reSimple( R"(^([\d.]+)\s+at)" );
+  QRegularExpressionMatch matchSimple = reSimple.match( ver );
+  
+  if ( matchSimple.hasMatch() ) {
+    return QStringLiteral( "v%1" ).arg( matchSimple.captured( 1 ) );
   }
+  
+  QRegularExpression reNoAt( R"(^([\d.]+))" );
+  QRegularExpressionMatch matchNoAt = reNoAt.match( ver );
+  
+  if ( matchNoAt.hasMatch() ) {
+    return QStringLiteral( "v%1" ).arg( matchNoAt.captured( 1 ) );
+  }
+  
+  return QString();
 }
 
 QString getReleaseUrl()
