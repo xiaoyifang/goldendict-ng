d2818b5601b15db8c1ed4ad3ec11dd391e8933b9
xiaoyifang <yifang.xiao@foxmail.com>
2026-06-05 16:11:52 +0800

    feat(ui): add changelog link to about dialog
    - Add  and  functions in version.cc
    - Add clickable Changelog link in About dialog that opens GitHub release page
    - Fix: ensure consistent git hash length (10 chars) in both CMake and CI workflow

--- src/version.cc
+++ src/version.cc
@@ -1,5 +1,6 @@
 #include "version.hh"
 #include <QFile>
+#include <QRegularExpression>
 
 namespace Version {
 QString version()
@@ -19,6 +20,41 @@ QString everything()
   return QStringLiteral( "Version: " ) + Version::version() + "\n" + "Qt " + QLatin1String( qVersion() ) + " "
     + Version::compiler + "\n" + QSysInfo::productType() + " " + QSysInfo::kernelType() + " "
     + QSysInfo::kernelVersion() + " " + QSysInfo::buildAbi() + "\nFlags: " + flags;
+}
+
+QString getVersionTag()
+{
+  QString ver = version();
+  
+  QRegularExpression re( R"(^([\d.]+)(?:\.([a-f0-9]+))?\s+at)" );
+  QRegularExpressionMatch match = re.match( ver );
+  
+  if ( match.hasMatch() ) {
+    QString baseVersion = match.captured( 1 );
+    QString gitHash = match.captured( 2 );
+    
+    if ( !gitHash.isEmpty() ) {
+      return QStringLiteral( "v%1_alpha.%2" ).arg( baseVersion, gitHash );
+    } else {
+      return QStringLiteral( "v%1" ).arg( baseVersion );
+    }
+  }
+  
+  int spacePos = ver.indexOf( ' ' );
+  if ( spacePos > 0 ) {
+    return QStringLiteral( "v%1" ).arg( ver.left( spacePos ) );
+  } else {
+    return QStringLiteral( "v%1" ).arg( ver );
+  }
+}
+
+QString getReleaseUrl()
+{
+  QString tag = getVersionTag();
+  if ( tag.isEmpty() ) {
+    return QStringLiteral( "https://github.com/xiaoyifang/goldendict-ng/releases" );
+  }
+  return QStringLiteral( "https://github.com/xiaoyifang/goldendict-ng/releases/tag/%1" ).arg( tag );
 }
 
 } // namespace Version
