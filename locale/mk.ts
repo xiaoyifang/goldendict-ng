75374422485b35833a1eabb6904e9fe3a5c272cd
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-07-15 18:24:30 +0800

    New translations Macedonian from Crowdin

--- locale/mk.ts
+++ locale/mk.ts
@@ -520,10 +520,6 @@ between classic and school orthography in cyrillic)</source>
       <translation>Филтер:</translation>
     </message>
     <message>
-      <source>Search mode</source>
-      <translation>Режим на пребарување</translation>
-    </message>
-    <message>
       <source>This element determines how filter string will be interpreted</source>
       <translation>Овој елемент одредува како ќе се толкува низата на филтерот</translation>
     </message>
@@ -546,18 +542,6 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <source>Specify the maximum filtered headwords returned.</source>
       <translation>Наведете ги максималните вратени филтрирани зборови.</translation>
-    </message>
-    <message>
-      <source>Filter max results:</source>
-      <translation>Филтрирајте ги максималните резултати:</translation>
-    </message>
-    <message>
-      <source>Help</source>
-      <translation>Помош</translation>
-    </message>
-    <message>
-      <source>OK</source>
-      <translation>добро</translation>
     </message>
     <message>
       <source>Text</source>
@@ -606,6 +590,10 @@ between classic and school orthography in cyrillic)</source>
     <message>
       <source>Regular Expression</source>
       <translation>Регуларен израз</translation>
+    </message>
+    <message>
+      <source>Max:</source>
+      <translation>Макс:</translation>
     </message>
   </context>
   <context>
