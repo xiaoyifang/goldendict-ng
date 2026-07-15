fa1bf85f621a69abc58876019b236672bb53787c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-07-15 18:25:00 +0800

    New translations Lojban from Crowdin

--- locale/jbo.ts
+++ locale/jbo.ts
@@ -520,10 +520,6 @@ between classic and school orthography in cyrillic)</translation>
       <translation>julne</translation>
     </message>
     <message>
-      <source>Search mode</source>
-      <translation>tadji lo nu sisku</translation>
-    </message>
-    <message>
       <source>This element determines how filter string will be interpreted</source>
       <translation>This element determines how filter string will be interpreted</translation>
     </message>
@@ -546,18 +542,6 @@ between classic and school orthography in cyrillic)</translation>
     <message>
       <source>Specify the maximum filtered headwords returned.</source>
       <translation>Specify the maximum filtered headwords returned.</translation>
-    </message>
-    <message>
-      <source>Filter max results:</source>
-      <translation>Filter max results:</translation>
-    </message>
-    <message>
-      <source>Help</source>
-      <translation>sidju</translation>
-    </message>
-    <message>
-      <source>OK</source>
-      <translation>mipri</translation>
     </message>
     <message>
       <source>Text</source>
@@ -606,6 +590,10 @@ between classic and school orthography in cyrillic)</translation>
     <message>
       <source>Regular Expression</source>
       <translation>Regular Expression</translation>
+    </message>
+    <message>
+      <source>Max:</source>
+      <translation type="unfinished">Max:</translation>
     </message>
   </context>
   <context>
