49912bd1f4fd09b687e9e69d539cd9724aa70f57
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-07-15 18:24:49 +0800

    New translations English from Crowdin

--- locale/en.ts
+++ locale/en.ts
@@ -520,10 +520,6 @@ between classic and school orthography in cyrillic)</translation>
       <translation type="unfinished">Filter:</translation>
     </message>
     <message>
-      <source>Search mode</source>
-      <translation type="unfinished">Search mode</translation>
-    </message>
-    <message>
       <source>This element determines how filter string will be interpreted</source>
       <translation type="unfinished">This element determines how filter string will be interpreted</translation>
     </message>
@@ -546,18 +542,6 @@ between classic and school orthography in cyrillic)</translation>
     <message>
       <source>Specify the maximum filtered headwords returned.</source>
       <translation type="unfinished">Specify the maximum filtered headwords returned.</translation>
-    </message>
-    <message>
-      <source>Filter max results:</source>
-      <translation type="unfinished">Filter max results:</translation>
-    </message>
-    <message>
-      <source>Help</source>
-      <translation type="unfinished">Help</translation>
-    </message>
-    <message>
-      <source>OK</source>
-      <translation type="unfinished">OK</translation>
     </message>
     <message>
       <source>Text</source>
@@ -606,6 +590,10 @@ between classic and school orthography in cyrillic)</translation>
     <message>
       <source>Regular Expression</source>
       <translation type="unfinished">Regular Expression</translation>
+    </message>
+    <message>
+      <source>Max:</source>
+      <translation type="unfinished">Max:</translation>
     </message>
   </context>
   <context>
