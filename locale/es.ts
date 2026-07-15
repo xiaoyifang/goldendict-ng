11259e4950e18eaac5206f8c9e90e8590a196f59
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-07-15 18:24:10 +0800

    New translations Spanish from Crowdin

--- locale/es.ts
+++ locale/es.ts
@@ -520,10 +520,6 @@ entre ortografía clásica y escolar en cirílico)</translation>
       <translation>Filtro:</translation>
     </message>
     <message>
-      <source>Search mode</source>
-      <translation>Modo de búsqueda</translation>
-    </message>
-    <message>
       <source>This element determines how filter string will be interpreted</source>
       <translation>Este elemento determina cómo se interpreta la cadena de filtro</translation>
     </message>
@@ -546,18 +542,6 @@ entre ortografía clásica y escolar en cirílico)</translation>
     <message>
       <source>Specify the maximum filtered headwords returned.</source>
       <translation>Especifique el máximo filtrado de palabras de cabecera devueltas.</translation>
-    </message>
-    <message>
-      <source>Filter max results:</source>
-      <translation>Filtrar máximos resultados:</translation>
-    </message>
-    <message>
-      <source>Help</source>
-      <translation>Ayuda</translation>
-    </message>
-    <message>
-      <source>OK</source>
-      <translation>Vale</translation>
     </message>
     <message>
       <source>Text</source>
@@ -606,6 +590,10 @@ entre ortografía clásica y escolar en cirílico)</translation>
     <message>
       <source>Regular Expression</source>
       <translation>Expresión regular</translation>
+    </message>
+    <message>
+      <source>Max:</source>
+      <translation>Max:</translation>
     </message>
   </context>
   <context>
