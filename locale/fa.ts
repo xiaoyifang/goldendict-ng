755127548cbe9719a93eb96442efce02b8b2a255
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-08-04 17:35:38 +0800

    New translations Persian from Crowdin

--- locale/fa.ts
+++ locale/fa.ts
@@ -3815,10 +3815,6 @@ from Stardict, Babylon and GLS dictionaries</source>
       <translation>Send word to main window (Alt+W)</translation>
     </message>
     <message>
-      <source>Alt+W</source>
-      <translation type="unfinished">Alt+W</translation>
-    </message>
-    <message>
       <source>Add word to Favorites (Ctrl+E)</source>
       <translation>Add word to Favorites (Ctrl+E)</translation>
     </message>
