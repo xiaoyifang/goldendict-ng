2bcaa26ecaf5825e0d7d73b5ff4a992ab84d109c
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-08-10 08:52:53 +0800

    New translations Spanish, Argentina from Crowdin

--- locale/es_AR.ts
+++ locale/es_AR.ts
@@ -3795,6 +3795,22 @@ de los diccionarios Stardict, Babylon y GLS.</translation>
       <source>Definition</source>
       <translation type="unfinished">Definition</translation>
     </message>
+    <message>
+      <source>Close current tab</source>
+      <translation>Cerrar pestaña actual</translation>
+    </message>
+    <message>
+      <source>Close all tabs except current</source>
+      <translation>Cerrar todas las pestañas excepto la actual</translation>
+    </message>
+    <message>
+      <source>Close all tabs</source>
+      <translation>Cerrar todas las pestañas</translation>
+    </message>
+    <message>
+      <source>Open in &amp;External Browser</source>
+      <translation type="unfinished">Open in &amp;External Browser</translation>
+    </message>
   </context>
   <context>
     <name>ScanPopupToolBar</name>
