ab7440a35ec8b9f72075ac19e84e1fd40a7bb4a3
xiaoyifang <105986+xiaoyifang@users.noreply.github.com>
2026-09-09 18:11:46 +0800

    New translations Kabyle from Crowdin

--- locale/kab.ts
+++ locale/kab.ts
@@ -3548,6 +3548,10 @@ from Stardict, Babylon and GLS dictionaries</translation>
       <source>Keep Dock icon visible in background</source>
       <translation>Eg ayen n "Dock" yettban-d deg yidisan</translation>
     </message>
+    <message>
+      <source>Add audio to Anki card when available</source>
+      <translation type="unfinished">Add audio to Anki card when available</translation>
+    </message>
   </context>
   <context>
     <name>ProgramTypeEditor</name>
